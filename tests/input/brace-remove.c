9de3f373a5e6bbcf423d0f067663b29e1f80471a
Ben Gardner <gardner.ben@gmail.com>
2006-03-10 04:37:49 +0000

    add brace removal tests
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@52 74908cc9-670d-0410-99d4-ca74d6b17880

---
+++ tests/input/brace-remove.c
@@ -0,0 +1,40 @@
+
+void foo(void)
+{
+   int a = 0;
+   while (a < 3)
+   {
+      a++;
+   }
+
+   while (b < a)
+      b++;
+
+   do
+   {
+      a--;
+   } while (a > 0);
+
+   for (a = 0; a < 10; a++)
+   {
+      printf("a=%d\n", a);
+   }
+
+   if (a == 10)
+   {
+      printf("a looks good\n");
+   }
+
+   if (state == ST_RUN)
+   {
+      if ((foo < bar) &&
+          (bar > foo2))
+      {
+         if (a < 5)
+         {
+            a *= a;
+         }
+      }
+   }
+}
+
