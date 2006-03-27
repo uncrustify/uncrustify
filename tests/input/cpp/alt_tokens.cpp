45c39de842eed1955d3bb593d53b6699b12a2b37
Ben Gardner <gardner.ben@gmail.com>
2006-03-27 03:46:22 +0000

    add a test of the alt tokens
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@129 74908cc9-670d-0410-99d4-ca74d6b17880

---
+++ tests/input/cpp/alt_tokens.cpp
@@ -0,0 +1,5 @@
+%:include <iostream>                // #include <iostream>
+int main(int argc, char *argv[]) <% // {
+int array<:10:>;                    // int array[10];
+%>                                  // }
+
---
+++ tests/output/cpp/30001-alt_tokens.cpp
@@ -0,0 +1,6 @@
+%:include <iostream>                // #include <iostream>
+int main(int argc, char *argv[])
+<%                                  // {
+   int array<:10:>;                 // int array[10];
+%>                                  // }
+
