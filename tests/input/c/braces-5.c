a661e518a93cb174b4083c52895442a5f29183e6
Ben Gardner <gardner.ben@gmail.com>
2006-03-30 00:36:37 +0000

    test case 65 was broken
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@140 74908cc9-670d-0410-99d4-ca74d6b17880

--- tests/input/c/braces-5.c
+++ tests/input/c/braces-5.c
@@ -8,7 +8,6 @@ void foo(void)
 #ifdef CONFIG_BLK_DEV_INITRD
    if (initrd_start)
       ROOT_DEV = Root_RAM0;
-   else
 #elif defined(CONFIG_ROOT_NFS)
    ROOT_DEV = Root_NFS;
 #elif defined(CONFIG_BLK_DEV_IDEDISK)
--- tests/output/c/00065-braces-5.c
+++ tests/output/c/00065-braces-5.c
@@ -5,10 +5,12 @@
 void foo(void)
 {
    int a;
+
 #ifdef CONFIG_BLK_DEV_INITRD
    if (initrd_start)
+   {
       ROOT_DEV = Root_RAM0;
-   else
+   }
 #elif defined(CONFIG_ROOT_NFS)
    ROOT_DEV = Root_NFS;
 #elif defined(CONFIG_BLK_DEV_IDEDISK)
