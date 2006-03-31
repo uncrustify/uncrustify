40566557c07d92e544d33feeecb0a00c86bd11a8
Ben Gardner <gardner.ben@gmail.com>
2006-03-31 21:07:04 +0000

    fix the elipsis bug again
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@151 74908cc9-670d-0410-99d4-ca74d6b17880

--- src/logger.h
+++ src/logger.h
@@ -93,7 +93,7 @@ void log_str(log_sev_t sev, const char *str, int len);
  */
 void log_fmt(log_sev_t sev, const char *fmt, ...) __attribute__((format(printf, 2, 3)));
 
-#define LOG_FMT(sev, args ...) \
+#define LOG_FMT(sev, args...) \
    do { if (log_sev_on(sev)) { log_fmt(sev, ## args); } } while (0)
 
 
