368d62349daf7744cffd65fe8ff389f6374a088a
Ben Gardner <gardner.ben@gmail.com>
2006-05-12 22:59:04 +0000

    uncrustify-0.0.18
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@216 74908cc9-670d-0410-99d4-ca74d6b17880

--- src/config.h
+++ src/config.h
@@ -73,19 +73,19 @@
 #define PACKAGE_NAME "uncrustify"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "uncrustify 0.0.17"
+#define PACKAGE_STRING "uncrustify 0.0.18"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "uncrustify"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.0.17"
+#define PACKAGE_VERSION "0.0.18"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.0.17"
+#define VERSION "0.0.18"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
