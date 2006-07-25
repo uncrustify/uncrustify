8ec0168650ec8661d49fb2a421558ccfea07866a
Ben Gardner <gardner.ben@gmail.com>
2006-07-25 23:44:12 +0000

    add a wacko test for neg neg
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@355 74908cc9-670d-0410-99d4-ca74d6b17880

---
+++ tests/input/c/minus-minus.c
@@ -0,0 +1,26 @@
+int a = 5;
+int negneg(void)
+{
+   return - -a;
+}
+
+int pospos(void)
+{
+   return + +a;
+}
+
+int negpos(void)
+{
+   return - +a;
+}
+
+int posneg(void)
+{
+   return + -a;
+}
+
+int negnegneg(void)
+{
+   return - - -a;
+}
+
