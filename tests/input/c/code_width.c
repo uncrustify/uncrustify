18daca22d654e7a77a5c62a0559e1abd26cf9f64
Ben Gardner <gardner.ben@gmail.com>
2006-08-05 00:48:37 +0000

    add a code width test
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@393 74908cc9-670d-0410-99d4-ca74d6b17880

---
+++ tests/input/c/code_width.c
@@ -0,0 +1,20 @@
+
+/* Assuming a 60-column limit */
+static int short_function_name(struct device *dev, struct device_driver *drv)
+{
+    call_some_really_long_function.of_some_sort(some_long_parameter1, some_long_parameter2);
+
+    abc = call_some_other_really_long_function.of_some_sort(some_long_parameter1, some_long_parameter2);
+
+    abc.def.ghi = call_some_other_really_long_function.of_some_sort(some_long_parameter1, some_long_parameter2);
+
+    return 1;
+}
+
+static void some_really_long_function_name(struct device *dev, struct device_driver *drv)
+{
+   if ((some_variable_name && somefunction(param1, param2, param3)))
+   {
+      asdfghjk = asdfasdfasd.aasdfasd + (asdfasd.asdas * 1234.65);
+   }
+}
