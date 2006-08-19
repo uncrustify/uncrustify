41e2c9f71a3a3e6db0fbf8c7006a868a44ac0ca8
Ben Gardner <gardner.ben@gmail.com>
2006-08-19 16:11:06 +0000

    add some tests
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@427 74908cc9-670d-0410-99d4-ca74d6b17880

---
+++ tests/input/c/nl-ver-def.c
@@ -0,0 +1,9 @@
+void foo(void)
+{
+   int a;
+   int b;// comment
+
+   /* comment */
+   a = b;
+   return;
+}
---
+++ tests/input/cpp/templates.cpp
@@ -0,0 +1,13 @@
+
+A<B> foo;
+A<B,C> bar;
+A<B*> baz;
+A<B<C> > bay;
+
+void asd(void)
+{
+A<B> foo;
+A<B,C> bar;
+A<B*> baz;
+A<B<C> > bay;
+}
