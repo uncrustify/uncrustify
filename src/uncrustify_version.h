64f60dfb1aa5239fce1e62b69918ce5a82d410ea
Ben Gardner <gardner.ben@gmail.com>
2006-09-24 00:19:21 +0000

    version 0.27
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@519 74908cc9-670d-0410-99d4-ca74d6b17880

--- src/uncrustify_version.h
+++ src/uncrustify_version.h
@@ -2,10 +2,11 @@
  * @file version.h
  * Simply defines UNCRUSTIFY_VERSION.
  * I don't particularly like how autoconf and friends handle the version...
+ * $Id$
  */
 #ifndef UNCRUSTIFY_VERSION_H
 #define UNCRUSTIFY_VERSION_H
 
-#define UNCRUSTIFY_VERSION    "0.26"
+#define UNCRUSTIFY_VERSION    "0.27"
 
 #endif   /* UNCRUSTIFY_VERSION */
