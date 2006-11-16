21c621c014e7fce40d4ec57cbbdef5af9e9ce91c
Ben Gardner <gardner.ben@gmail.com>
2006-11-16 04:33:05 +0000

    update tests for recent bugfix
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@571 74908cc9-670d-0410-99d4-ca74d6b17880

--- tests/input/c/return-multi.c
+++ tests/input/c/return-multi.c
@@ -15,3 +15,33 @@ get_tv32(struct timeval *o, struct timeval32 __user *i)
 		 __get_user(o->tv_usec, &i->tv_usec)));
 }
 
+const char *
+dcrp_license_feature(int32_t idx)
+{
+#define FEATURESTR(f) \
+  case DCRMIB_LICENSE_##f: \
+    return DCRP_LICENSE_FEATURE_##f##_STR
+
+  switch(idx) {
+    DCRP_LICENSE_FOREACH_FEATURES(FEATURESTR);
+  }
+
+  return "";
+
+}
+
+
+static int
+isValidLicenseType(int32_t idx)
+{
+#define CHECKFEATURE(f) \
+  case DCRMIB_LICENSE_##f: \
+    return 1
+
+  switch(idx) {
+    DCRP_LICENSE_FOREACH_FEATURES(CHECKFEATURE);
+  }
+
+  return 0;
+
+}
