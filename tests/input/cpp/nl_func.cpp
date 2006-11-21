d9a191d61128e32c6a429b5fac5187df32a894fb
Ben Gardner <gardner.ben@gmail.com>
2006-11-21 15:41:52 +0000

    add a test for the recent fix
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@579 74908cc9-670d-0410-99d4-ca74d6b17880

---
+++ tests/input/cpp/nl_func.cpp
@@ -0,0 +1,11 @@
+class BSRRE1D_file : PhysicalFile
+{
+   public BSRRE1D_file()
+   {
+       this.addFormatName("BSRRE1DF");
+   }
+   public void xxx()
+   {
+       ahoj();
+   }
+}
---
+++ tests/output/cpp/30055-nl_func.cpp
@@ -0,0 +1,14 @@
+class BSRRE1D_file : PhysicalFile
+{
+	public BSRRE1D_file()
+	{
+		this.addFormatName("BSRRE1DF");
+	}
+
+
+
+	public void xxx()
+	{
+		ahoj();
+	}
+}
