7af643bed79b8a408addfd9dd2f533e47213cb3e
Ben Gardner <gardner.ben@gmail.com>
2006-11-27 23:35:26 +0000

    improve test
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@595 74908cc9-670d-0410-99d4-ca74d6b17880

--- tests/output/c/00004-switch.c
+++ tests/output/c/00004-switch.c
@@ -3,25 +3,25 @@ int foo(int op)
 {
 	switch (op)
 	{
-	case 1: {
-			do_something();
-			return 0;
-        }
+		case 1: {
+				do_something();
+				return 0;
+		}
 
-	case 2:
-		do_something_else();
-		return 1;
+		case 2:
+			do_something_else();
+			return 1;
 
-	case 3:
-	case 4:
-		/* don't do anything */
-		break;
+		case 3:
+		case 4:
+			/* don't do anything */
+			break;
 
-	case 5:
-		return 3;
+		case 5:
+			return 3;
 
-	default:
-		break;
+		default:
+			break;
 	}
 
 	return -1;
