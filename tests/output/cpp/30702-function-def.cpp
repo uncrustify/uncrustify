1c1954d018c20f2d500da67dd01e5a44a971095e
Ben Gardner <gardner.ben@gmail.com>
2006-11-29 01:22:31 +0000

    add tests for recent bugfix
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@598 74908cc9-670d-0410-99d4-ca74d6b17880

---
+++ tests/output/cpp/30702-function-def.cpp
@@ -0,0 +1,40 @@
+void
+foo1(int param1, int param2, char *param2);
+
+void
+foo2(int param1,
+     int param2,
+     char *param2);
+
+void
+foo3(int param1,
+     int param2,
+     char *param2);
+
+struct whoopee *
+foo4(int param1, int param2, char *param2);
+
+const struct snickers *
+foo5(int param1, int param2, char *param2);
+
+
+void
+foo(int param1, int param2, char *param2)
+{
+   printf("boo!\n");
+}
+
+int classname::method();
+
+int classname::method()
+{
+   foo();
+}
+
+int classname::method2();
+
+int classname::method2()
+{
+   foo2();
+}
+
