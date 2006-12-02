7206c1ce635555def9a03f557e5e47ba25fe4c9e
Ben Gardner <gardner.ben@gmail.com>
2006-12-02 15:28:35 +0000

    update test cases to reflect indenting bug
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@603 74908cc9-670d-0410-99d4-ca74d6b17880

--- tests/input/c/case.c
+++ tests/input/c/case.c
@@ -5,27 +5,39 @@ void foo(void)
    case 'a':
       {
          handle_a();
+         multiline(123,
+                   345);
          break;
       }
 
    case 'b':
       handle_b();
+      multiline(123,
+                345);
       break;
 
    case 'c':
    case 'd':
       handle_cd();
+      multiline(123,
+                345);
       break;
 
    case 'e':
       {
          handle_a();
+         multiline(123,
+                   345);
       }
       break;
 
    default:
       handle_default();
+      multiline(123,
+                345);
       break;
    }
+   multiline(123,
+             345);
 }
 
--- tests/output/c/00203-case.c
+++ tests/output/c/00203-case.c
@@ -1,31 +1,43 @@
 void foo(void)
 {
-   switch(ch)
-   {
-      case 'a':
-         {
-         handle_a();
-         break;
-         }
+	switch(ch)
+	{
+		case 'a':
+			{
+			handle_a();
+			multiline(123,
+			          345);
+			break;
+			}
 
-      case 'b':
-         handle_b();
-         break;
+		case 'b':
+			handle_b();
+			multiline(123,
+			          345);
+			break;
 
-      case 'c':
-      case 'd':
-         handle_cd();
-         break;
+		case 'c':
+		case 'd':
+			handle_cd();
+			multiline(123,
+			          345);
+			break;
 
-      case 'e':
-         {
-         handle_a();
-         }
-         break;
+		case 'e':
+			{
+			handle_a();
+			multiline(123,
+			          345);
+			}
+			break;
 
-      default:
-         handle_default();
-         break;
-   }
+		default:
+			handle_default();
+			multiline(123,
+			          345);
+			break;
+	}
+	multiline(123,
+	          345);
 }
 
