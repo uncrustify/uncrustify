b8856185156ce5a7c789c31e2dc6a89ae046c5f0
Ben Gardner <gardner.ben@gmail.com>
2007-02-14 04:50:45 +0000

    remove the assert
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@658 74908cc9-670d-0410-99d4-ca74d6b17880

--- src/align.cpp
+++ src/align.cpp
@@ -929,10 +929,9 @@ static chunk_t *scan_ib_line(chunk_t *start, bool first_pass)
                      cpd.al[idx].col = pc->column;
                   }
                }
-               else
+               else if (idx > 0)
                {
                   int min_col_diff = pc->column - prev_match->column;
-                  assert(idx > 0);
                   int cur_col_diff = cpd.al[idx].col - cpd.al[idx - 1].col;
                   if (cur_col_diff < min_col_diff)
                   {
