c18db09271c95a677ca1a3ce3941bc1bbe6b93b0
Ben Gardner <gardner.ben@gmail.com>
2007-03-02 03:02:00 +0000

    improve test
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@701 74908cc9-670d-0410-99d4-ca74d6b17880

--- tests/output/cpp/30016-custom-open.cpp
+++ tests/output/cpp/30016-custom-open.cpp
@@ -6,6 +6,12 @@ void className::set(const objectName& obj)
 		DOSTUFF(params)
 	MACRO_ELSE_STUFF()
 		DOMORESTUFF(moreparams)
+		junk = 1;
+		MACRO2_BEGIN_STUFF
+			junk += 3;
+		MACRO2_ELSE_STUFF
+			junk += 4;
+		MACRO2_END_STUFF
 		DOLASTSTUFF(lastparams)
 	MACRO_END_STUFF()
 	statement2();
