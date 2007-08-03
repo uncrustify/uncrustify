b60f5497f163965e3f02752c355830cb6cfeee0b
Ben Gardner <gardner.ben@gmail.com>
2007-08-03 20:29:00 +0000

    add tests
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@845 74908cc9-670d-0410-99d4-ca74d6b17880

--- tests/output/cpp/30018-class-addr.h
+++ tests/output/cpp/30018-class-addr.h
@@ -2,10 +2,10 @@ class C
 {
  public:
    A         *B;
-   C &       D;
-   const C & D;
-   static C &D;
-   public C &D;
+   C&        D;
+   const C&  D;
+   static C& D;
+   public C& D;
    E = C & D;
 };
 
