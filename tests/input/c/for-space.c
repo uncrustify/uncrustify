c3a591eb3d96dfd5d2d3a1225a3fcf497ec8a2d6
Ben Gardner <gardner.ben@gmail.com>
2007-08-04 02:50:54 +0000

    update a bunch of tests
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@846 74908cc9-670d-0410-99d4-ca74d6b17880

---
+++ tests/input/c/for-space.c
@@ -0,0 +1,14 @@
+void f()
+{
+for ( ; it != Values.end(); ++it)
+
+BTree.insert(std::pair < int, double > (*it, double (*it) + 1.0));
+
+
+for ( ; vIt != Values.end(); ++vIt)
+{ }
+for ( ; vIt != Values.end(); ++vIt)
+{ }
+
+}
+
--- tests/output/c/00413-align-typedef.c
+++ tests/output/c/00413-align-typedef.c
@@ -1,8 +1,8 @@
 
 typedef int                        MY_INT;
 typedef int *                      MY_INTP;
-typedef int (*foo_t)(void *bar);
-typedef int (*somefunc_t)(void *barstool);
+typedef int                        (*foo_t)(void *bar);
+typedef int                        (*somefunc_t)(void *barstool);
 typedef int                        int8_t __attribute__((__mode__(__QI__)));
 typedef int                        uint8_t;
 typedef struct _IDirectFBSurface   IDirectFBSurface;
