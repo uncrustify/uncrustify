2ef9d643469dcc6b9ad9bbd7cdb2399d90862740
Ben Gardner <gardner.ben@gmail.com>
2007-08-16 21:50:09 +0000

    add some tests for indent_paren_close
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@867 74908cc9-670d-0410-99d4-ca74d6b17880

---
+++ tests/output/c/00431-paren-indent.c
@@ -0,0 +1,35 @@
+static void *foo
+(
+        int param1,
+        char *param2
+);
+
+
+static void *foo
+(
+        int param1,
+        char *param2
+)
+{
+	for (
+	        int i = 0;
+	        i< 10;
+	        i++
+	    )
+	{
+		bar(
+		        arg1,
+		        arg2
+		   );
+		if ( ( abc < bcd )
+		     &&( 123 < abc )
+		   )
+		{
+			none(arg1,
+			     arg2,
+			     arg3
+			    );
+		}
+	}
+}
+
