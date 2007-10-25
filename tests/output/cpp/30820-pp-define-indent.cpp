69553b7bd23355f8d9c9aff35e543953351efa73
Ben Gardner <gardner.ben@gmail.com>
2007-10-25 00:54:29 +0000

    add pp_define_at_level test
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@976 74908cc9-670d-0410-99d4-ca74d6b17880

---
+++ tests/output/cpp/30820-pp-define-indent.cpp
@@ -0,0 +1,21 @@
+
+#define outpsize
+#define some(f)\
+    foo(f)
+
+class CRC
+{
+public:
+    int foo;
+// Initial CRC Start Value
+    #define 24 BITCRC ((ULONG) 0x00864CFB) // This line is not aligned with the other lines
+    char ch;
+    #define MULTI LINE DEFINE \
+        in column 0 \
+        that spans
+//// Operations ////
+public:
+    ...
+}
+
+
