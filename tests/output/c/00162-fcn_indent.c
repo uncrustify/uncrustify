dca92e881ec896df5c6134da5004120d5e728e32
Ben Gardner <gardner.ben@gmail.com>
2007-11-17 02:34:56 +0000

    add tests for new options
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@1024 74908cc9-670d-0410-99d4-ca74d6b17880

---
+++ tests/output/c/00162-fcn_indent.c
@@ -0,0 +1,14 @@
+int this_is_a_function_proto(int a,
+                             char * b);
+
+int this_is_a_function_def(int a,
+	char * b)
+{
+	this_is_a_function_call(a,
+	                        b);
+
+	a = another_function_call(a,
+	                          b);
+
+}
+
