167a66b8724ba3ed107771803c5691b7f5826442
Ben Gardner <gardner.ben@gmail.com>
2008-04-25 19:03:07 +0000

    add some test code to see if a Objective C/C++ mix will work
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@1163 74908cc9-670d-0410-99d4-ca74d6b17880

--- src/uncrustify.cpp
+++ src/uncrustify.cpp
@@ -1178,24 +1178,25 @@ struct file_lang
 
 struct file_lang languages[] =
 {
-   { ".c",    "C",    LANG_C    },
-   { ".cpp",  "CPP",  LANG_CPP  },
-   { ".d",    "D",    LANG_D    },
-   { ".cs",   "CS",   LANG_CS   },
-   { ".vala", "VALA", LANG_VALA },
-   { ".java", "JAVA", LANG_JAVA },
-   { ".pawn", "PAWN", LANG_PAWN },
-   { ".p",    "",     LANG_PAWN },
-   { ".sma",  "",     LANG_PAWN },
-   { ".inl",  "",     LANG_PAWN },
-   { ".h",    "",     LANG_CPP  },
-   { ".cxx",  "",     LANG_CPP  },
-   { ".hpp",  "",     LANG_CPP  },
-   { ".hxx",  "",     LANG_CPP  },
-   { ".cc",   "",     LANG_CPP  },
-   { ".di",   "",     LANG_D    },
-   { ".m",    "OC",   LANG_OC   },
-   { ".sqc",  "",     LANG_C    }, // embedded SQL
+   { ".c",    "C",    LANG_C             },
+   { ".cpp",  "CPP",  LANG_CPP           },
+   { ".d",    "D",    LANG_D             },
+   { ".cs",   "CS",   LANG_CS            },
+   { ".vala", "VALA", LANG_VALA          },
+   { ".java", "JAVA", LANG_JAVA          },
+   { ".pawn", "PAWN", LANG_PAWN          },
+   { ".p",    "",     LANG_PAWN          },
+   { ".sma",  "",     LANG_PAWN          },
+   { ".inl",  "",     LANG_PAWN          },
+   { ".h",    "",     LANG_CPP           },
+   { ".cxx",  "",     LANG_CPP           },
+   { ".hpp",  "",     LANG_CPP           },
+   { ".hxx",  "",     LANG_CPP           },
+   { ".cc",   "",     LANG_CPP           },
+   { ".di",   "",     LANG_D             },
+   { ".m",    "OC",   LANG_OC            },
+   { ".mpp",  "OC+",  LANG_OC | LANG_CPP },
+   { ".sqc",  "",     LANG_C             }, // embedded SQL
 };
 
 /**
@@ -1265,6 +1266,16 @@ static const char *language_to_string(int lang)
 {
    int i;
 
+   /* Check for an exact match first */
+   for (i = 0; i < (int)ARRAY_SIZE(languages); i++)
+   {
+      if (languages[i].lang == lang)
+      {
+         return(languages[i].tag);
+      }
+   }
+
+   /* Check for the first set language bit */
    for (i = 0; i < (int)ARRAY_SIZE(languages); i++)
    {
       if ((languages[i].lang & lang) != 0)
