d3188c0ecb7f407611858c52ae00068322f42171
Ben Gardner <gardner.ben@gmail.com>
2008-06-06 17:45:57 +0000

    clean up test after fixing cmt_start stuff
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@1189 74908cc9-670d-0410-99d4-ca74d6b17880

--- tests/output/cpp/30105-av.h
+++ tests/output/cpp/30105-av.h
@@ -1,7 +1,7 @@
 /*
-    2) There seems to be a problem handling .h-files compared to .cpp-files.
-    The following problem only occurs in header-files, in source-files the
-    output is as desired.
+   2) There seems to be a problem handling .h-files compared to .cpp-files.
+   The following problem only occurs in header-files, in source-files the
+   output is as desired.
  */
 
 static inline void foo()
@@ -21,7 +21,7 @@ static inline void foo()
 
 
 /*
-    3) The spacing around pointer stars is not always maintained as desired.
+   3) The spacing around pointer stars is not always maintained as desired.
  */
 //BEFORE:
   Buffer<T>* buffer;
@@ -30,7 +30,7 @@ static inline void foo()
 
 
 /*
-    4) Inside of casts the types are not formatted as outside.
+   4) Inside of casts the types are not formatted as outside.
  */
 //BEFORE:
   T* t = dynamic_cast<T*>(obj);
@@ -38,8 +38,8 @@ static inline void foo()
   T* t = dynamic_cast<T*>(obj);
 
 /*
-    5) Inside some template-stuff the spacing goes weird. Multiple spaces
-    are inserted, although the configuration (should) say otherwise.
+   5) Inside some template-stuff the spacing goes weird. Multiple spaces
+   are inserted, although the configuration (should) say otherwise.
  */
 //BEFORE:
   for (std::map<Key, Value*>::iterator it = map.begin(); it != map.end(); it++) {
