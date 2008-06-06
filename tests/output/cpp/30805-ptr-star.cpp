2bca5d71d56c1cfdc9a0a7a19b0b45a35af6cadc
Ben Gardner <gardner.ben@gmail.com>
2008-06-06 19:13:13 +0000

    Add tests for sp_before_unnamed_byref and sp_before_unnamed_ptr_star
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@1193 74908cc9-670d-0410-99d4-ca74d6b17880

--- tests/output/cpp/30805-ptr-star.cpp
+++ tests/output/cpp/30805-ptr-star.cpp
@@ -3,9 +3,9 @@ int dx = m_ClipBox.GetWidth() * GetZoom();
 m_ClipBox.m_Pos.y = PaintClipBox.y * GetZoom();
 
 int* i;
-char*i;
+char* i;
 
 int MyFunc(std::string& s, char*) {
-char *c = const_cast<char*>(s.c_str());
+	char* c = const_cast<char*>(s.c_str());
 }
 
--- tests/output/cpp/30806-ptr-star.cpp
+++ tests/output/cpp/30806-ptr-star.cpp
@@ -2,10 +2,10 @@ int dx = m_ClipBox.GetWidth() * GetZoom();
 
 m_ClipBox.m_Pos.y = PaintClipBox.y * GetZoom();
 
-int* i;
-char*i;
+int *i;
+char *i;
 
-int MyFunc(std::string& s, char*) {
-char *c = const_cast<char*>(s.c_str());
+int MyFunc(std::string &s, char *) {
+	char *c = const_cast<char *>(s.c_str());
 }
 
