bd65c7662399b84db1e4d722758c65814b816b81
Ben Gardner <gardner.ben@gmail.com>
2008-07-17 23:37:09 +0000

    improve comment right-align test
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@1289 74908cc9-670d-0410-99d4-ca74d6b17880

---
+++ tests/input/cpp/cmt_right.cpp
@@ -0,0 +1,26 @@
+class X
+{
+    struct Zone
+    {
+        //    int a;
+        //    int b;
+        int c;
+        int d;
+        double e;
+        inline Zone(int _c) : c(_c)
+        { }                                     // constructor for zone search
+
+        inline Zone(
+            //int _a,
+            //int _b,
+            int _c,
+            int _d, double _e) :
+            //a(_a),
+            //b(_b),
+            c(_c),
+            d(_d),
+            e(_e)
+        { }
+    };
+};
+
