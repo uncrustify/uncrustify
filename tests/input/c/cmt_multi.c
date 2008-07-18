0c9aa3df71887679d1b1208414aafd34d069d24c
Ben Gardner <gardner.ben@gmail.com>
2008-07-18 00:04:13 +0000

    Add tests for cmt_multi_check_last
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@1291 74908cc9-670d-0410-99d4-ca74d6b17880

---
+++ tests/input/c/cmt_multi.c
@@ -0,0 +1,5 @@
+/************************************************
+ * This is a multiline comment.
+ * The first and last lines are the same length
+ ************************************************/
+
