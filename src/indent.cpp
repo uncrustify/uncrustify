14a1e348b50e372df2fb807015d8583b46c4b87c
Ben Gardner <gardner.ben@gmail.com>
2008-08-16 18:24:03 +0000

    Indent comments before 'case' at the same level as 'case'
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@1331 74908cc9-670d-0410-99d4-ca74d6b17880

--- src/indent.cpp
+++ src/indent.cpp
@@ -862,6 +862,22 @@ void indent_text(void)
 
          /* Always set on case statements */
          indent_column_set(frm.pse[frm.pse_tos].indent_tmp);
+
+         {
+            chunk_t *pct = pc;
+
+            while (((pct = chunk_get_prev_nnl(pct)) != NULL) &&
+                   chunk_is_comment(pct))
+            {
+               chunk_t *t2 = chunk_get_prev(pct);
+               if (chunk_is_newline(t2))
+               {
+                  pct->column = frm.pse[frm.pse_tos].indent_tmp;
+                  pct->column_indent = pct->column;
+                  //LOG_FMT(LSYS, "Outdent '%.*s' to %d\n", pct->len, pct->str, pct->column);
+               }
+            }
+         }
       }
       else if (pc->type == CT_BREAK)
       {
