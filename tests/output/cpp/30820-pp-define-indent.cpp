cae754eecec643a815a794610ebce95e04e4978d
Ben Gardner <gardner.ben@gmail.com>
2008-08-19 01:33:02 +0000

    update test for the tokenizer changes
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@1344 74908cc9-670d-0410-99d4-ca74d6b17880

--- tests/output/cpp/30820-pp-define-indent.cpp
+++ tests/output/cpp/30820-pp-define-indent.cpp
@@ -8,7 +8,7 @@ class CRC
 public:
     int foo;
 // Initial CRC Start Value
-    #define 24 BITCRC ((ULONG) 0x00864CFB) // This line is not aligned with the other lines
+    #define 24BITCRC ((ULONG) 0x00864CFB) // This line is not aligned with the other lines
     char ch;
     #define MULTI LINE DEFINE \
         in column 0 \
