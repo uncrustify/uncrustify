326ba4c7ced9e419189e0432701b17b72252b5f0
Ben Gardner <gardner.ben@gmail.com>
2008-10-25 15:53:06 +0000

    already had INDENT-ON stuff
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@1399 74908cc9-670d-0410-99d4-ca74d6b17880

--- tests/output/c/00170-beautifier-off.c
+++ tests/output/c/00170-beautifier-off.c
@@ -2,19 +2,19 @@
 
 #define FOO(bar) create_a_really_long_identifier name(some_function(bar1 + bar2), bar3, bar4);
 
-/*BEAUTIFIER-OFF*/
+/* *INDENT-OFF* */
    int foo[] = {
       1,   3,   5,
       3,   5,   7,
       5,   7,   9,
    };
-/*BEAUTIFIER-ON*/
+/* *INDENT-ON* */
 
 #define multilinemacro do { (x+5); } while (0); \
         printf("a multilinemacro"); \
         printf("a multilinemacro2");
 
-/*BEAUTIFIER-OFF*/
+/* *INDENT-OFF* */
 
 int main(int argc, char *argv[])
 {
@@ -22,11 +22,11 @@ int main(int argc, char *argv[])
 a = 1; // stupid comment \
 b = 2;
 
-/*BEAUTIFIER-ON*/
+/* *INDENT-ON* */
 return(a+b);
 }
 
-/*BEAUTIFIER-OFF*/
+/* *INDENT-OFF* */
 int a;
-/*BEAUTIFIER-ON*/
+/* *INDENT-ON* */
 
