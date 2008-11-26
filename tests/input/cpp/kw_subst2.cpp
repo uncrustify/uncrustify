307b691d32c48dcd4d437c5f55934dc7009481b7
Ben Gardner <gardner.ben@gmail.com>
2008-11-26 20:39:19 +0000

    expand kw subst test
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@1441 74908cc9-670d-0410-99d4-ca74d6b17880

---
+++ tests/input/cpp/kw_subst2.cpp
@@ -0,0 +1,28 @@
+#include <string>
+
+namespace bar
+{
+
+class CFoo
+{
+int foo1(int arg);
+int foo2();
+};
+
+int CFoo::foo1(int arg, char arg2)
+{
+}
+
+int CFoo::foo2()
+{
+}
+
+int CFoo::operator +()
+{
+}
+
+int some_func(void)
+{
+}
+
+}
---
+++ tests/output/cpp/30831-kw_subst2.cpp
@@ -0,0 +1,60 @@
+/**
+ * @file kw_subst2.cpp
+ * Description
+ *
+ * $Id$
+ */
+#include <string>
+
+namespace bar
+{
+
+	/**
+	 * CLASS:  CFoo
+	 * TODO: DESCRIPTION
+	 */
+	class CFoo
+	{
+	int foo1(int arg);
+	int foo2();
+	};
+
+	/**
+	 * foo1
+	 * TODO: DESCRIPTION
+	 * @param arg TODO
+	 * @param arg2 TODO
+	 * @return TODO
+	 */
+	int CFoo::foo1(int arg, char arg2)
+	{
+	}
+
+	/**
+	 * foo2
+	 * TODO: DESCRIPTION
+	 * @return TODO
+	 */
+	int CFoo::foo2()
+	{
+	}
+
+	/**
+	 * operator +
+	 * TODO: DESCRIPTION
+	 * @return TODO
+	 */
+	int CFoo::operator +()
+	{
+	}
+
+	/**
+	 * some_func
+	 * TODO: DESCRIPTION
+	 * @return TODO
+	 */
+	int some_func(void)
+	{
+	}
+
+}
