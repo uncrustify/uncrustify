daa78f35457a7723276b2493daa6a706dd778104
Ben Gardner <gardner.ben@gmail.com>
2009-01-28 02:06:23 +0000

    add tests for nl_func_type_name with operators
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@1473 74908cc9-670d-0410-99d4-ca74d6b17880

---
+++ tests/input/cpp/nl_func_type_name.cpp
@@ -0,0 +1,24 @@
+
+//zero
+// one
+//  two
+//   three
+void foo(void);
+
+struct A
+{
+public:
+long_complicated_type f();
+A& operator+(const A& other);
+};
+
+A& A::operator+(const A& other)
+{}
+
+B
+operator+(const B& other)
+{}
+
+B foo(const B& other)
+{}
+
