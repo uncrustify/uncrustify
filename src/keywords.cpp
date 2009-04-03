c02b6a35a8a383e9414d0f9c62e0c88c38bc7c61
Ben Gardner <gardner.ben@gmail.com>
2009-04-03 16:34:24 +0000

    Handle the ObjC '@protocol' the same as '@interface'
    git-svn-id: https://uncrustify.svn.sourceforge.net/svnroot/uncrustify/trunk@1528 74908cc9-670d-0410-99d4-ca74d6b17880

--- src/keywords.cpp
+++ src/keywords.cpp
@@ -39,6 +39,7 @@ static const chunk_tag_t keywords[] =
    { "@interface",       CT_OC_INTF,      LANG_OC | LANG_CPP | LANG_C                                     },
    { "@private",         CT_PRIVATE,      LANG_OC | LANG_CPP | LANG_C                                     },
    { "@property",        CT_OC_PROPERTY,  LANG_OC | LANG_CPP | LANG_C                                     },
+   { "@protocol",        CT_OC_INTF,      LANG_OC | LANG_CPP | LANG_C                                     },
    { "@synthesize",      CT_OC_DYNAMIC,   LANG_OC | LANG_CPP | LANG_C                                     },
    { "_Bool",            CT_TYPE,         LANG_CPP                                                        },
    { "_Complex",         CT_TYPE,         LANG_CPP                                                        },
