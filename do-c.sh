0eef7440ba20893e8b895bacda426dd756fa2fdc
Ben Gardner <ben@ben-desktop.(none)>
2009-11-05 17:42:01 -0600

    Remove unused files

--- do-c.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-src/uncrustify -c etc/ben.cfg -p parsed.txt -f $1 $2 $3 $4  > out.c 2> out.err.txt 
--- do-d.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-src/uncrustify -c etc/d.cfg -f $1 -p parsed.txt > out.d 2> out.err.txt
