9855ec9cfd5b39daa35f389892cebcd48dd48452
Ben Gardner <gardner.ben@gmail.com>
2009-11-19 20:08:54 -0600

    Update test

---
+++ tests/input/c/br_cmt.c
@@ -0,0 +1,14 @@
+int main()
+{
+	if( foo == bar )
+	{ /* this works */
+                a;
+	}else
+    if( ranz != bar )
+	{ /* this works too */
+                b;
+	}else
+	{ /* this is broken */
+        c;
+	}
+}
