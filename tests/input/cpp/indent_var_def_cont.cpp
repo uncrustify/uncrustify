e63d84a58f7f4f7e13553e5c49d011a5e2866d6f
Ben Gardner <gardner.ben@gmail.com>
2009-12-29 13:42:17 -0600

    Add tests for indent_var_def_cont

---
+++ tests/input/cpp/indent_var_def_cont.cpp
@@ -0,0 +1,16 @@
+int 
+a, b, c;
+
+int d, 
+e, f;
+
+void bar(void)
+{
+   struct foobar
+a = { 'x', 0 };
+   struct foobar
+b = { 'y', 2 },
+c = { 'z', 4 };
+   struct foobar d = { 'y', 2 },
+e = { 'z', 4 };
+}
