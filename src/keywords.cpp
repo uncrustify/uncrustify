e63fe8b359ac2efa1e015c87ae4eb079de7071fa
Ben Gardner <gardner.ben@gmail.com>
2010-08-13 09:00:40 -0500

    'foreach' isn't a C++ keyword

--- src/keywords.cpp
+++ src/keywords.cpp
@@ -128,7 +128,7 @@ static const chunk_tag_t keywords[] =
    { "flags",            CT_TYPE,         LANG_VALA                                                                   },
    { "float",            CT_TYPE,         LANG_ALLC                                                                   },
    { "for",              CT_FOR,          LANG_ALL                                                                    }, // PAWN
-   { "foreach",          CT_FOR,          LANG_CPP | LANG_CS | LANG_D | LANG_VALA                                     },
+   { "foreach",          CT_FOR,          LANG_CS | LANG_D | LANG_VALA                                                },
    { "foreach_reverse",  CT_FOR,          LANG_D                                                                      },
    { "forward",          CT_FORWARD,      LANG_PAWN                                                                   }, // PAWN
    { "friend",           CT_FRIEND,       LANG_CPP                                                                    },
