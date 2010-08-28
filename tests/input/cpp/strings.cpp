247a8053952291032b134b8ef2d35f94e4707d8c
Ben Gardner <gardner.ben@gmail.com>
2010-08-28 17:58:14 -0500

    Update test for C++0x R strings with embedded newlines.

--- tests/input/cpp/strings.cpp
+++ tests/input/cpp/strings.cpp
@@ -7,18 +7,27 @@ void foo()
 
 
 /* The 'u8', 'u', and 'U' prefixes */
-const char     s1 = u8"I'm a UTF-8 string.";
-const char16_t s2 = u"This is a UTF-16 string.";
-const char32_t s3 = U"This is a UTF-32 string.";
+const char     *s1 = u8"I'm a UTF-8 string.";
+const char16_t *s2 = u"This is a UTF-16 string.";
+const char32_t *s3 = U"This is a UTF-32 string.";
 
 /* The 'R' and 'R"delim(' prefixes */
-const char     r1 = R"(Xhe String Data \ Stuff " )";
-const char     r2 = R"delimiter(The String Data \ Stuff ")delimiter";
+const char    *r1 = R"(Xhe String Data \ Stuff " )";
+const char    *r2 = R"delimiter(The String Data \ Stuff ")delimiter";
+
+/* Multiline string */
+auto foo = R"FOO"(
+ some
+ text
+ and
+ more
+ text
+)FOO"";
 
 /* Combo */
-const char     c1 = u8R"XXX(I'm a "raw UTF-8" string.)XXX";
-const char16_t c2 = uR"*(This is a "raw UTF-16" string.)*";
-const char32_t c3 = UR"(This is a "raw UTF-32" string.)";
+const char     *c1 = u8R"XXX(I'm a "raw UTF-8" string.)XXX";
+const char16_t *c2 = uR"*(This is a "raw UTF-16" string.)*";
+const char32_t *c3 = UR"(This is a "raw UTF-32" string.)";
 
 /* user-defined */
 OutputType operator "" _Suffix(unsigned long long);
