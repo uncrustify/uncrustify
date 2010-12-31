a6f728ec83a5798f65566cf70f67b0eea44e2050
Ben Gardner <gardner.ben@gmail.com>
2010-12-31 14:52:23 -0600

    Add tests for sp_catch_paren, sp_throw_paren, sp_version_paren and sp_scope_paren.

---
+++ tests/input/d/d_sp_paren.d
@@ -0,0 +1,23 @@
+version(unittest) {
+import foo;
+}
+
+void main() {
+scope(exit) {
+foo();
+}
+
+scope(success) suckit();
+
+scope f = new Foo();
+if(foo)
+{
+}
+
+try {
+throw(e);
+}
+catch(Exception e) {
+}
+}
+
