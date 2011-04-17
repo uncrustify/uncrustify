837ccbf4713428383857453fcd1ef1831dfb2036
Ben Gardner <gardner.ben@gmail.com>
2011-04-16 22:38:18 -0500

    Add test for D sized enums

---
+++ tests/input/d/enum.d
@@ -0,0 +1,6 @@
+/* sized enum like in C# */
+enum Metrics : int {
+SM_CXSCREEN = 0,
+SM_CYSCREEN,
+SM_CXVSCROLL,
+}
---
+++ tests/output/d/40035-enum.d
@@ -0,0 +1,7 @@
+/* sized enum like in C# */
+enum Metrics : int
+{
+    SM_CXSCREEN = 0,
+    SM_CYSCREEN,
+    SM_CXVSCROLL,
+}
