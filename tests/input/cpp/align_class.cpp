5b8cf13145ed0e104d6add03a338b8c80b1f6c1c
Ben Gardner <gardner.ben@gmail.com>
2011-04-20 21:50:19 -0500

    Add a test for aligning static class variables

---
+++ tests/input/cpp/align_class.cpp
@@ -0,0 +1,14 @@
+//---------------------------------------------------------------------------
+//    Statics                                                               |
+//---------------------------------------------------------------------------
+void CTdrFile::SetDistanceMode( TDistMode dm )   {   CTdrFile::ms_DistMode = dm;         }
+TDistMode CTdrFile::GetDistanceMode( void )           {   return CTdrFile::ms_DistMode;      }
+String CTdrFile::GetDistanceModeUnits( void )      {   return ( CTdrFile::GetDistanceMode() == dmKM ) ? "km" : "Miles";   }
+void CTdrFile::SetBSTCompensation( bool bUseBST ){   ms_bCompBST = bUseBST;              }
+void CTdrFile::SetFactoryMode( bool bFactory )   {   ms_bFactory = bFactory;             }
+bool CTdrFile::GetFactoryMode( void )            {   return ms_bFactory;                 }
+
+unsigned int CAgentCharacter::iReferenceCount = 0;
+IAgentEx* CAgentCharacter::pAgentEx = NULL;
+CAgentNotifySink* CAgentCharacter::pSink = NULL;
+
