4efe8c86ce16fc9fbfb00ab18725c3063e75dcfc
Ben Gardner <gardner.ben@gmail.com>
2011-06-11 16:18:37 -0500

    Add a test for the Java assert statement.

---
+++ tests/input/java/assert.java
@@ -0,0 +1,9 @@
+public class JavaClass {
+
+    public static void main(String[] args) {
+	/* assert EXPRESSION1 ; */
+	assert ( a != null ) && ( b != null );
+	/* assert EXPRESSION1 : EXPRESSION2 ; */
+        assert ( a != null ) && ( b != null ) : "Message";
+    }
+}
