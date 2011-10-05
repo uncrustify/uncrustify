8efb7ea7ff46a7b6196f9233541bb63a1a9d3142
Ben Gardner <gardner.ben@gmail.com>
2011-10-04 21:37:04 -0500

    Save off changes for version 0.59

--- src/config.h
+++ src/config.h
@@ -68,7 +68,7 @@
 #define PACKAGE_NAME "uncrustify"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "uncrustify 0.58"
+#define PACKAGE_STRING "uncrustify 0.59"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "uncrustify"
@@ -77,13 +77,13 @@
 #define PACKAGE_URL "http://uncrustify.sourceforge.net/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.58"
+#define PACKAGE_VERSION "0.59"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.58"
+#define VERSION "0.59"
 
 /* Define to appropriate substitue if compiler doesnt have __func__ */
 /* #undef __func__ */
