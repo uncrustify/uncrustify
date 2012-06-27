1712cbe29a4afd2b00ec4f4a5b1698a080be8215
Ben Gardner <gardner.ben@gmail.com>
2012-06-26 22:02:03 -0500

    Expand the ObjC boxed test

--- tests/input/oc/box.m
+++ tests/input/oc/box.m
@@ -3,3 +3,10 @@ NSNumber *test = @ (42 * 2);
 NSNumber *test = @4.0;
 NSDictionary *test = @{@"foo":@"bar"};
 
+@implementation UrlTemplateTest
+- (void)test {
+       NSString *test = @"";
+       NSString *string = [[NSMutableString alloc] initWithString:@""];
+       STAssertEqualObjects(string, @"", nil);
+}
+@end
--- tests/output/oc/50095-box.m
+++ tests/output/oc/50095-box.m
@@ -5,3 +5,12 @@ NSDictionary *test = @{
    @"foo":@"bar"
 };
 
+@implementation UrlTemplateTest
+-(void)test
+{
+   NSString *test   = @"";
+   NSString *string = [[NSMutableString alloc] initWithString: @""];
+
+   STAssertEqualObjects(string, @"", nil);
+}
+@end
