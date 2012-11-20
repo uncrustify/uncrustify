0375f45fca8fef84ea1b4f764a2fff246aef707f
Ben Gardner <ben@ben-MS-7637.(none)>
2012-11-19 21:32:19 -0600

    Update obj-c.cfg with new option: sp_after_oc_msg_receiver Add a test that uses it.

---
+++ tests/input/oc/receiver.m
@@ -0,0 +1,10 @@
+#import <Cocoa/Cocoa.h>
+
+@implementation MyDocument
+
+- (NSString*) appPath {
+  [ AClass         AFunc ];
+  return [NSSearchPathForDirectoriesInDomains (  NSDocumentDirectory,       NSUserDomainMask, YES    )             lastObject];
+}
+
+@end
