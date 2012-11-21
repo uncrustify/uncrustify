dd9adc5775e7a56cd2c0c39220f246fbe449fa14
Ben Gardner <ben@ben-MS-7637.(none)>
2012-11-20 22:36:44 -0600

    Add a test for negative values of indent_continue

---
+++ tests/output/cpp/30090-bug_488.cpp
@@ -0,0 +1,12 @@
+void baz()
+{
+        foobar= bar[a + b + (c
+            + d)];
+
+        foobar = bar(a + b + (c +
+            +d));
+
+        foo = bar[a] + b + qux(c +
+            +d);
+}
+
