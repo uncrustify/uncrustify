0edf827adcb8b509b58607faacecec906ed0980b
Ben Gardner <ben@ben-MS-7637.(none)>
2012-12-29 15:51:59 -0600

    And and update tests for new ObjC options

---
+++ tests/input/oc/msg_align.m
@@ -0,0 +1,20 @@
+
+BOOL immediatlyReady = [self ensureResource:mutableResources[0]
+                    existsInDirectoryAtPath:mutablePaths[0]
+                                  queueMode:mode
+                          completionHandler:completionHandler
+                               errorHandler:errorHandler];
+
+[myObject doFooWith1:arg1 name1:arg2  // some lines with >1 arg
+              error1:arg3];
+
+[myObject doFooWith2:arg4
+               name2:arg5 error2:arg6];
+
+[myObject doFooWith3:arg7
+          name3:arg8  // aligning keywords instead of colons
+          error3:arg9];
+
+[myObject doithereguysA:argA
+ reallylongargname:argB another:argC];
+
--- tests/output/oc/50007-misc.m
+++ tests/output/oc/50007-misc.m
@@ -4,8 +4,11 @@
    GLfloat cc[3][3] = { { 0.0, 0.0, 0.6 }, { 0.3, 0.1, 0.5 }, { 0.0, 0.0, 0.5 }, };
    GLfloat sc[3] = { 0.75, 0.75, 0.75 };
 
-   return [self initWithWarmColors: (float *)&wc coolColors: (float *)&cc
-                      surfaceColor: sc enableTexturing: NO enableSpecular: YES
+   return [self initWithWarmColors: (float *)&wc
+                        coolColors: (float *)&cc
+                      surfaceColor: sc
+                   enableTexturing: NO
+                    enableSpecular: YES
               enableQuakeDisruptor: NO];
 
    [NSException raise: NSInternalInconsistency
