cb9853b4cb653fccb8dee0321e3117bad9692b41
Ben Gardner <ben@ben-MS-7637.(none)>
2013-05-25 16:57:21 -0500

    Add test for SF#550

---
+++ tests/input/cpp/enum_shr.cpp
@@ -0,0 +1,7 @@
+enum MyEnum {
+    kOne   = 0,
+    kTwo   = 1 << 0,
+    kThree = 1 << 1,
+    kFour  = 1 << 2
+};
+
---
+++ tests/output/cpp/30300-enum_shr.cpp
@@ -0,0 +1,8 @@
+enum MyEnum
+{
+   kOne   = 0,
+   kTwo   = 1 << 0,
+   kThree = 1 << 1,
+   kFour  = 1 << 2
+};
+
