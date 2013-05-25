0288131faf20267b174f44ecf87c0ea426940d3c
Ben Gardner <ben@ben-MS-7637.(none)>
2013-05-25 17:24:21 -0500

    Add test case for SF#548

---
+++ tests/input/cpp/enum_class.h
@@ -0,0 +1,15 @@
+enum class A
+{
+a,
+b
+}
+
+class B {
+private:
+   int x;
+}
+enum C
+{
+a,
+b
+}
---
+++ tests/output/cpp/30301-enum_class.h
@@ -0,0 +1,17 @@
+enum class A
+{
+    a,
+    b
+
+}
+
+class B {
+private:
+int x;
+}
+enum C
+{
+    a,
+    b
+
+}
