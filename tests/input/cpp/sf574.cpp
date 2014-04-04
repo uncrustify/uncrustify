4ddf2df6dbc480568f3d3a307eb29c5ae2361643
Ben Gardner <gardner.ben@gmail.com>
2014-04-03 22:00:26 -0500

    Add test case for SF#574

---
+++ tests/input/cpp/sf574.cpp
@@ -0,0 +1,13 @@
+
+class A : public B
+{
+A& operator=(const A& other){
+if (this == &other)return *this;
+B::operator=(other);
+if (this == &other)return *this;
+B::opera(other);
+copy(other);
+return *this;
+}
+
+};
