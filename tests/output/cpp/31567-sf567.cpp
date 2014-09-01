7f3296723c6f97adc7164c1fd3c922b1bd18a35c
Ben Gardner <gardner.ben@gmail.com>
2014-09-01 15:07:23 -0500

    Update tests for new align_same_func_call_params functionality

---
+++ tests/output/cpp/31567-sf567.cpp
@@ -0,0 +1,16 @@
+package com.temp.test;
+
+public class Database
+{
+private Database(String fileName)
+{
+	readConfig(fileName, "asdfasdf", 1);
+	readConfig(ame,      "aasdf",    1);
+
+	Database::readConfig(fileName, "asdfasdf", 1);
+	Database::readConfig(ame,      "aasdf",    1);
+
+	::readConfig(fileName, "asdfasdf", 1);
+	::readConfig(ame,      "aasdf",    1);
+}
+}
