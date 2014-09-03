4120cd976bacfe11189795138138030acf65d03d
Ben Gardner <gardner.ben@gmail.com>
2014-09-03 11:14:37 -0500

    Add tests for nl_paren_dbrace_open

--- tests/input/java/double_brace.java
+++ tests/input/java/double_brace.java
@@ -4,3 +4,18 @@ member.func(new List() {{
         "Hello");
 }});
 
+
+private static final Set<String> VALID_CODES = new HashSet<String>() {{
+	add("XZ13s");
+	add("AB21/X");
+	add("YYLEX");
+	add("AR2D");
+ }};
+
+add(new JPanel() {{
+	setLayout(...);
+	setBorder(...);
+	add(new JLabel(...));
+	add(new JSpinner(...));
+ }});
+
