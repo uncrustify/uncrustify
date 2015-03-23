ead319cab7b2012ccb0e8d7880fe7a18f5d73505
MeXx <mexx@devsub.net>
2015-03-23 22:33:45 +0100

    ObjC: Add testcase to reproduce ObjC methodcall-in-ternary-expression issue #355

--- tests/input/oc/ternary.m
+++ tests/input/oc/ternary.m
@@ -1,2 +1,3 @@
 NSString *str = (otherString ?: @"this is the placeholder");
 NSString *str2 = (str ? otherString : @"this is the other placeholder");
+NSString *str3 = str ? [[NSString alloc] initWithString:str] : @"this is the third placeholder";
