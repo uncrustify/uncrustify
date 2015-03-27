df4eb9165ea8b4296bdb0b9b338de5759206f650
Guy Maurel <guy.maurel@kde.org>
2015-03-27 17:26:37 +0100

    detect Version of Python 2 or 3

---
+++ detect_Python_2_3.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+#
+import sys
+if (sys.version_info > (3, 0)):
+    print("Python 3 is detected. Cannot be used.")
+    print("Please install Python 2 before running again.")
+    sys.exit(-1)
