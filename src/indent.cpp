f86ef174cbc7e87c4de367cd42b388f70bc68107
Scott Bilas <scobi@unity3d.com>
2015-04-20 16:14:18 +0200

    [Unity-only hack] Fix issue with multiline defines getting reindented (we're re-tagging 'define' as PP_OTHER to avoid other complex parse issues).

--- src/indent.cpp
+++ src/indent.cpp
@@ -1834,6 +1834,10 @@ void indent_text(void)
                     __func__, pc->orig_line, indent_column, pc->str.c_str());
             reindent_line(pc, indent_column);
          }
+         else if (pc->type == CT_PREPROC_BODY)
+         {
+            /* [Unity-only hack] Do nothing. We're using 'define' as CT_PP_OTHER to avoid reformatting of complex and weird macros. Likewise leave indentation alone because probably also weird. */
+         }
          else
          {
             if (pc->column != indent_column)
