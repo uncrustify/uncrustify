3788ca58f8e92ec50cf1f35178dd1197aa1d8b6c
B. Kevin Hardman <nivek.research@gmail.com>
2016-05-19 10:15:48 -0400

    a dereference after in keyword (e.g., *) in Objective-C for loop should not honor sp_before_ptr_star or sp_after_ptr_star settings and should be treated as sp_deref

--- src/space.cpp
+++ src/space.cpp
@@ -118,6 +118,7 @@ static argval_t do_space(chunk_t *first, chunk_t *second, int& min_sp, bool comp
    int      idx;
    argval_t arg;
    chunk_t  *next;
+   chunk_t  *prev;
 
    min_sp = 1;
 
@@ -1377,6 +1378,11 @@ static argval_t do_space(chunk_t *first, chunk_t *second, int& min_sp, bool comp
 
    if ((first->type == CT_PTR_TYPE) && CharTable::IsKw1(second->str[0]))
    {
+      prev = chunk_get_prev(first);
+      if ((prev != NULL) && (prev->type == CT_IN)) {
+         log_rule("sp_deref");
+         return(cpd.settings[UO_sp_deref].a);
+      }
       if ((second->type == CT_QUALIFIER) &&
           (cpd.settings[UO_sp_after_ptr_star_qualifier].a != AV_IGNORE))
       {
@@ -1390,7 +1396,8 @@ static argval_t do_space(chunk_t *first, chunk_t *second, int& min_sp, bool comp
       }
    }
 
-   if (second->type == CT_PTR_TYPE)
+   if ((second->type == CT_PTR_TYPE) &&
+       (first->type != CT_IN))
    {
       if (cpd.settings[UO_sp_before_ptr_star_func].a != AV_IGNORE)
       {
