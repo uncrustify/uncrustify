8058221d88a781f310e7799ddd775bbb10618830
Mihai Popescu <mihai@unity3d.com>
2016-07-02 13:29:26 +0200

    Collapsed revision 'staging-test-suite' * Deactivating failing staging tests * upstream-master doesn't know about 'warn_level_tabs_found_in_verbatim_string_literals' * Option indent_acces_spec was turned off accidentally updating option comments. * Corrected oneline_property test. Actually the problem was that the provided generated C# code was bad as it had the visibility modifiers (public/private/internal) after the class/struct/enum keywords which is illegal in C#. * 'dont-detab-strings' test was failing with 'Unmatched BRACE_OPEN' because *INDENT-ON* was missing.

--- tests/output/staging/10027-oneline_property.cs
+++ tests/output/staging/10027-oneline_property.cs
@@ -1,7 +1,7 @@
 public class Class
 {
-    public float prop
-    {
-        get; set;
-    }
+    public float prop { get; set; }
+    public float prop { get { return m; } }
+    public float prop { set { m = value; } }
+    public float prop { get { return m; } set { m = value; } }
 };
