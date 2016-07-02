ab123481936dfefc6a86aee716ac7c40d8f19ce5
Mihai Popescu <mihai@unity3d.com>
2016-07-02 13:29:26 +0200

    'dont-detab-strings' test was failing with 'Unmatched BRACE_OPEN' because *INDENT-ON* was missing.

--- tests/input/staging/dont-detab-strings.cpp
+++ tests/input/staging/dont-detab-strings.cpp
@@ -2,4 +2,5 @@ void f() {
 	 			auto x = "	test\t 	 	 		...   ???";
 // *INDENT-OFF*
 	 			auto x = "	test\t 	 	 		...   ???";
+// *INDENT-ON*
 }
--- tests/output/staging/10006-dont-detab-strings.cpp
+++ tests/output/staging/10006-dont-detab-strings.cpp
@@ -2,4 +2,5 @@ void f() {
 	auto x = "\ttest\t \t \t \t\t...   ???";
 // *INDENT-OFF*
 	 			auto x = "	test\t 	 	 		...   ???";
+// *INDENT-ON*
 }
