c602899c8e87a165610589b2ede2e2208ea7b0dd
Mihai Popescu <mihai@unity3d.com>
2016-07-07 00:01:47 +0200

    Add new failing cases that needs to be fixed.

---
+++ tests/input/staging/nl-brace.cs
@@ -0,0 +1,16 @@
+public class Class : Base<int> { public Class() : base(Value) { } }
+public class Foo
+{
+	public void foo()
+	{
+		// nl_brace_catch=force
+		try { return 1; } catch (Exception) {}
+
+		// nl_brace_finally=force
+		try { bar(); } finally { barr(); }
+
+		var v = foo(yolo,
+			new List<Type>() {
+				new Type { Value = prop; }});
+	}
+}
