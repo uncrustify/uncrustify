ca982b9df3a32ff732728606c954244a6e19a71c
Mihai Popescu <mihai@unity3d.com>
2016-07-21 23:56:08 +0200

    Support for hg if the github repository is pulled using hg git extension

--- make_version.py
+++ make_version.py
@@ -9,14 +9,24 @@ import subprocess
 def main (argv):
     root = os.path.abspath(os.path.dirname(__file__))
     git_path = os.path.join(root, '.git')
+    hg_path = os.path.join(root, '.hg')
 
-    if not os.path.exists(git_path):
-        return
-
-    try:
-        txt = subprocess.check_output(['git', 'describe', '--long', '--always', '--dirty']).strip()
-    except:
-        return
+    if os.path.exists(git_path):
+        try:
+            txt = subprocess.check_output(['git', 'describe', '--long', '--always', '--dirty']).strip()
+        except:
+            print "Failed to retrieve version from git"
+            return 1
+    elif os.path.exists(hg_path):
+        try:
+            branch = subprocess.check_output(['hg', 'log', '-r', '.', '--template', '{latesttag}']).strip()
+            txt = subprocess.check_output(['git', '--git-dir=.hg/git', 'describe', '--long', '--always', branch]).strip()
+        except:
+            print "Failed to retrieve version from hg"
+            return 1
+    else:
+        print "Unknown version control system."
+        return 1
 
     # convert the git describe text to a version
     pts = txt.split('-', 2)
@@ -43,6 +53,7 @@ def main (argv):
                 line = '%s,%s,%s' % (pts[0], tag_vers, pts[2])
             fh.write(line)
             fh.write('\n')
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
