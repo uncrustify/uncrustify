da48eea466776ed3bc49b9d61d760a64fc05054f
Mihai Popescu <mihai@unity3d.com>
2016-08-17 13:26:33 +0200

    adding test for issue 633

---
+++ tests/output/cpp/32003-issue_633_typename.cpp
@@ -0,0 +1,5 @@
+char                       m_char;
+typename TImage::PixelType m_Label;
+NonPODType                 m_var;
+int                        m_Axis;
+bool                       m_Heuristic;
