4ccd95c7fbebdbce99cd18f52dd97f0ea5cb3ee3
Guy Maurel <guy.j@maurel.de>
2016-09-19 19:26:40 +0200

    new option nl_before_class The number of newlines before a class definition

---
+++ tests/output/cpp/34001-nl_before_after.h
@@ -0,0 +1,17 @@
+namespace A {
+namespace S {
+
+class C
+{
+public:
+    virtual ~C()
+    {
+    }
+
+    virtual void addSearch(const int &col) = 0;
+
+    virtual void removeSearch(int id) = 0;
+};
+
+} // namespace S
+} // namespace A
