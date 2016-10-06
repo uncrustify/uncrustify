f91eb3626f3d6cde5cdda31644b573c3b5a39479
Guy Maurel <guy.j@maurel.de>
2016-10-06 15:22:56 +0200

    Add the uigui file for the use of universalindentgui

--- scripts/update-defaults.sh
+++ scripts/update-defaults.sh
@@ -2,4 +2,5 @@
 #
 ./src/uncrustify -c /dev/null --update-config-with-doc > etc/defaults.cfg
 cp etc/defaults.cfg documentation/htdocs/default.cfg
-src/uncrustify --show-config > documentation/htdocs/config.txt
+./src/uncrustify --show-config > documentation/htdocs/config.txt
+./src/uncrustify --universalindent > etc/uigui_uncrustify.ini
