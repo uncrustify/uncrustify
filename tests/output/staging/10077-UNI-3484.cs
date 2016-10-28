577fd0294c640c9e0abf1a84afa11bf9a5e8d3a7
Scott Bilas <scobi@unity3d.com>
2016-10-28 08:35:48 +0200

    new tests showing bug lucas found that has something to do with wrapping

--- tests/output/staging/10077-UNI-3484.cs
+++ tests/output/staging/10077-UNI-3484.cs
@@ -1,7 +1,12 @@
 // all the forms of strings
 contents.Append("#include \"{file.GetBoundPath ()}\"");
 contents.Append($"#include \"{file.GetBoundPath ()}\"");
+contents.Append(
+    $"#include \"{file.GetBoundPath ()}\"");
 contents.Append($@"#include
+""{file.GetBoundPath ()}""");
+contents.Append(
+    $@"#include
 ""{file.GetBoundPath ()}""");
 contents.Append(@"#include
 ""{file.GetBoundPath ()}""");
