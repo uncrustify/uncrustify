e9b9f77e02c166a8733f4131256cea045723abcf
Daniel Chumak <chumak.daniel.dev@gmail.com>
2016-12-31 01:10:53 +0100

    adds test python script for the emscripten interface build

--- emscripten/test/run.sh
+++ emscripten/test/run.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 #https://github.com/kripken/sql.js/blob/master/test/run.sh
 
+if [ "$#" -lt 1 ] || ! [ -f "$1" ] || ! [ -d "$2" ]; then
+    echo "Usage:" >&2
+    echo "   arg 1: libUncrustify.js file path" >&2
+    echo "   arg 2: test directory path" >&2
+    exit 1
+fi
+
 passed=0
 total=0
 testfiles="$(dirname "$(readlink -f "$0")")/test_*.js"
@@ -9,21 +16,21 @@ do
 	total=`expr $total + 1`
 	echo -ne "Testing $f...\t"
 	
-	node "$f" > /tmp/sqljstest
+	node "$f" "$1" "$2" > /tmp/libuncrustifyjs_test
 	if [ $? = 0 ]; then
 		echo "Passed."
 		passed=`expr $passed + 1`
 	else
-		echo -e "\033[31mFail!\e[0m"
-		cat /tmp/sqljstest
+		echo -e "\033[31mFail!\033[0m"
+		cat /tmp/libuncrustifyjs_test
 	fi
 done
 
 if [ $passed = $total ]
 then
-	echo -e "\033[32mAll $total tests passed\e[0m"
+	echo -e "\033[32mAll $total tests passed\033[0m"
 	exit 0
 else
-	echo -e "\033[31mWarning\e[0m : $passed tests passed out of $total"
+	echo -e "\033[31mWarning\033[0m : $passed tests passed out of $total"
 	exit 1
 fi 
