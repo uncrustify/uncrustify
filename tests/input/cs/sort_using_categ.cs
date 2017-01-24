003b2db51927270188b5d0dd78a3e46e7b989835
Mihai Popescu <mihai@unity3d.com>
2017-01-24 22:05:53 +0100

    added tests for sort using categorites (as taken from issue #935)

---
+++ tests/input/cs/sort_using_categ.cs
@@ -0,0 +1,12 @@
+using Client.Common;
+using LeopotamGroup.Common;
+using LeopotamGroup.Serialization;
+using System;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine;
+
+using NameSpace;
+using NameSpacEveryday;
+using nameSpace;
+using Namespace;
