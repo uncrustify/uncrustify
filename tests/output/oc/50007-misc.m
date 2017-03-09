43f6321d40fe7417c66b66226c4be93cd1d8a74e
Joel Ostraat <joel.ostraat.os@gmail.com>
2017-03-09 10:28:37 -0600

    Fix align_assign and an unstable test
    To determine how many new lines have spanned, use the count from the
    NEWLINE token.  Before, this span was determined from the difference
    of the original line numbers.  Since other functions might insert or
    remove new lines, it would generate different output depending on the
    input.  This made test 50007 unstable.
    
    This new method was ported from align_var_def_brace.

--- tests/output/oc/50007-misc.m
+++ tests/output/oc/50007-misc.m
@@ -2,7 +2,7 @@
 {
    GLfloat wc[3][3] = { { 0.6, 0.6, 0.0 }, { 1.0, 0.7, 0.1 }, { 0.5, 0.7, 0.2 }, };
    GLfloat cc[3][3] = { { 0.0, 0.0, 0.6 }, { 0.3, 0.1, 0.5 }, { 0.0, 0.0, 0.5 }, };
-   GLfloat sc[3] = { 0.75, 0.75, 0.75 };
+   GLfloat sc[3]    = { 0.75, 0.75, 0.75 };
 
    return [self initWithWarmColors: (float *)&wc
                         coolColors: (float *)&cc
