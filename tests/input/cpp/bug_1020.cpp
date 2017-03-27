c4750fafbeb5ed0a8089937123da271626ac196c
Guy Maurel <g.maurel@agwest.de>
2017-03-27 18:36:33 +0200

    Bugfix for #1020

---
+++ tests/input/cpp/bug_1020.cpp
@@ -0,0 +1,4 @@
+void HouseNumberData(Translation const & trans  = Translation { }, Orientation const & orient = Orientation { },  CategoryIds const & cats   = CategoryIds(), std::string const & txt    = std::string { }, bool  active = false);
+           
+void HouseNumberData(Translation const & trans______________, Orientation const & orient______________________,  CategoryIds const & cats_____________________, std::string const & txt___________________, bool active_________);
+
