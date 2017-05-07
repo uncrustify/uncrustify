1bd27993f987ada9d76c962c7ae601831f9c9d04
Daniel Chumak <chumak.daniel.dev@gmail.com>
2017-05-07 23:05:49 +0200

    Refactor code

--- src/newlines.cpp
+++ src/newlines.cpp
@@ -2698,15 +2698,66 @@ void newlines_cleanup_braces(bool first)
       }
       else if (pc->type == CT_BRACE_OPEN)
       {
-         if ((pc->parent_type == CT_DOUBLE_BRACE) &&
-             (cpd.settings[UO_nl_paren_dbrace_open].a != AV_IGNORE))
+         switch (pc->parent_type)
          {
-            prev = chunk_get_prev_ncnl(pc, scope_e::PREPROC);
-            if (chunk_is_paren_close(prev))
+         case CT_DOUBLE_BRACE:
+         {
+            if (cpd.settings[UO_nl_paren_dbrace_open].a != AV_IGNORE)
             {
-               newline_iarf_pair(prev, pc, cpd.settings[UO_nl_paren_dbrace_open].a);
+               prev = chunk_get_prev_ncnl(pc, scope_e::PREPROC);
+               if (chunk_is_paren_close(prev))
+               {
+                  newline_iarf_pair(prev, pc, cpd.settings[UO_nl_paren_dbrace_open].a);
+               }
             }
+            break;
          }
+
+         case CT_ENUM:
+         {
+            if (cpd.settings[UO_nl_enum_own_lines].a != AV_IGNORE)
+            {
+               newlines_enum_entries(pc, cpd.settings[UO_nl_enum_own_lines].a);
+            }
+            if (cpd.settings[UO_nl_ds_struct_enum_cmt].b)
+            {
+               newlines_double_space_struct_enum_union(pc);
+            }
+            break;
+         }
+
+         case CT_STRUCT:
+         case CT_UNION:
+         {
+            if (cpd.settings[UO_nl_ds_struct_enum_cmt].b)
+            {
+               newlines_double_space_struct_enum_union(pc);
+            }
+            break;
+         }
+
+         case CT_CLASS:
+         {
+            if (pc->level == pc->brace_level)
+            {
+               newlines_do_else(chunk_get_prev_nnl(pc), cpd.settings[UO_nl_class_brace].a);
+            }
+            break;
+         }
+
+
+         case CT_OC_BLOCK_EXPR:
+         {
+            // issue # 477
+            newline_iarf_pair(chunk_get_prev(pc), pc, cpd.settings[UO_nl_oc_block_brace].a);
+            break;
+         }
+
+         default:
+         {
+            break;
+         }
+         } // switch
 
          if (cpd.settings[UO_nl_brace_brace].a != AV_IGNORE)
          {
@@ -2715,31 +2766,6 @@ void newlines_cleanup_braces(bool first)
             {
                newline_iarf_pair(pc, next, cpd.settings[UO_nl_brace_brace].a);
             }
-         }
-
-         if ((pc->parent_type == CT_ENUM) &&
-             (cpd.settings[UO_nl_enum_own_lines].a != AV_IGNORE))
-         {
-            newlines_enum_entries(pc, cpd.settings[UO_nl_enum_own_lines].a);
-         }
-
-         if (cpd.settings[UO_nl_ds_struct_enum_cmt].b &&
-             ((pc->parent_type == CT_ENUM) ||
-              (pc->parent_type == CT_STRUCT) ||
-              (pc->parent_type == CT_UNION)))
-         {
-            newlines_double_space_struct_enum_union(pc);
-         }
-
-         if ((pc->parent_type == CT_CLASS) && (pc->level == pc->brace_level))
-         {
-            newlines_do_else(chunk_get_prev_nnl(pc), cpd.settings[UO_nl_class_brace].a);
-         }
-
-         if (pc->parent_type == CT_OC_BLOCK_EXPR)
-         {
-            // issue # 477
-            newline_iarf_pair(chunk_get_prev(pc), pc, cpd.settings[UO_nl_oc_block_brace].a);
          }
 
          next = chunk_get_next_nnl(pc);
