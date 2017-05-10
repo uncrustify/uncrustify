d490a03830f4098adfc351d0f408f8dfc6f22701
Guy Maurel <g.maurel@agwest.de>
2017-05-10 18:27:52 +0200

    a better test for 1002

--- tests/output/cpp/34147-bug_1002.cpp
+++ tests/output/cpp/34147-bug_1002.cpp
@@ -1,5 +1,5 @@
 template< class B1 = void, class B2 = void >
-struct conjunction : bool_constant<B1::value1      &&  B2::value2>
+struct conjunction : bool_constant<B1::value1&&B2::value2>
 {
 };
 template< class B1 = void, class B2 = void >
