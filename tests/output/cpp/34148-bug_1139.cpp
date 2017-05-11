95a7bb1ebbf44b86d834bcd177d98380fb76336f
Guy Maurel <g.maurel@agwest.de>
2017-05-11 18:57:18 +0200

    with a better test source

--- tests/output/cpp/34148-bug_1139.cpp
+++ tests/output/cpp/34148-bug_1139.cpp
@@ -5,10 +5,21 @@ void a()
         (tmp->type != CT_SIZEOF) &&
         !(tmp->flags & (PCF_IN_STRUCT | PCF_IN_CLASS))) ||
        (tmp->type == CT_NEWLINE)
-       )                  // closing parenthesis
+       )
    {
       set_chunk_type(next, CT_LABEL_COLON);
    }
+   else if ((tmp == nullptr) ||
+            ((tmp->type != CT_NUMBER) &&
+             (tmp->type != CT_SIZEOF) &&
+             !(tmp->flags & (PCF_IN_STRUCT | PCF_IN_CLASS))) ||
+            (tmp->type == CT_NEWLINE)
+            )
+   {
+      set_chunk_type(next, CT_LABEL_COLON);
+   }
+
+
    if ((tmp == nullptr) ||
        ((tmp->type != CT_NUMBER) &&
         (tmp->type != CT_SIZEOF) &&
--- tests/output/cpp/34149-bug_1139.cpp
+++ tests/output/cpp/34149-bug_1139.cpp
@@ -4,10 +4,20 @@ void a()
        ((tmp->type != CT_NUMBER) &&
         (tmp->type != CT_SIZEOF) &&
         !(tmp->flags & (PCF_IN_STRUCT | PCF_IN_CLASS))) ||
-       (tmp->type == CT_NEWLINE)) // closing parenthesis
+       (tmp->type == CT_NEWLINE))
    {
       set_chunk_type(next, CT_LABEL_COLON);
    }
+   else if ((tmp == nullptr) ||
+            ((tmp->type != CT_NUMBER) &&
+             (tmp->type != CT_SIZEOF) &&
+             !(tmp->flags & (PCF_IN_STRUCT | PCF_IN_CLASS))) ||
+            (tmp->type == CT_NEWLINE))
+   {
+      set_chunk_type(next, CT_LABEL_COLON);
+   }
+
+
    if ((tmp == nullptr) ||
        ((tmp->type != CT_NUMBER) &&
         (tmp->type != CT_SIZEOF) &&
