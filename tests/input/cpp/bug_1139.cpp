5895103857e3aad614b475524df16d5b503b977c
Guy Maurel <g.maurel@agwest.de>
2017-05-13 10:11:48 +0200

    Add an if, typos

--- tests/input/cpp/bug_1139.cpp
+++ tests/input/cpp/bug_1139.cpp
@@ -24,7 +24,18 @@ else if ((tmp == nullptr) ||
        ((tmp->type != CT_NUMBER) &&
         (tmp->type != CT_SIZEOF) &&
         !(tmp->flags & (PCF_IN_STRUCT | PCF_IN_CLASS))) ||
-       (tmp->type == CT_NEWLINE)) // closing parenthesis
+          (tmp->type == CT_NEWLINE))
+   {
+      set_chunk_type(next, CT_LABEL_COLON);
+   }
+
+   if                 ((tmp == nullptr) ||
+       ((tmp->type != CT_NUMBER) &&
+        (tmp->type != CT_SIZEOF) &&
+        !(tmp->flags & (PCF_IN_STRUCT | PCF_IN_CLASS))) ||
+          (tmp->type == CT_NEWLINE)
+
+)
    {
       set_chunk_type(next, CT_LABEL_COLON);
    }
