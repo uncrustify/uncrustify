9c83e03dbe0d63d69c4819433e91a59192030ae5
Gino <gino.galotti@gmail.com>
2017-05-29 15:22:45 +0200

    Add remaining favro bugs
    Add new tests for Unity
    
    Add extra case for UNI-17253
    
    Add new tests for Unity

---
+++ tests/input/staging/UNI-19895.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.Serialization;
+namespace UnityEngine.Experimental.Input
+{
+    public class ActionMap : ScriptableObject, IControlDomainSource
+    {
+        public List<InputControl> BuildControlsList()
+        {
+            ControlSetup controlsSetup = new ControlSetup();
+            for (int i = 0; i < actions.Count; i++)
+            {
+                var action = actions[i];
+// This line is kept with 71 spaces.
+                SupportedControl supportedControl = (SupportedControl)(typeof(SupportedControl)
+                                                                       .GetMethod("Get")
+                                                                       .MakeGenericMethod(actions[i].controlType)
+                                                                       .Invoke(null, new object[] { actions[i].name }));
+                action.controlIndex = controlsSetup.AddControl(supportedControl).index;
+            }
+            return controlsSetup.controls;
+        }
+    }
+}
