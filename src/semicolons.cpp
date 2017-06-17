6e11b80a91b9c5515f68b121e910c5592039be36
Daniel Chumak <chumak.daniel.dev@gmail.com>
2017-06-17 19:00:59 +0200

    Semicolon removal fix
    return vector<int>{1}; <- Semicolon was removed.
    
    The check_unknown_brace_close() function did not check for template type
    argument-list angle brackets which lead to the faulty removal.

--- src/semicolons.cpp
+++ src/semicolons.cpp
@@ -119,12 +119,13 @@ static void check_unknown_brace_close(chunk_t *semi, chunk_t *brace_close)
    LOG_FUNC_ENTRY();
    chunk_t *pc = chunk_get_prev_type(brace_close, CT_BRACE_OPEN, brace_close->level);
    pc = chunk_get_prev_ncnl(pc);
-   if (  (pc != nullptr)
-      && (pc->type != CT_RETURN)
-      && (pc->type != CT_WORD)
-      && (pc->type != CT_TYPE)
-      && (pc->type != CT_SQUARE_CLOSE)
-      && (pc->type != CT_TSQUARE)
+   if (  pc != nullptr
+      && pc->type != CT_RETURN
+      && pc->type != CT_WORD
+      && pc->type != CT_TYPE
+      && pc->type != CT_SQUARE_CLOSE
+      && pc->type != CT_ANGLE_CLOSE
+      && pc->type != CT_TSQUARE
       && !chunk_is_paren_close(pc))
    {
       remove_semicolon(semi);
---
+++ tests/input/cpp/i1207.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+std::vector<int> f()
+{;
+return std::vector<int>{1};
+};
+
+int main()
+{;
+    return f()[0];;;;;
+};
