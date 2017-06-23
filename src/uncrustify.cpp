dbbe3ba9a9cff8330f03add4410c67e921f89579
Ben Mayo <ben.mayo@garmin.com>
2017-06-23 17:27:31 -0500

    Remove useless space from uncrustify help
    Remove useless space from uncrustify help because it's
    throwing errors when trying to format.
    
    Change-Id: I59d37761d433d11dba3891cce66f0c323e708caa

--- src/uncrustify.cpp
+++ src/uncrustify.cpp
@@ -277,7 +277,7 @@ void usage_exit(const char *msg, const char *argv0, int code)
            "uncrustify -c my.cfg --prefix=out -F files.txt\n"
            "\n"
            "Note: Use comments containing ' *INDENT-OFF*' and ' *INDENT-ON*' to disable\n"
-           "      processing of parts of the source file (these can be overridden with \n"
+           "      processing of parts of the source file (these can be overridden with\n"
            "      enable_processing_cmt and disable_processing_cmt).\n"
            "\n"
            "There are currently %d options and minimal documentation.\n"
