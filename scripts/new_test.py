44fbeedd28a8d0521adcc34393e9421de3d7070a
Mihai Popescu <mihai@unity3d.com>
2017-07-18 13:18:28 +0200

    Finished new_test.py script

--- scripts/new_test.py
+++ scripts/new_test.py
@@ -2,23 +2,33 @@
 from sys import exit as sys_exit, stdin
 from os import path
 import re
+import argparse
 
 def parse_test(data):
-    lines = [ line.strip() for line in data.splitlines() ]
-    
-    tests = [ 
-        {
-            # 'prefix': m.group(1),
-            'id': m.group(2),
-            'config': m.group(3),
-            'input': m.group(4),
-            'output': m.group(5)
-        } for m in [ re.match(line, r'\s*(#|!)?(\d)\s+(\S+)\s+(\S+)$') for line in lines ] if m and not m.group(1) ]
+    """parses a test file data into a simple python list of dictionaries"""
+    lines = [line.strip() for line in data.splitlines()]
+    tests = [{'id'    : int(m.group(2)),
+              'config': m.group(3),
+              'input' : m.group(4)}
+             for m in [re.match(r'\s*(#|!)?(\d+)\s+(\S+)\s+(\S+)$', line) for line in lines]
+             if m and not m.group(1)]
 
-    print tests
+    return tests
 
 
 def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--lang', type=str, help="Language, (CPP, CS)", default='CPP')
+    arg_parser.add_argument('--id', type=int, help="bug number", default=99999)
+
+    args = arg_parser.parse_args()
+
+    ext = args.lang.lower()
+    if ext == 'cpp':
+        cfg = "Cpp"
+    elif ext == 'cs':
+        cfg = "CSharp"
+
     root_dir = path.dirname(path.dirname(path.abspath(__file__)))
     test_dir = path.join(root_dir, "tests")
     config_dir = path.join(test_dir, "config")
@@ -30,13 +40,33 @@ def main():
     staging_input_dir = path.join(input_dir, "staging")
     staging_output_dir = path.join(output_dir, "staging")
 
-
-    # source = [ x.strip() for x in stdin.readlines() ]
-
+    tests = None
     with open(staging_test_file, 'r') as f:
         test_data = f.read()
-        parse_test(test_data)
+        tests = parse_test(test_data)
 
+    max_test = max(tests, key=lambda x: x['id'])
+    new_id = max_test['id'] + 1
+
+    file_name = "UNI-%d.%s" % (args.id, ext)
+    config = "staging/Uncrustify.%s.cfg" % (cfg)
+    input = "staging/" + file_name
+    test = "%d %s %s" % (new_id, config, input)
+
+    input_file_path = path.join(staging_input_dir, file_name)
+
+    input_data = stdin.read()
+    with open(input_file_path, 'w') as f:
+        f.write(input_data)
+
+    output_file_path = path.join(staging_output_dir, "%s-%s" % (new_id, file_name))
+    with open(output_file_path, 'w') as f:
+        f.write(input_data)
+
+    with open(staging_test_file, 'a') as f:
+        f.write("%s\n" % test)
+
+    print("Test %d was added to staging test\n" % (new_id))
 
 if __name__ == "__main__":
     sys_exit(main())
