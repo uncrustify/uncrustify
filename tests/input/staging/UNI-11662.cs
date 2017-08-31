b67b1d4157d0520511701e19b2f7aabed1aef722
Mihai Popescu <mihai@unity3d.com>
2017-08-31 12:33:21 +0200

    fixed removal of semicolon test which now shows the bug

--- tests/input/staging/UNI-11662.cs
+++ tests/input/staging/UNI-11662.cs
@@ -3,10 +3,8 @@ namespace Unity
     public class Class
     {
         // doesn't work because ; gets removed but
-        public static readonly Vector2Quantization Uncompressed = new Vector2Quantization() {};
+        public static readonly Class A = new Class() { name = "A", id = 1 };
         // works and ; doesn't get removed
-        public static readonly Vector2Quantization Uncompressed = new Vector2Quantization {};
-
-        public static readonly SupportedControl none = new SupportedControl() { standardName = "None", hash = -1 };
+        public static readonly Class B = new Class { name = "B", id = 2 };
     }
 }
