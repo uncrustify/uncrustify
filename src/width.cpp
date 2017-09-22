dfe791684b24adc9eef81baef867f824866abdfa
Guy Maurel <g.maurel@agwest.de>
2017-09-22 18:13:19 +0200

    A bugfix for #1352. A new run in the loop is necessary.

--- src/width.cpp
+++ src/width.cpp
@@ -338,6 +338,8 @@ static bool split_line(chunk_t *start)
       LOG_FMT(LSPLIT, " ** ONCE LINER SPLIT **\n");
       undo_one_liner(start);
       newlines_cleanup_braces(false);
+      // Issue #1352
+      cpd.changes++;
       return(false);
    }
 
