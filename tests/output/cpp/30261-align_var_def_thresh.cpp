ae41941afb003705a02f52392eeb5d81c6dc853e
Viktor Kireev <vitech-job@yandex.ru>
2017-09-27 20:35:36 +0300

    Add test for align_var_def_thresh.

---
+++ tests/output/cpp/30261-align_var_def_thresh.cpp
@@ -0,0 +1,29 @@
+void test1()
+{
+	float  a;
+	double b;
+
+	float &  c;
+	double & d;
+}
+
+void test2()
+{
+	int      int_var;
+	int *    int_ptr_var;
+	float    float_var;
+	float &  float_ref_var;
+	double & double_var;
+	SomeLongNamespace::SomeLongType long_var;
+	int * other_int_var;
+	SomeLooooongType long_var;
+	SomeLoooooooooongType looong_var;
+	int int_var;
+	SomeLongNamespace::OtherLongNamespace::SomeLongType very_long_var;
+	int *    int_ptr_var;
+	float    float_var;
+	float &  float_ref_var;
+	double & double_var;
+	SomeLongNamespace::SomeLongType long_var;
+	int * other_int_var;
+}
