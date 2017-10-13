1dbc2c87451d735ab6e8a15d19f202843afb0def
puppalasatish <satishpuppala87@gmail.com>
2017-10-13 14:40:26 +0530

    fixes issue # 1257
    Added few extra test scenarios
    staging updated for other stories
    removed 10054 as part of story #1236
    removed 60036 as part of story #1292

--- tests/input/staging/UNI-17253.cs
+++ tests/input/staging/UNI-17253.cs
@@ -1,9 +1,56 @@
-// It adds an extra newline after each case 
-switch (someString)
+// Extra test cases for  # 1257
+switch (sometext)
 {
     case "a":
-        return foo;
-
+        return 0;
     case "b":
-        return bar;
+        Console.WrieLine("hello world\n");
+        return 0;
+    case "c":
+    {
+        Console.WrieLine("hello world\n");
+        return 0;
+    }
+    case "d":
+        Console.WrieLine("hello world\n");
+
+        if (hello)
+            return 0;
+        else
+            return 1;
+
+    case "e":
+        Console.WrieLine("hello world\n");
+
+        if (hello)
+        {
+            int a;
+            int b;
+            return 0;
+        }
+
+    case "f":
+    {
+        return 0;
+    }
+    case "g":
+
+        return 0;
+
+    case "h":
+        for (i = 0; i < 10 i++)
+        {
+            a += i;
+            return 0;
+        }
+    case "i":
+
+        if (hello)
+        {
+            int a;
+            int b;
+            return 0;
+        }
+
+        return 1;
 }
