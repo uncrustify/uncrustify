4238a2962915638e1b19fceb284779f8d27cfe46
Rasmus Sigsgaard <rasmussi@unity3d.com>
2017-11-03 12:05:27 +0100

    PR #40 tests for indentation regressions

---
+++ tests/input/staging/UNI-30628.cpp
@@ -0,0 +1,10 @@
+// Remaining issue
+extern void BillboardRenderer_RenderMultiple(const RenderBatchedData&renderData, ShaderChannelMask channels);
+
+// Regression 1 FAKE_METHOD expands to a function prototype. Could possibly use PROTO_WRAP like for FAKE_FUNCTION
+class Foo
+{
+    FAKE_FUNCTION(Bar, GetBarInfo, const BarInfo &());
+    FAKE_METHOD(Bar, GetBarInfo, const BarInfo &());
+    FAKE_FUNCTION_WITH_LOCAL_NAME(FakeGetCommonScriptingClasses, GetCommonScriptingClasses, const CommonScriptingClasses &());
+}
