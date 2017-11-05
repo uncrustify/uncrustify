be283c00baf442c486a6a3434012c15332b2572c
Daniel Chumak <chumak.daniel.dev@gmail.com>
2017-11-05 02:34:40 +0100

    unc_text move header functions

--- src/unc_text.cpp
+++ src/unc_text.cpp
@@ -45,6 +45,178 @@ static size_t fix_len_idx(size_t size, size_t idx, size_t len)
 }
 
 
+unc_text::unc_text()
+   : m_logok(false)
+{
+}
+
+
+unc_text::unc_text(const unc_text &ref)
+{
+   set(ref);
+}
+
+
+unc_text::unc_text(const unc_text &ref, size_t idx, size_t len)
+{
+   set(ref, idx, len);
+}
+
+
+unc_text::unc_text(const char *ascii_text)
+{
+   set(ascii_text);
+}
+
+
+unc_text::unc_text(const std::string &ascii_text)
+{
+   set(ascii_text);
+}
+
+
+unc_text::unc_text(const value_type &data, size_t idx, size_t len)
+{
+   set(data, idx, len);
+}
+
+
+size_t unc_text::size() const
+{
+   return(m_chars.size());
+}
+
+
+unc_text &unc_text::operator =(int ch)
+{
+   set(ch);
+   return(*this);
+}
+
+
+unc_text &unc_text::operator =(const unc_text &ref)
+{
+   set(ref);
+   return(*this);
+}
+
+
+unc_text &unc_text::operator =(const std::string &ascii_text)
+{
+   set(ascii_text);
+   return(*this);
+}
+
+
+unc_text &unc_text::operator =(const char *ascii_text)
+{
+   set(ascii_text);
+   return(*this);
+}
+
+
+unc_text &unc_text::operator +=(int ch)
+{
+   append(ch);
+   return(*this);
+}
+
+
+unc_text &unc_text::operator +=(const unc_text &ref)
+{
+   append(ref);
+   return(*this);
+}
+
+
+unc_text &unc_text::operator +=(const std::string &ascii_text)
+{
+   append(ascii_text);
+   return(*this);
+}
+
+
+unc_text &unc_text::operator +=(const char *ascii_text)
+{
+   append(ascii_text);
+   return(*this);
+}
+
+
+unc_text::value_type &unc_text::get()
+{
+   m_logok = false;
+   return(m_chars);
+}
+
+
+const unc_text::value_type &unc_text::get() const
+{
+   return(m_chars);
+}
+
+
+int unc_text::operator[](size_t idx) const
+{
+   return((idx < m_chars.size()) ? m_chars[idx] : 0);
+}
+
+
+int &unc_text::at(size_t idx)
+{
+   return(m_chars.at(idx));
+}
+
+
+const int &unc_text::at(size_t idx) const
+{
+   return(m_chars.at(idx));
+}
+
+
+const int &unc_text::back() const
+{
+   return(m_chars.back());
+}
+
+
+int &unc_text::back()
+{
+   // TODO: returning a temporary via a reference this has to be checked and probably changed
+   return(m_chars.back());
+}
+
+
+void unc_text::push_back(int ch)
+{
+   append(ch);
+}
+
+
+void unc_text::pop_back()
+{
+   if (size() == 0)
+   {
+      return;
+   }
+
+   m_chars.pop_back();
+   update_logtext();
+}
+
+
+void unc_text::pop_front()
+{
+   if (size() == 0)
+   {
+      return;
+   }
+
+   m_chars.pop_front();
+   update_logtext();
+}
+
+
 void unc_text::update_logtext()
 {
    if (m_logok)
