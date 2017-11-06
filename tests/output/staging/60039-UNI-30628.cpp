7ac34a0cea3f78c0816cac051f608b04f791860f
Rasmus Sigsgaard <rasmussi@unity3d.com>
2017-11-06 13:25:38 +0100

    Removed wrong test. Works as intended. In order to get space added use option sp_after_byref = add.

--- tests/output/staging/60039-UNI-30628.cpp
+++ tests/output/staging/60039-UNI-30628.cpp
@@ -1,6 +1,3 @@
-// Remaining issue
-extern void BillboardRenderer_RenderMultiple(const RenderBatchedData& renderData, ShaderChannelMask channels);
-
 // Regression 1 FAKE_METHOD expands to a function prototype. Could possibly use PROTO_WRAP like for FAKE_FUNCTION
 class Foo
 {
