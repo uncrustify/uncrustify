5bba649cc96d60592e858e5e5c552d6d8e360a3e
Daniel Chumak <chumak.daniel.dev@gmail.com>
2017-12-05 14:15:15 +0100

    add cast to remove compiler warning

--- src/chunk_list.cpp
+++ src/chunk_list.cpp
@@ -444,7 +444,7 @@ static void chunk_log(chunk_t *pc, const char *text)
       {
          chunk_log_msg(prev, log, " @ after");
       }
-      LOG_FMT(log, " stage is %d", cpd.unc_stage);
+      LOG_FMT(log, " stage is %u", static_cast<unsigned int>(cpd.unc_stage));
       log_func_stack_inline(log);
    }
 }
