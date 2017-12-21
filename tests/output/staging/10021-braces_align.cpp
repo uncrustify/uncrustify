953cda37c7f0a1e47f5968ac40f0154bbada687a
KalyanaChakravarthi <31840402+KalyanaChakravarthi@users.noreply.github.com>
2017-12-21 12:28:43 +0530

    Fix for #1234
    Reverted the option sp_inside_braces to default.

--- tests/output/staging/10021-braces_align.cpp
+++ tests/output/staging/10021-braces_align.cpp
@@ -2,12 +2,12 @@ char *array_assign[2][4] =
 {
     {
         // foo
-        { "foo" },
-        { "foo@1" }, { "foo@2" }, { "foo@3" }
+        {"foo"},
+        {"foo@1"}, {"foo@2"}, {"foo@3"}
     },
     {
         // bar
-        { "bar" },
-        { "bar@1" }, { "bar@2" }, { "bar@3" }
+        {"bar"},
+        {"bar@1"}, {"bar@2"}, {"bar@3"}
     }
 };
