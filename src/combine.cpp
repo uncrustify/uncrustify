aae6fa7429ac05e5c1aee84668c7c31c1a725e43
Antti Tapaninen <aet@unity3d.com>
2018-01-02 15:06:31 -0800

    improve extern "C" support
    Otherwise types won't get properly detected and
    with sp_arith=add, it leads to whitespace issues
    
    Remove potential extra whitespace from extern "C"
    oneliners, there was no mathing existing
    configuration option for such case and unlikely
    need, too
    
    This fixes the issue with C/CPP but not with OC/OC+,
    which needs an extra commit for proper C keyword
    support
    
    Example failure from the diff file:
    
    -extern "C" int *i;
    +extern "C"    int *i;
     extern "C" {  int *i; }
     int *i;
    -extern "C" FooString *i;
    +extern "C"    FooString * i;
     extern "C" {  FooString *i; }
     FooString *i;
    
    part of
    original commit: 13a82ccd93d5d03da98d355b27926927d5435cf9

--- src/combine.cpp
+++ src/combine.cpp
@@ -1860,6 +1860,32 @@ void fix_symbols(void)
          }
       }
 
+      if ((pc->type == CT_EXTERN) && (cpd.lang_flags & LANG_ALLC))
+      {
+         chunk_t *next = chunk_get_next_ncnl(pc);
+         if (next->type == CT_STRING)
+         {
+            chunk_t *tmp = chunk_get_next_ncnl(next);
+            while (tmp != NULL)
+            {
+               if (  (tmp->type == CT_TYPE)
+                  || (tmp->type == CT_BRACE_OPEN)
+                  || (tmp->type == CT_ATTRIBUTE))
+               {
+                  break;
+               }
+
+               if (tmp->type == CT_WORD)
+               {
+                  tmp->flags |= PCF_STMT_START | PCF_EXPR_START;
+                  break;
+               }
+
+               tmp = chunk_get_next_ncnl(tmp);
+            }
+         }
+      }
+
       /*
        * A variable definition is possible after at the start of a statement
        * that starts with: QUALIFIER, TYPE, or WORD
