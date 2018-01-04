01b65ac229d69f12a770dc5ba56214a0964b800b
Antti Tapaninen <aet@unity3d.com>
2018-01-03 17:43:32 -0800

    add OC test method_ends_semicolon.m
    (was: improve CT_OC_MSG_DECL type check)
    
    Occasionally there will be user errors where someone will
    copy the interface method declaration to implementation
    and leaves the semicolon
    
    Depending on brace settings, this leads to incorrect braces handling
    
    Example failure from the diff file:
    
    -+ (void)cancelRequest:(id)request; {
    ++ (void)cancelRequest:(id)request;
    +{
     }

---
+++ tests/input/oc/method_ends_semicolon.m
@@ -0,0 +1,18 @@
+#import <Foundation/Foundation.h>
+
+@interface TestClass
+
++ (void)cancelRequest:(id)request;
+
+@end
+
+@implementation TestClass
+
+// Occasionally there will be user errors where someone will
+// copy the interface method declaration to implementation
+// and leaves the semicolon
++ (void)cancelRequest:(id)request;
+{
+}
+
+@end
