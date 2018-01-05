640ac8bd394908998c3daf046278cbe4b6b550c9
Aneesh Gandavarapu <agandavarapu@rythmos.com>
2018-01-05 11:20:00 +0530

    fixes #1269

--- tests/output/staging/60023-UNI-18437.cs
+++ tests/output/staging/60023-UNI-18437.cs
@@ -2,34 +2,14 @@ private static Type[] GetAllVisualElementTypes()
 {
     return typeof(VisualElement).Assembly.GetTypes()
         .Where(t => t != typeof(VisualElement) &&
-                t != typeof(Panel) &&
-                !t.IsAbstract &&
-                !typeof(IMElement).IsAssignableFrom(t) &&
-                !typeof(IMContainer).IsAssignableFrom(t) &&
-                typeof(VisualElement).IsAssignableFrom(t)).ToArray();
+            t != typeof(Panel) &&
+            !t.IsAbstract &&
+            !typeof(IMElement).IsAssignableFrom(t) &&
+            !typeof(IMContainer).IsAssignableFrom(t) &&
+            typeof(VisualElement).IsAssignableFrom(t)).ToArray();
 }
 
-private static Type[] GetAllVisualElementAssetTypes()
-{
-    return typeof(VisualElementAsset).Assembly.GetTypes()
-        .Where(t => t != typeof(VisualElementAsset) &&
-                !t.IsAbstract &&
-                typeof(VisualElementAsset).IsAssignableFrom(t)).ToArray();
-}
-
-//We would like to get something like
-private static Type[] GetAllVisualElementAssetTypes()
-{
-    return typeof(VisualElement).Assembly.GetTypes()
-        .Where(t => t != typeof(VisualElement) &&
-                    t != typeof(Panel) &&
-                    !t.IsAbstract &&
-                    !typeof(IMElement).IsAssignableFrom(t) &&
-                    !typeof(IMContainer).IsAssignableFrom(t) &&
-                    typeof(VisualElement).IsAssignableFrom(t)).ToArray();
-}
-
-// Or at least this
+// to this
 private static Type[] GetAllVisualElementAssetTypes()
 {
     return typeof(VisualElement).Assembly.GetTypes()
