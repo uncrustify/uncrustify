5bef13531b28123ccd59418770e44150d7ec432e
B. Kevin Hardman <nivek.research@gmail.com>
2018-01-12 20:46:59 -0500

    add tests for sp_cpp_before_struct_binding, fix formatting

---
+++ tests/input/cpp/cpp17.cpp
@@ -0,0 +1,8 @@
+bool CompareGenomeByFeatureResults::clickOnLink(std::string const& inLink) {
+   auto const[sequence, type, firstPosition, lastPosition] = parseLink(inLink);
+   if (sequence.empty()) {
+      return true;
+   }
+   return showFeature(statistics.nameDocumentA, type, firstPosition, lastPosition);
+}
+
