47845503ab2e2b15b26c802a4d34edf506b27500
satish puppala <spuppala@rythmos.com>
2018-01-23 16:40:13 +0530

    fixed build issue
    changed testcase numbers and renamed the testcases names

---
+++ tests/output/c/09905-negative_value.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int i = -1;
+    int j = 30 * -1;
+
+    int frames = 127;
+    frames = ((frames + 15) & -16);
+    return 0;
+}
--- tests/output/java/80200-sp_before_byref.java
+++
@@ -1,5 +0,0 @@
-public static void method() {
-	if (argA != null && argB != null) {
-	}
-	return (argA != null && argB != null);
-}
