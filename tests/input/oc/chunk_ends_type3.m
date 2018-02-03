f526150d2c874b3863a04d9ac1e5e57de15b0ce0
Guy Maurel <g.maurel@agwest.de>
2018-02-03 17:36:10 +0100

    Adopt UT tests 50612, 50613, 50614, 50615, 50616, 50617, 50618, 50619, 50008, 50090, 60040, 80204

---
+++ tests/input/oc/chunk_ends_type3.m
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+#define MACRO(foo) \
+    if (_##foo == NULL) { \
+        Log("do %s", str(foo)); \
+        _##foo = (foo##_t *)process(PR_FLAG, str(foo)); \
+    }
+
+#define OVERLOAD(base, foo) \
+    foo##_override_t * foo##_bar = (foo##_override_t *)process(base##_bar, str(foo)); \
+    _##foo##_override = (foo##_override_t *)process(base##_cache, str(foo));
