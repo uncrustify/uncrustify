525409a35a25d1c6abd1c620ea0f492d8df716ae
satish puppala <spuppala@rythmos.com>
2018-02-09 10:42:53 +0530

    fix for #36664
    fixed where indentation for where in function proto and updated the testcases with new scenarios.

--- tests/input/staging/UNI-32658.cs
+++ tests/input/staging/UNI-32658.cs
@@ -22,3 +22,15 @@ class Dictionary<TKey, TVal>
 {
     public void Add(TKey key, TVal val) {}
 }
+
+long DeviceCommand<TCommand>(int deviceId, ref TCommand command)
+    where TCommand : struct, IInputDeviceCommandInfo;
+
+public virtual long OnDeviceCommand<TCommand>(ref TCommand command)
+    where TCommand : struct, IInputDeviceCommandInfo;
+
+long DeviceCommand<TCommand>(int deviceId, ref TCommand command)
+    where TCommand : struct, IInputDeviceCommandInfo
+
+public virtual long OnDeviceCommand<TCommand>(ref TCommand command)
+    where TCommand : struct, IInputDeviceCommandInfo
