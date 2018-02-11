2cc8c1a11038aa76f6abf4fb46baff77f146b180
Daniel Chumak <chumak.daniel.dev@gmail.com>
2018-02-11 12:59:00 +0100

    Allow align_func_param to align params proceeded by a leading comma

---
+++ tests/input/cpp/func_param_indent_leading_comma.cpp
@@ -0,0 +1,7 @@
+uint32_t foo ( uint8_t param1
+               , some_datatype param2
+               , datatype param3
+               , another_datatype *param4
+               , uint16_t param5
+               , uint32_t *param6
+               );
