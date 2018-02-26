3785a7ea9899989e2f09a6f298158cc07750b37c
JavDevGames <admin@javdev.com>
2018-02-26 14:31:34 +0100

    There was a resource contention issue when invoking ctest with the parallel flag (-j) when attempting to access the ERR-1.txt or ERR-2.txt files in the uncrustify/tests folder. This caused ctest runs with the -j flag to randomly fail as the file system was returning an access denied issue.
    Adding a random number to the .txt file name (i.e. ERR-1-1234.txt) has fixed the issue, so we no longer have the resource contention and all tests pass when running them with the parallel flag.

--- tests/run_test.cmake
+++ tests/run_test.cmake
@@ -39,21 +39,23 @@ endif()
 
 
 # first pass
+string(RANDOM LENGTH 8 ALPHABET 0123456789 test_uuid)
+
 execute_process(
   COMMAND ${TEST_PROGRAM} -l ${TEST_LANG} -c ${TEST_CONFIG} -f ${TEST_INPUT} -o ${TEST_RESULT}
     WORKING_DIRECTORY ${TEST_DIR}
     RESULT_VARIABLE uncrustify_error_code
     OUTPUT_QUIET
     ERROR_VARIABLE uncrustify_error
-    ERROR_FILE ${TEST_DIR}/ERR-1.txt
+    ERROR_FILE ${TEST_DIR}/ERR-1-${test_uuid}.txt
 )
 
 if(uncrustify_error_code)
   execute_process(
-    COMMAND ${CMAKE_COMMAND} -E echo "Uncrustify error. The ERR-1.txt file is:"
+    COMMAND ${CMAKE_COMMAND} -E echo "Uncrustify error: " ${uncrustify_error_code} "  The ERR-1-${test_uuid}.txt file is:"
   )
   execute_process(
-    COMMAND ${CMAKE_COMMAND} -E copy ${TEST_DIR}/ERR-1.txt /dev/stderr
+    COMMAND ${CMAKE_COMMAND} -E copy ${TEST_DIR}/ERR-1-${test_uuid}.txt /dev/stderr
   )
   message(SEND_ERROR "Uncrustify error_code (Pass 1)")
 else(uncrustify_error_code)
@@ -73,7 +75,7 @@ else(uncrustify_error_code)
     message(SEND_ERROR "Uncrustify MISSMATCH (Pass 1)")
   endif()
 endif(uncrustify_error_code)
-file(REMOVE ${TEST_DIR}/ERR-1.txt)
+file(REMOVE ${TEST_DIR}/ERR-1-${test_uuid}.txt)
 
 
 # Re-run with the output file as the input to check stability.
@@ -83,15 +85,15 @@ execute_process(
     RESULT_VARIABLE uncrustify_error_code
     OUTPUT_QUIET
     ERROR_VARIABLE uncrustify_error
-    ERROR_FILE ${TEST_DIR}/ERR-2.txt
+    ERROR_FILE ${TEST_DIR}/ERR-2-${test_uuid}.txt
 )
 
 if(uncrustify_error_code)
   execute_process(
-    COMMAND ${CMAKE_COMMAND} -E echo "Uncrustify error. The ERR-2.txt file is:"
+    COMMAND ${CMAKE_COMMAND} -E echo "Uncrustify error: " ${uncrustify_error_code} "  The ERR-2-${test_uuid}.txt file is:"
   )
   execute_process(
-    COMMAND ${CMAKE_COMMAND} -E copy ${TEST_DIR}/ERR-2.txt /dev/stderr
+    COMMAND ${CMAKE_COMMAND} -E copy ${TEST_DIR}/ERR-2-${test_uuid}.txt /dev/stderr
   )
   message(SEND_ERROR "Uncrustify error_code (Pass 2)")
 else(uncrustify_error_code)
@@ -110,4 +112,4 @@ else(uncrustify_error_code)
     message(SEND_ERROR "Uncrustify UNSTABLE (Pass 2)")
   endif()
 endif()
-file(REMOVE ${TEST_DIR}/ERR-2.txt)
+file(REMOVE ${TEST_DIR}/ERR-2-${test_uuid}.txt)
