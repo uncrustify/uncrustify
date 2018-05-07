30c84a549218cadf7875f1700802e490ab4ef453
Prasanth Kusumaraju <pkusumaraju@rythmos.com>
2018-05-07 16:00:08 +0530

    Adding the test-case that causing crash

---
+++ tests/output/staging/60057-UNI-crash.cpp
@@ -0,0 +1,12 @@
+UNIT_TEST_SUITE(CollabTracker)
+{
+    TEST(IsAssetsPath_WhenAssetPathWinSeparator_Returns_True)
+    {
+        CHECK(CollabTracker::IsAssetsPath(kTestAssetPath2));
+    }
+
+    TEST_FIXTURE(Fixture, IsPathTracked_WhenNotAssetsPath_Returns_False)
+    {
+        CHECK(!CollabTracker::IsPathTracked(kTestNotAssetPath));
+    }
+}
