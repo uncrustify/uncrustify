a62931020367dc64afa979de3006721526dd7cf4
Guy Maurel <g.maurel@agwest.de>
2018-06-12 17:08:35 +0200

    Reduce the config file

--- tests/output/cpp/30880-bug_1758.cpp
+++ tests/output/cpp/30880-bug_1758.cpp
@@ -1,13 +1,12 @@
-for (int f = 0; f < (Element::nf)*2; f++) {
-	if (f % 2 == 1) p = p - 1;
+for(int f=0; f<(Element::nf)*2; f++) {
+	if (f%2==1) p = p-1;
 
 	{
-		this->pInterpolation[i] = p;
-		this->cInterpolation[i] = 0.;
-		this->dofInterpolation[i] = e + f;
-		this->coefInterpolation[i] = 1.;
+		this->pInterpolation[i]=p;
+		this->cInterpolation[i]=0.;
+		this->dofInterpolation[i]=e+f;
+		this->coefInterpolation[i]=1.;
 		i++;
 		p++;
 	}
 }
-
