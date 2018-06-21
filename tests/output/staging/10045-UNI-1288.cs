516d178ad61d720b34ccda88b4e682b8080897ab
Mihai Popescu <mihai@unity3d.com>
2018-06-21 16:33:08 +0200

    UNI-1288

--- tests/output/staging/10045-UNI-1288.cs
+++ tests/output/staging/10045-UNI-1288.cs
@@ -41,15 +41,15 @@ class ObjInitializers
 
         List<Cat> cats = new List<Cat>
         {
-            new Cat() { Name = "Sylvester", Age = 8 },
-            new Cat() { Name = "Whiskers", Age = 2 },
-            new Cat() { Name = "Sasha", Age = 14 }
+            new Cat(){ Name = "Sylvester", Age = 8 },
+            new Cat(){ Name = "Whiskers", Age = 2 },
+            new Cat(){ Name = "Sasha", Age = 14 }
         };
 
         List<Cat> moreCats = new List<Cat>
         {
-            new Cat() { Name = "Furrytail", Age = 5 },
-            new Cat() { Name = "Peaches", Age = 4 },
+            new Cat(){ Name = "Furrytail", Age = 5 },
+            new Cat(){ Name = "Peaches", Age = 4 },
             null
         };
 
