8b35eed31f91e22362b2c89b264013a7fd44b894
Mihai Popescu <mihai@unity3d.com>
2018-06-22 10:38:45 +0200

    improve test case UNI-34114

--- tests/output/staging/20005-UNI-34114.cpp
+++ tests/output/staging/20005-UNI-34114.cpp
@@ -30,3 +30,11 @@
 / |         |       |
 / +---------+       /
 / \- width -/ */
+
+/// E.g. you might want have a single "editor_assets:\" root which combines multiple Assets folders together:
+/// project's "Assets", global shared (tools) "Assets", collab assets, etc.
+/// editor_assets:\
+/// |-project_assets\ - ".\Assets"
+/// |-shared_assets\  - "C:\MyStudio\Assets"
+/// |-builtins_assets\  - "%programfiles%\Unity\BuiltinAssets"
+/// | ...
