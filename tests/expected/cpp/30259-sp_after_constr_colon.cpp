4abdbbe15e1783a3e2a3349fa48407e06ef11d3e
Guy Maurel <g.maurel@agwest.de>
2018-07-19 17:24:06 +0200

    simplify the data

--- tests/expected/cpp/30259-sp_after_constr_colon.cpp
+++ tests/expected/cpp/30259-sp_after_constr_colon.cpp
@@ -1,17 +1,6 @@
-struct MyClass : public Foo,
-                 private Bar {
-  MyClass(
-      int a,
-      int b,
-      int c)
-      :m_a(a),
-        m_b(b),
-        m_c(c) {}
-
- private:
-  int m_a, m_b, m_c;
+struct MyClass : public Foo {
+  MyClass(int a,
+          int b,
+          int c)
+      :m_a(a), m_b(b), m_c(c) {}
 };
-
-struct TheirClass
-  : public Foo,
-    private Bar {};
