uncrustify/uncrustify

commit 2194f946193ce5460470f07c7ab84f095dde96fa

C#: support exception filters in try catch new optional keyword when after catch fixes bug 672

src/brace_cleanup.cpp
@@ -721,7 +721,7 @@ static bool check_complex_statements(struct parse_frame *frm, chunk_t *pc)
       {
          /* Replace CT_TRY with CT_CATCH on the stack & we are done */
          frm->pse[frm->pse_tos].type  = pc->type;
-         frm->pse[frm->pse_tos].stage = (pc->type == CT_CATCH) ? BS_OP_PAREN1 : BS_BRACE2;
+         frm->pse[frm->pse_tos].stage = (pc->type == CT_CATCH) ? BS_CATCH_WHEN : BS_BRACE2;
          print_stack(LBCSSWAP, "=Swap   ", frm, pc);
          return(true);
       }
@@ -732,6 +732,30 @@ static bool check_complex_statements(struct parse_frame *frm, chunk_t *pc)
       if (close_statement(frm, pc))
       {
          return(true);
+      }
+   }
+
+   /* Check for optional paren and optional CT_WHEN after CT_CATCH */
+   if (frm->pse[frm->pse_tos].stage == BS_CATCH_WHEN)
+   {
+      if (pc->type == CT_PAREN_OPEN) // this is for the paren after "catch"
+      {
+         /* Replace CT_PAREN_OPEN with CT_SPAREN_OPEN */
+         set_chunk_type(pc, CT_SPAREN_OPEN);
+         frm->pse[frm->pse_tos].type  = pc->type;
+         frm->pse[frm->pse_tos].stage = BS_PAREN1;
+         return(false);
+      }
+      else if (pc->type == CT_WHEN)
+      {
+         frm->pse[frm->pse_tos].type  = pc->type;
+         frm->pse[frm->pse_tos].stage = BS_OP_PAREN1;
+         return(true);
+      }
+      else if (pc->type == CT_BRACE_OPEN)
+      {
+         frm->pse[frm->pse_tos].stage = BS_BRACE2;
+         return(false);
       }
    }
 
@@ -816,8 +840,17 @@ static bool handle_complex_close(struct parse_frame *frm, chunk_t *pc)
 
    if (frm->pse[frm->pse_tos].stage == BS_PAREN1)
    {
-      /* PAREN1 always => BRACE2 */
-      frm->pse[frm->pse_tos].stage = BS_BRACE2;
+      if (pc->next->type == CT_WHEN)
+      {
+         frm->pse[frm->pse_tos].type = pc->type;
+         frm->pse[frm->pse_tos].stage = BS_CATCH_WHEN;
+         return true;
+      }
+      else
+      {
+         /* PAREN1 always => BRACE2 */
+         frm->pse[frm->pse_tos].stage = BS_BRACE2;
+      }
    }
    else if (frm->pse[frm->pse_tos].stage == BS_BRACE2)
    {

src/keywords.cpp
@@ -282,6 +282,7 @@ static const chunk_tag_t keywords[] =
    { "wchar",            CT_TYPE,         LANG_D                                                                      },
    { "wchar_t",          CT_TYPE,         LANG_C | LANG_CPP                                                           },
    { "weak",             CT_QUALIFIER,    LANG_VALA                                                                   },
+   { "when",             CT_WHEN,         LANG_CS                                                                     },
    { "while",            CT_WHILE,        LANG_ALL                                                                    }, // PAWN
    { "with",             CT_D_WITH,       LANG_D | LANG_ECMA                                                          },
    { "xor",              CT_SARITH,       LANG_C | LANG_CPP                                                           },

src/space.cpp
@@ -884,6 +884,12 @@ static argval_t do_space(chunk_t *first, chunk_t *second, int& min_sp, bool comp
    }
 
    if ((first->type == CT_PAREN_CLOSE) &&
+       (second->type == CT_WHEN))
+   {
+      return(AV_FORCE);
+   }
+
+   if ((first->type == CT_PAREN_CLOSE) &&
        ((second->type == CT_PAREN_OPEN) ||
         (second->type == CT_FPAREN_OPEN)))
    {

src/token_enum.h
@@ -132,6 +132,7 @@ typedef enum
    CT_ASM,
    CT_ATTRIBUTE,
    CT_CATCH,
+   CT_WHEN,
    CT_CLASS,
    CT_DELETE,
    CT_EXPORT,

src/uncrustify_types.h
@@ -44,6 +44,7 @@ enum brstage_e
    BS_ELSEIF,    /* expecting 'if' after 'else' */
    BS_WHILE,     /* expecting 'while' after 'do' */
    BS_CATCH,     /* expecting 'catch' or 'finally' after 'try' */
+   BS_CATCH_WHEN,  /* optional 'when' after 'catch' */
 };
 
 enum CharEncoding

tests/c-sharp.test
@@ -20,6 +20,7 @@
 
 10040 tcf.cfg                  cs/tcf.cs
 10041 tcf.cfg                  cs/gs.cs
+10042 ben.cfg                  cs/exception-filters.cs
 
 10050 cs_generics.cfg          cs/generics.cs
 

tests/input/cs/exception-filters.cs
@@ -1,0 +1,29 @@
+using System;
+class Test
+{
+void TestExceptionFilter()
+{
+try {
+  int i = 0;
+} catch (Exception e)
+{
+  int j = -1;
+}
+try {
+  int i = 0;
+} catch
+{
+  int j = -1;
+}
+try {
+  int i = 0;
+} catch when (DateTime.Now.DayOfWeek == DayOfWeek.Saturday)
+{
+  int j = -1;
+}
+try {
+  int i = 0;
+} catch (Exception e) when (DateTime.Now.DayOfWeek == DayOfWeek.Saturday)
+{
+  int j = -1;
+}}}

tests/output/cs/10042-exception-filters.cs
@@ -1,0 +1,39 @@
+using System;
+class Test
+{
+   void TestExceptionFilter()
+   {
+      try
+      {
+         int i = 0;
+      }
+      catch (Exception e)
+      {
+         int j = -1;
+      }
+      try
+      {
+         int i = 0;
+      }
+      catch
+      {
+         int j = -1;
+      }
+      try
+      {
+         int i = 0;
+      }
+      catch when (DateTime.Now.DayOfWeek == DayOfWeek.Saturday)
+      {
+         int j = -1;
+      }
+      try
+      {
+         int i = 0;
+      }
+      catch (Exception e) when (DateTime.Now.DayOfWeek == DayOfWeek.Saturday)
+      {
+         int j = -1;
+      }
+   }
+}

commit 5fe4bfee912212f49c00e31ec2f7b98373bcc7ee

correct brace cleanup for non-C# source files which was broken by 2194f94

src/brace_cleanup.cpp
@@ -760,9 +760,16 @@ static bool check_complex_statements(ParseFrame &frm, chunk_t *pc)
    {
       if (chunk_is_token(pc, CT_CATCH) || chunk_is_token(pc, CT_FINALLY))
       {
-         // Replace CT_TRY with CT_CATCH on the stack & we are done
+         // Replace CT_TRY with CT_CATCH or CT_FINALLY on the stack & we are done
          frm.top().type  = pc->type;
-         frm.top().stage = (chunk_is_token(pc, CT_CATCH)) ? brace_stage_e::CATCH_WHEN : brace_stage_e::BRACE2;
+         if (language_is_set(LANG_CS))
+		 {
+            frm.top().stage = (chunk_is_token(pc, CT_CATCH)) ? brace_stage_e::CATCH_WHEN : brace_stage_e::BRACE2;
+         }
+         else
+		 {
+            frm.top().stage = (chunk_is_token(pc, CT_CATCH)) ? brace_stage_e::OP_PAREN1 : brace_stage_e::BRACE2;
+         }
          print_stack(LBCSSWAP, "=Swap   ", frm);
 
          return(true);