1aa7340ba8ed7a523e782c7ed7752dd96005011a
Guy Maurel <g.maurel@agwest.de>
2018-08-09 15:49:58 +0200

    Test the change of newlines ending

---
+++ tests/expected/cpp/30072-lineEndings-Win.cpp
@@ -0,0 +1,9 @@
+int main ()
+{
+   a    = 5;
+   bbbb = 6.0;
+   int a      = 5;
+   float bbbb = 6.0;
+
+   bbbb = 1.0
+}
---
+++ tests/expected/cpp/30073-lineEndings-Unix.cpp
@@ -0,0 +1,1 @@
+int main (){   a    = 5;   bbbb = 6.0;   int a      = 5;   float bbbb = 6.0;   bbbb = 1.0}
