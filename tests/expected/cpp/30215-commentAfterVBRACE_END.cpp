af26861b097c1f514f37177c294ed3345c48b806
Guy Maurel <g.maurel@agwest.de>
2018-11-12 17:43:29 +0100

    update test data

--- tests/expected/cpp/30215-commentAfterVBRACE_END.cpp
+++ tests/expected/cpp/30215-commentAfterVBRACE_END.cpp
@@ -1,5 +1,8 @@
 void do_something(int foo, int foobar)
 {
-	if (foobar < foo)
-		foobar++;  /* comment after VBRACE_END with >1 spaces */
+        if (foobar < foo)
+                foobar++;  /* comment after VBRACE_END with >1 spaces */
+        if (foobar < foo) {
+                foobar++;  /* comment after ; */
+        }
 }
