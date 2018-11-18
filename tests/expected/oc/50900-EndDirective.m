df57e3385996dd6287ebe758ad9d0ab38c85405d
Gary Ash <gary.ash@icloud.com>
2018-11-17 22:49:08 -0500

    Added test to the option to force a newline after an  Objective C @end directive

---
+++ tests/expected/oc/50900-EndDirective.m
@@ -0,0 +1,35 @@
+#import "Fraction.h"
+#import <stdio.h>
+
+@implementation Fraction
+-(void) print
+{
+	printf("%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator: (int) n
+{
+	numerator = n;
+}
+
+-(void) setDenominator: (int) d
+{
+	denominator = d;
+}
+
+-(int) denominator
+{
+	return denominator;
+}
+
+-(int) numerator
+{
+	return numerator;
+}
+@end
+@implementation SimpleTest
+-(void) print
+{
+	printf("Hello");
+}
+@end
