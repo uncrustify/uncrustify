4a05c4dd3fc156826c506ede15f385a174e55564
Guy Maurel <g.maurel@agwest.de>
2018-11-28 16:48:36 +0100

    Provide test data for the option sp_oc_brace_catch

---
+++ tests/input/oc/1927.m
@@ -0,0 +1,17 @@
+- (void) foo:(NSString*) inString
+{
+   @try {
+   }@catch(NSException* const inException)
+   {
+   }
+}
+
+- (void) bar:(NSString*) inString
+{
+   @try {
+   }
+   @catch (NSException* const inException){
+   }
+   @finally {
+   }
+}
