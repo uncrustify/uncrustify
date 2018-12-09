c4612dbe7feaf3efdfc8c3c61ff89bc536753758
Guy Maurel <g.maurel@agwest.de>
2018-12-09 18:08:16 +0100

    update config and test files.

--- tests/expected/c/00076-1225.c
+++ tests/expected/c/00076-1225.c
@@ -1,7 +1,6 @@
-float        ign_guy()  {}
-a            alig_guy() {}
+float        ign_guy(){}
+a            alig_guy(){}
 int          align_guy(){}
 unsigned int align_here();
 int          this_works(int x);
-int          bug(int); // BUG: left-aligned
-
+int          bug(int);
--- tests/input/c/1225.c
+++ tests/input/c/1225.c
@@ -3,5 +3,4 @@ a alig_guy(){}
 int align_guy(){}
 unsigned int align_here();
 int this_works(int x);
-int bug(int); // BUG: left-aligned
-
+int bug(int);
