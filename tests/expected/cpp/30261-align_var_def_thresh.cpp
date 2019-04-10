c18cfd8e382307c72df9558a68f1253893dce474
Peter Bruin <peter@bruin.sg>
2019-04-10 16:04:27 +0800

    Amended test to show case the alignment bug
    Ref. #2254

--- tests/expected/cpp/30261-align_var_def_thresh.cpp
+++ tests/expected/cpp/30261-align_var_def_thresh.cpp
@@ -37,13 +37,14 @@ void testShortTypes()
 
 void testLongTypes()
 {
-	int      int_var;
-	int *    int_ptr_var;
-	int *    int_ptr_var;
-	float    float_var;
-	float &  float_ref_var;
-	float &  float_ref_var;
-	double & double_var;
+	int       int_var;
+	int *     int_ptr_var;
+	int *     int_ptr_var;
+	float     float_var;
+	float &   float_ref_var;
+	float &   float_ref_var;
+	double &  double_var;
+	long_type little_long_var;
 	SomeLongNamespace::SomeLongType long_var;
 	int * other_int_var;
 	SomeLooooongType long_var;
