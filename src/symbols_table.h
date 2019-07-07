96caba9da69a81e0fb5a3d20052a12236f268a7b
Guy Maurel <g.maurel@agwest.de>
2019-07-07 10:12:15 +0200

    The symbol "->*" is written twice. Typo.

--- src/symbols_table.h
+++ src/symbols_table.h
@@ -55,7 +55,6 @@ static const chunk_tag_t symbols3[] =
    { "===",      CT_COMPARE,      LANG_D | LANG_ECMA                                           },
    { ">>=",      CT_ASSIGN,       LANG_ALL                                                     },
    { ">>>",      CT_ARITH,        LANG_D | LANG_JAVA | LANG_PAWN | LANG_ECMA                   },
-   { "->*",      CT_MEMBER,       LANG_C | LANG_CPP | LANG_OC | LANG_D                         },
    { "%:@",      CT_POUND,        LANG_C | LANG_CPP | LANG_OC                                  },           // digraph  #@ MS extension
    { R"_(??=)_", CT_POUND,        LANG_C | LANG_CPP | LANG_OC | FLAG_DIG                       },           // trigraph #
    { R"_(??()_", CT_SQUARE_OPEN,  LANG_C | LANG_CPP | LANG_OC | FLAG_DIG                       },           // trigraph [
