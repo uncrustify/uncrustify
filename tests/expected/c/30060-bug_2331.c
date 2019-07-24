ed7a9d72de826ab8d90153b7d275b6072951d303
Piet Van Reepingen <vnt@melexis.com>
2019-07-24 08:39:13 +0200

    Add test for sp_before_square_ignore_asm_block
    Boolean option sp_before_square_ignore_asm_block is added
    to be able to ignore sp_before_square for asm macros.
    The difference between the normal and ignored case is tested.
    
    Ref. #2331

---
+++ tests/expected/c/30060-bug_2331.c
@@ -0,0 +1,11 @@
+#define SET_STACK(stack)            \
+	do {                            \
+		__asm__ __volatile__ (      \
+			"mov S, %[oper]"        \
+			:                       \
+			: [oper] "r" (stack)    \
+			: "S"                   \
+			);                      \
+	} while(0)
+
+int somearray[10];
