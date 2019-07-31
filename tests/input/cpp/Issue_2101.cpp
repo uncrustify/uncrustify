2e8d2b0a97103c8b0889a3779a180ade8db18c09
Guy Maurel <g.maurel@agwest.de>
2019-07-31 17:27:34 +0200

    Provive test data

---
+++ tests/input/cpp/Issue_2101.cpp
@@ -0,0 +1,5 @@
+void Test()
+{
+    aaaaaa = condition ? FunctionWithVeryLongName( andWithVeryLongArgumentsToo1, andWithVeryLongArgumentsToo2 )
+                       : FunctionWithVeryLongName( andWithVeryLongArgumentsToo2, andWithVeryLongArgumentsToo1 );
+}
