2ceae273a1ddf84ef4681d4e1ff5887a575f65fb
Dan Rose <dan@digilabs.io>
2019-08-03 21:10:42 -0500

    Add tests for multiline sparen

---
+++ tests/expected/c/09617-issue_2360.c
@@ -0,0 +1,44 @@
+int short_function();
+int some_very_very_very_very_very_very_very_very_long_function();
+int main() {
+	// short condition, no existing newlines
+	if (short_function()) {}
+
+	// short condition, existing newlines
+	if (
+		short_function()
+		) {}
+
+	// long condition, no newlines
+	if (
+		some_very_very_very_very_very_very_very_very_long_function() &&
+		some_very_very_very_very_very_very_very_very_long_function
+		) {}
+
+	// long condition, newlines
+	else if (
+		some_very_very_very_very_very_very_very_very_long_function() &&
+		some_very_very_very_very_very_very_very_very_long_function()
+		) {}
+
+	// switch condition
+	switch (
+		some_very_very_very_very_very_very_very_very_long_function() &&
+		some_very_very_very_very_very_very_very_very_long_function()
+		) {
+	case default: break;
+	}
+
+	// while condition, no newlines
+	while (
+		some_very_very_very_very_very_very_very_very_long_function() ||
+		some_very_very_very_very_very_very_very_very_long_function()
+		) {}
+
+	// for condition, no newlines
+	for (
+		some_very_very_very_very_very_very_very_very_long_function();
+		some_very_very_very_very_very_very_very_very_long_function();
+		some_very_very_very_very_very_very_very_very_long_function()
+		) {}
+}
