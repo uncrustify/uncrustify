a115bd23ecccb10f5929bd73df6aba393c0fd7de
Matthew Woehlke <matthew.woehlke@kitware.com>
2019-10-14 12:29:06 -0400

    Use enum flags type for token flags
    Replace PCF_* preprocessor defines with an enumeration. Use a proper
    flags type for dealing with these flags, rather than a bare integer
    type.
    
    This breaks a ton of code, which will be fixed in the next commits.

--- src/enum_flags.h
+++ src/enum_flags.h
@@ -18,7 +18,7 @@
 #endif
 
 #define UNC_DECLARE_FLAGS(flag_type, enum_type) \
-   using flag_type = flags<enum_type>
+   using flag_type = ::uncrustify::flags<enum_type>
 
 #define UNC_DECLARE_OPERATORS_FOR_FLAGS(flag_type)                        \
    inline flag_type operator&(flag_type::enum_t f1, flag_type::enum_t f2) \
