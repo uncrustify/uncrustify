720919436d2fd32e60ceda662e59b29483692e66
Samish Chandra Kolli <samishchandra@gmail.com>
2020-01-31 13:34:10 -0800

    Add LANG_OC for handling option sp_angle_shift
    Space inside angle shifts are correctly handled in `LANG_JAVA`, `LANG_CS` and `LANG_CPP` with option `sp_permit_cpp11_shift`. ObjectiveC language also need to support this option.

---
+++ tests/expected/oc/50904-double_angle_space.m
@@ -0,0 +1,25 @@
+static const NSArray< id< NSObject>> **controllers = nil;
+
+NSArray< id< BlockController>> *someMethod();
+
+@interface Fraction : NSObject
+	void Compute(
+		Image< E::Matrix<SType, Dim,1>> const& src,
+		Image<E::Matrix<TType,Dim,1>>& dst);
+@end
+@implementation SomeClass
+- (void)initializeControllers:( NSArray< id< BlockController>> *)hybridContollers {
+	if (index < children.count) {
+		const unsigned int wl = w>>lvl;
+
+		assert(x<0 && y>=3);
+		assert(y <0&&z> 2);
+		assert(a>>1);
+		assert(b >>1);
+
+		return static_cast< id <CKMountable>>(children[index]);
+	}
+
+	NSArray<id< BlockController>> *controllers = hybridContollers;
+}
+@end
