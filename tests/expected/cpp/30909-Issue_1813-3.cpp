3e6454d0f81025a5e3e8196ed1544a94549596ff
Guy Maurel <guy.j@maurel.de>
2020-01-31 19:00:31 +0100

    indentation of a lambda body may not depends on how many nested namespaces

--- tests/expected/cpp/30909-Issue_1813-3.cpp
+++ tests/expected/cpp/30909-Issue_1813-3.cpp
@@ -2,11 +2,11 @@ namespace n1 {
 namespace n2 {
 namespace n3 {
 
-  void func() {
-    another_func([]() {
-      return 42;
-    });
-  }
+   void func() {
+      another_func([]() {
+         return 42;
+      });
+   }
 
 }
 }
