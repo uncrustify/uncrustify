57f108deadfb9980040cdb183c059eab87b3021a
Samish Chandra Kolli <samishchandra@gmail.com>
2020-02-02 19:42:17 -0800

    Fix issue 2623 with nested template syntax
    The template token parsing logic has a bug when `static_cast` is present. `static_cast` already takes care of the angle_close token, hence during the check for template we need to take one less angle_close into account.
    
    `tok_split_gte` option is added as a hack to parse nested template syntax like `Class1<Class2<Class3>>>`, but it breaks the simple syntax `assert(x<0 && y>=3)`. This commit correctly fixes the template parsing logic, hence the option `tok_split_gte` option may become obsolete and can be removed.

---
+++ tests/expected/cpp/34324-issue_2623.cpp
@@ -0,0 +1,30 @@
+void child() {
+	static_cast< id<Mountable >> ( object);
+}
+
+assert(x<0 && y>=3);
+assert(y <0&&z> 2);
+assert(a>>1);
+
+std::unique_ptr<Interface< T >> GetProjectionAdapter(const std::string& model_name);
+
+auto c = a< b>>c;
+auto c = a << b >>c;
+
+if (Something<a> == c) {
+}
+
+if (id< Something<a >> == c) {
+}
+
+const std::vector< Eigen::Matrix<T, A, B >> & P_c;
+
+const unsigned int wl = w>> lvl;
+
+using Poly = Model<P, Poly<Dx,Dy, Dz>>;
+
+void Compute(
+	Image<E::Matrix< SType, Dim,Int >> const& src,
+	Image< E::Matrix<  TType,Dim, std::string> >& dst);
+
+Opt<std::vector < std::unordered_set<FrameId> >> partition;
