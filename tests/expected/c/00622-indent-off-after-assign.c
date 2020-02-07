94f18ac9c0c7c7fab64f14425aea69f0f49bb240
Samish Chandra Kolli <samishchandra@gmail.com>
2020-02-06 23:01:22 -0800

    Add option indent_off_after_assign to turn off indent similar to indent_off_after_return_new
    The added option will help turn off indentation after assign expression. This is how Xcode does formatting by default. Hence this option would provide a great value in staying close to regular formatting rules which most people use.

---
+++ tests/expected/c/00622-indent-off-after-assign.c
@@ -0,0 +1,29 @@
+void foo(void)
+{
+	int a;
+	int b =
+	veryLongMethodCall(
+		arg1,
+		longMethodCall(
+			arg2,
+			methodCall(
+				arg3, arg4
+				)
+			)
+		);
+	junk(a =
+	     3);
+}
+
+void f()
+{
+	int x = size_t(1.0) +
+	2;
+	int y = (size_t(1.0) +
+	         5);
+
+	int z =
+	size_t(1.0)
+	+ 5
+	+ size_t(2.0);
+}
