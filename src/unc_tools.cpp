207d475b8273d33cb4475cad206e77f3f853d10b
Guy Maurel <guy.j@maurel.de>
2020-03-17 14:48:12 +0100

    correct the comment

--- src/unc_tools.cpp
+++ src/unc_tools.cpp
@@ -30,19 +30,20 @@ static size_t counter = 0;
 static size_t tokenCounter;
 
 
-// protocol of the line
-// examples:
-//   prot_the_line(__func__, __LINE__, pc->orig_line);
-//   prot_the_line(__func__, __LINE__, 0, 0);
-//   prot_the_line(__func__, __LINE__, 6, 5);
-//   prot_the_source(__LINE__);
-//   log_pcf_flags(LSYS, pc->flags);
+/* protocol of the line
+ * examples:
+ *   prot_the_line(__func__, __LINE__, pc->orig_line);
+ *   prot_the_line(__func__, __LINE__, 0, 0);
+ *   prot_the_line(__func__, __LINE__, 6, 5);
+ *   prot_the_source(__LINE__);
+ *   log_pcf_flags(LSYS, pc->flags);
 
-// if actual_line is zero, use the option debug_line_number_to_protocol.
-// if the value is zero, don't make any protocol and return.
+ * if actual_line is zero, use the option debug_line_number_to_protocol.
+ * if the value is zero, don't make any protocol and return.
 
-// if partNumber is zero, all the tokens of the line are shown,
-// if partNumber is NOT zero, only the token with this partNumber is shown.
+ * if partNumber is zero, all the tokens of the line are shown,
+ * if partNumber is NOT zero, only the token with this partNumber is shown.
+ */
 void prot_the_line(const char *func_name, int theLine, unsigned int actual_line, size_t partNumber)
 {
    if (actual_line == 0)
