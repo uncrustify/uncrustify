94e283fcc2454795211002b726efe32bf0c4e12d
Markus Geimer <m.geimer@fz-juelich.de>
2020-03-19 11:03:04 +0100

    Add C test for 'nl_before_return' in switch/case

---
+++ tests/expected/c/00206-case-nl_before_return.c
@@ -0,0 +1,36 @@
+int foo(int arg)
+{
+	switch (arg)
+	{
+	case 0: return 1;
+	case 1:
+		return 2;
+	case 2:
+		printf("Hello world!\n");
+
+		return 3;
+	case 3:
+	{
+		int a = 4;
+
+		return a;
+	}
+	case 4:
+
+		return 5;
+	case 5:
+		printf("Hello world!\n");
+
+		return 6;
+	case 6:
+	{
+		int a = 7;
+
+		return a;
+	}
+	default:
+		return arg++;
+	}
+
+	return 0;
+}
