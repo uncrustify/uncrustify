12ec0c626b948df044ed7066c3b1b15bfd5a63a7
Guy Maurel <guy.j@maurel.de>
2020-09-25 17:55:22 +0200

    provide test data

---
+++ tests/expected/cpp/30266-Issue_2921.cpp
@@ -0,0 +1,34 @@
+namespace Namespace1
+{
+namespace Namespace2
+{
+namespace Namespace3
+{
+namespace Namespace4
+{
+namespace Namespace5
+{
+namespace Namespace6
+{
+namespace Namespace7
+{
+namespace Namespace8
+{
+class ClassName
+{
+public:
+ClassName(int a,
+          int b);
+
+private:
+int a;
+int b;
+};
+}
+}
+}
+}
+}
+}
+}
+}
