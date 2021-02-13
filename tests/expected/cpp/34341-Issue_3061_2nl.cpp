uncrustify/uncrustify

commit 313f6eb72038d0d95af2283553e3d1274ad9364f

Fix eat_blanks_after_open_brace for line-wrapping
This commit fixes issues where the eat_blanks_after_open_brace
option, if true, would ordinarily remove blanks following an
open brace - if not for line-wrapping imposed by code-width
constraints (non-zero code_width is specified). After this
commit, blanks are removed (if eat_blanks_after_open_brace=true)
regardless of whether or not any line-wrapping actually occurs.

src/newlines.cpp
@@ -2463,7 +2463,7 @@ static void newlines_brace_pair(chunk_t *br_open)
          log_rule_B("nl_inside_namespace");
 
          if (  options::nl_inside_empty_func() > 0
-            && chunk_is_token(chunk_get_next_ncnl(br_open), CT_BRACE_CLOSE)
+            && chunk_is_token(chunk_get_next_nnl(br_open), CT_BRACE_CLOSE)
             && (  get_chunk_parent_type(br_open) == CT_FUNC_CLASS_DEF
                || get_chunk_parent_type(br_open) == CT_FUNC_DEF))
          {
@@ -3630,6 +3630,32 @@ void newlines_remove_newlines(void)
 }
 
 
+void newlines_remove_disallowed()
+{
+   LOG_FUNC_ENTRY();
+
+   auto *pc = chunk_get_head();
+
+   while ((pc = chunk_get_next_nl(pc)) != nullptr)
+   {
+      LOG_FMT(LBLANKD, "%s(%d): orig_line is %zu, orig_col is %zu, <Newline>, nl is %zu\n",
+              __func__, __LINE__, pc->orig_line, pc->orig_col, pc->nl_count);
+
+      if (!can_increase_nl(pc))
+      {
+         LOG_FMT(LBLANKD, "%s(%d): force to 1 orig_line is %zu, orig_col is %zu\n",
+                 __func__, __LINE__, pc->orig_line, pc->orig_col);
+
+         if (pc->nl_count != 1)
+         {
+            pc->nl_count = 1;
+            MARK_CHANGE();
+         }
+      }
+   }
+}
+
+
 void newlines_cleanup_angles()
 {
    // Issue #1167
@@ -4117,7 +4143,7 @@ void newlines_cleanup_braces(bool first)
                log_rule_B("nl_inside_empty_func");
 
                if (  options::nl_inside_empty_func() > 0
-                  && chunk_is_token(chunk_get_prev_ncnlni(pc), CT_BRACE_OPEN)
+                  && chunk_is_token(chunk_get_prev_nnl(pc), CT_BRACE_OPEN)
                   && (  get_chunk_parent_type(pc) == CT_FUNC_CLASS_DEF
                      || get_chunk_parent_type(pc) == CT_FUNC_DEF))
                {

tests/cpp.test
@@ -789,6 +789,7 @@
 34252  nl_after_func_proto_group-3.cfg      cpp/issue_2001.cpp
 34253  nl_after_func_proto_group-3.cfg      cpp/friends.cpp
 34254  issue_1985.cfg                       cpp/issue_1985.cpp
+34255  eat_blanks_after_codewidth.cfg       cpp/eat_blanks_after_codewidth.cpp
 
 34280  UNI-29935.cfg                        cpp/UNI-29935.cpp
 

src/newlines.h
@@ -23,6 +23,13 @@ void double_newline(chunk_t *nl);
  */
 void newlines_remove_newlines(void);
 
+
+/**
+ * Remove all newlines that fail the checks performed by the can_increase_nl() function
+ */
+void newlines_remove_disallowed();
+
+
 /** Step through all chunks, altering newlines inside parens of if/for/while/do as needed.
  * Handles the style options: nl_multi_line_sparen_open, nl_multi_line_sparen_close, nl_before_if_closing_paren
  */

src/options.h
@@ -2517,7 +2517,8 @@ nl_inside_empty_func;
 extern BoundedOption<unsigned, 0, 16>
 nl_before_func_body_proto;
 
-// The number of newlines before a multi-line function definition.
+// The number of newlines before a multi-line function definition. Where
+// applicable, this option is overridden with eat_blanks_after_open_brace=true
 extern BoundedOption<unsigned, 0, 16>
 nl_before_func_body_def;
 

src/uncrustify.cpp
@@ -2217,7 +2217,7 @@ void uncrustify_file(const file_mem &fm, FILE *pfout,
             {
                if (cpd.changes > options::debug_max_number_of_loops())                 // Issue #2432
                {
-                  LOG_FMT(LNEWLINE, "%s(%d): too many loop. Make a report, please.\n",
+                  LOG_FMT(LNEWLINE, "%s(%d): too many loops. Make a report, please.\n",
                           __func__, __LINE__);
                   log_flush(true);
                   exit(EX_SOFTWARE);
@@ -2231,6 +2231,7 @@ void uncrustify_file(const file_mem &fm, FILE *pfout,
                newlines_cleanup_braces(false);
                newlines_insert_blank_lines();
                newlines_functions_remove_extra_blank_lines();
+               newlines_remove_disallowed();
                first = false;
             }
          }

tests/cli/output/universalindent.cfg
@@ -4626,7 +4626,7 @@ ValueDefault=0
 
 [Nl Before Func Body Def]
 Category=4
-Description="<html>The number of newlines before a multi-line function definition.</html>"
+Description="<html>The number of newlines before a multi-line function definition. Where<br/>applicable, this option is overridden with eat_blanks_after_open_brace=true</html>"
 Enabled=false
 EditorType=numeric
 CallName="nl_before_func_body_def="

tests/config/eat_blanks_after_codewidth.cfg
@@ -1,0 +1,11 @@
+cmt_width=30
+code_width=30
+eat_blanks_after_open_brace=true
+nl_inside_empty_func=2
+nl_fdef_brace=force
+indent_columns=4
+indent_class=true
+indent_with_tabs=0
+input_tab_size=4
+nl_func_type_name=force
+nl_before_func_body_def=2

tests/expected/cpp/34255-eat_blanks_after_codewidth.cpp
@@ -1,0 +1,15 @@
+class A
+{
+    void
+    func1()
+    {
+        // comment
+    }
+
+    void
+    func2()
+    {
+        auto result = 1 + 2 +
+                      3 + 4;
+    }
+};

commit 24a7efd6936e037c5dc1249928ef14d069bf6ca3

Fix issue 3061
This issue was introduced by commit 313f6eb7.

'void newlines_remove_disallowed()' should not eat new line characters
in the following cases:
1. we are at the end of the file
2. there are consecutive new line characters
because that is already dealt with in a different part of the code.

Signed-off-by: Michele Calgaro <michele.calgaro@yahoo.it>

src/newlines.cpp
@@ -3647,14 +3647,19 @@ void newlines_remove_disallowed()
 {
    LOG_FUNC_ENTRY();
 
-   auto *pc = chunk_get_head();
+   chunk_t *pc = chunk_get_head();
+   chunk_t *next;
 
    while ((pc = chunk_get_next_nl(pc)) != nullptr)
    {
       LOG_FMT(LBLANKD, "%s(%d): orig_line is %zu, orig_col is %zu, <Newline>, nl is %zu\n",
               __func__, __LINE__, pc->orig_line, pc->orig_col, pc->nl_count);
 
-      if (!can_increase_nl(pc))
+      next = chunk_get_next(pc);
+
+      if (  next != nullptr
+         && !chunk_is_token(next, CT_NEWLINE)
+         && !can_increase_nl(pc))
       {
          LOG_FMT(LBLANKD, "%s(%d): force to 1 orig_line is %zu, orig_col is %zu\n",
                  __func__, __LINE__, pc->orig_line, pc->orig_col);

tests/config/Issue_3061_0nl.cfg
@@ -1,0 +1,13 @@
+cmt_width=30
+code_width=30
+eat_blanks_after_open_brace=true
+nl_inside_empty_func=2
+nl_fdef_brace=force
+indent_columns=4
+indent_class=true
+indent_with_tabs=0
+input_tab_size=4
+nl_func_type_name=force
+nl_before_func_body_def=2
+nl_end_of_file=remove
+

tests/config/Issue_3061_1nl.cfg
@@ -1,0 +1,14 @@
+cmt_width=30
+code_width=30
+eat_blanks_after_open_brace=true
+nl_inside_empty_func=2
+nl_fdef_brace=force
+indent_columns=4
+indent_class=true
+indent_with_tabs=0
+input_tab_size=4
+nl_func_type_name=force
+nl_before_func_body_def=2
+nl_end_of_file=force
+nl_end_of_file_min=1
+

tests/config/Issue_3061_2nl.cfg
@@ -1,0 +1,14 @@
+cmt_width=30
+code_width=30
+eat_blanks_after_open_brace=true
+nl_inside_empty_func=2
+nl_fdef_brace=force
+indent_columns=4
+indent_class=true
+indent_with_tabs=0
+input_tab_size=4
+nl_func_type_name=force
+nl_before_func_body_def=2
+nl_end_of_file=force
+nl_end_of_file_min=2
+

tests/config/Issue_3061_3nl.cfg
@@ -1,0 +1,14 @@
+cmt_width=30
+code_width=30
+eat_blanks_after_open_brace=true
+nl_inside_empty_func=2
+nl_fdef_brace=force
+indent_columns=4
+indent_class=true
+indent_with_tabs=0
+input_tab_size=4
+nl_func_type_name=force
+nl_before_func_body_def=2
+nl_end_of_file=force
+nl_end_of_file_min=3
+

tests/cpp.test
@@ -838,6 +838,19 @@
 34327  Issue_3044.cfg                       cpp/Issue_3044.cpp
 34328  Issue_3048.cfg                       cpp/Issue_3048.cpp
 
+34330  Issue_3061_0nl.cfg                   cpp/Issue_3061_0nl.cpp
+34331  Issue_3061_0nl.cfg                   cpp/Issue_3061_1nl.cpp
+34332  Issue_3061_0nl.cfg                   cpp/Issue_3061_2nl.cpp
+34333  Issue_3061_1nl.cfg                   cpp/Issue_3061_0nl.cpp
+34334  Issue_3061_1nl.cfg                   cpp/Issue_3061_1nl.cpp
+34335  Issue_3061_1nl.cfg                   cpp/Issue_3061_2nl.cpp
+34336  Issue_3061_2nl.cfg                   cpp/Issue_3061_0nl.cpp
+34337  Issue_3061_2nl.cfg                   cpp/Issue_3061_1nl.cpp
+34338  Issue_3061_2nl.cfg                   cpp/Issue_3061_2nl.cpp
+34339  Issue_3061_3nl.cfg                   cpp/Issue_3061_0nl.cpp
+34340  Issue_3061_3nl.cfg                   cpp/Issue_3061_1nl.cpp
+34341  Issue_3061_3nl.cfg                   cpp/Issue_3061_2nl.cpp
+
 # Adopt some UT tests
 10000  empty.cfg                            cpp/621_this-spacing.cpp
 10001  empty.cfg                            cpp/622_ifdef-indentation.cpp

tests/expected/cpp/34330-Issue_3061_0nl.cpp
@@ -1,0 +1,14 @@
+DCOPClient::DCOPClient()
+{
+    TQObject::connect(
+        &d->postMessageTimer, TQT_SIGNAL(
+            timeout()), this,
+        TQT_SLOT(
+            processPostedMessagesInternal()));
+    TQObject::connect(
+        &d->eventLoopTimer, TQT_SIGNAL(
+            timeout()), this, TQT_SLOT(
+            eventLoopTimeout()));
+}
+
+#include <dcopclient.moc>

tests/expected/cpp/34331-Issue_3061_1nl.cpp
@@ -1,0 +1,14 @@
+DCOPClient::DCOPClient()
+{
+    TQObject::connect(
+        &d->postMessageTimer, TQT_SIGNAL(
+            timeout()), this,
+        TQT_SLOT(
+            processPostedMessagesInternal()));
+    TQObject::connect(
+        &d->eventLoopTimer, TQT_SIGNAL(
+            timeout()), this, TQT_SLOT(
+            eventLoopTimeout()));
+}
+
+#include <dcopclient.moc>

tests/expected/cpp/34332-Issue_3061_2nl.cpp
@@ -1,0 +1,14 @@
+DCOPClient::DCOPClient()
+{
+    TQObject::connect(
+        &d->postMessageTimer, TQT_SIGNAL(
+            timeout()), this,
+        TQT_SLOT(
+            processPostedMessagesInternal()));
+    TQObject::connect(
+        &d->eventLoopTimer, TQT_SIGNAL(
+            timeout()), this, TQT_SLOT(
+            eventLoopTimeout()));
+}
+
+#include <dcopclient.moc>

tests/expected/cpp/34333-Issue_3061_0nl.cpp
@@ -1,0 +1,14 @@
+DCOPClient::DCOPClient()
+{
+    TQObject::connect(
+        &d->postMessageTimer, TQT_SIGNAL(
+            timeout()), this,
+        TQT_SLOT(
+            processPostedMessagesInternal()));
+    TQObject::connect(
+        &d->eventLoopTimer, TQT_SIGNAL(
+            timeout()), this, TQT_SLOT(
+            eventLoopTimeout()));
+}
+
+#include <dcopclient.moc>

tests/expected/cpp/34334-Issue_3061_1nl.cpp
@@ -1,0 +1,14 @@
+DCOPClient::DCOPClient()
+{
+    TQObject::connect(
+        &d->postMessageTimer, TQT_SIGNAL(
+            timeout()), this,
+        TQT_SLOT(
+            processPostedMessagesInternal()));
+    TQObject::connect(
+        &d->eventLoopTimer, TQT_SIGNAL(
+            timeout()), this, TQT_SLOT(
+            eventLoopTimeout()));
+}
+
+#include <dcopclient.moc>

tests/expected/cpp/34335-Issue_3061_2nl.cpp
@@ -1,0 +1,14 @@
+DCOPClient::DCOPClient()
+{
+    TQObject::connect(
+        &d->postMessageTimer, TQT_SIGNAL(
+            timeout()), this,
+        TQT_SLOT(
+            processPostedMessagesInternal()));
+    TQObject::connect(
+        &d->eventLoopTimer, TQT_SIGNAL(
+            timeout()), this, TQT_SLOT(
+            eventLoopTimeout()));
+}
+
+#include <dcopclient.moc>

tests/expected/cpp/34336-Issue_3061_0nl.cpp
@@ -1,0 +1,15 @@
+DCOPClient::DCOPClient()
+{
+    TQObject::connect(
+        &d->postMessageTimer, TQT_SIGNAL(
+            timeout()), this,
+        TQT_SLOT(
+            processPostedMessagesInternal()));
+    TQObject::connect(
+        &d->eventLoopTimer, TQT_SIGNAL(
+            timeout()), this, TQT_SLOT(
+            eventLoopTimeout()));
+}
+
+#include <dcopclient.moc>
+

tests/expected/cpp/34337-Issue_3061_1nl.cpp
@@ -1,0 +1,15 @@
+DCOPClient::DCOPClient()
+{
+    TQObject::connect(
+        &d->postMessageTimer, TQT_SIGNAL(
+            timeout()), this,
+        TQT_SLOT(
+            processPostedMessagesInternal()));
+    TQObject::connect(
+        &d->eventLoopTimer, TQT_SIGNAL(
+            timeout()), this, TQT_SLOT(
+            eventLoopTimeout()));
+}
+
+#include <dcopclient.moc>
+

tests/expected/cpp/34338-Issue_3061_2nl.cpp
@@ -1,0 +1,15 @@
+DCOPClient::DCOPClient()
+{
+    TQObject::connect(
+        &d->postMessageTimer, TQT_SIGNAL(
+            timeout()), this,
+        TQT_SLOT(
+            processPostedMessagesInternal()));
+    TQObject::connect(
+        &d->eventLoopTimer, TQT_SIGNAL(
+            timeout()), this, TQT_SLOT(
+            eventLoopTimeout()));
+}
+
+#include <dcopclient.moc>
+

tests/expected/cpp/34339-Issue_3061_0nl.cpp
@@ -1,0 +1,16 @@
+DCOPClient::DCOPClient()
+{
+    TQObject::connect(
+        &d->postMessageTimer, TQT_SIGNAL(
+            timeout()), this,
+        TQT_SLOT(
+            processPostedMessagesInternal()));
+    TQObject::connect(
+        &d->eventLoopTimer, TQT_SIGNAL(
+            timeout()), this, TQT_SLOT(
+            eventLoopTimeout()));
+}
+
+#include <dcopclient.moc>
+
+

tests/expected/cpp/34340-Issue_3061_1nl.cpp
@@ -1,0 +1,16 @@
+DCOPClient::DCOPClient()
+{
+    TQObject::connect(
+        &d->postMessageTimer, TQT_SIGNAL(
+            timeout()), this,
+        TQT_SLOT(
+            processPostedMessagesInternal()));
+    TQObject::connect(
+        &d->eventLoopTimer, TQT_SIGNAL(
+            timeout()), this, TQT_SLOT(
+            eventLoopTimeout()));
+}
+
+#include <dcopclient.moc>
+
+

tests/expected/cpp/34341-Issue_3061_2nl.cpp
@@ -1,0 +1,16 @@
+DCOPClient::DCOPClient()
+{
+    TQObject::connect(
+        &d->postMessageTimer, TQT_SIGNAL(
+            timeout()), this,
+        TQT_SLOT(
+            processPostedMessagesInternal()));
+    TQObject::connect(
+        &d->eventLoopTimer, TQT_SIGNAL(
+            timeout()), this, TQT_SLOT(
+            eventLoopTimeout()));
+}
+
+#include <dcopclient.moc>
+
+

tests/input/cpp/Issue_3061_0nl.cpp
@@ -1,0 +1,8 @@
+DCOPClient::DCOPClient()
+{
+	TQObject::connect(&d->postMessageTimer, TQT_SIGNAL(timeout()), this,
+	        TQT_SLOT(processPostedMessagesInternal()));
+	TQObject::connect(&d->eventLoopTimer, TQT_SIGNAL(timeout()), this, TQT_SLOT(eventLoopTimeout()));
+}
+
+#include <dcopclient.moc>

tests/input/cpp/Issue_3061_1nl.cpp
@@ -1,0 +1,8 @@
+DCOPClient::DCOPClient()
+{
+	TQObject::connect(&d->postMessageTimer, TQT_SIGNAL(timeout()), this,
+	        TQT_SLOT(processPostedMessagesInternal()));
+	TQObject::connect(&d->eventLoopTimer, TQT_SIGNAL(timeout()), this, TQT_SLOT(eventLoopTimeout()));
+}
+
+#include <dcopclient.moc>

tests/input/cpp/Issue_3061_2nl.cpp
@@ -1,0 +1,9 @@
+DCOPClient::DCOPClient()
+{
+	TQObject::connect(&d->postMessageTimer, TQT_SIGNAL(timeout()), this,
+	        TQT_SLOT(processPostedMessagesInternal()));
+	TQObject::connect(&d->eventLoopTimer, TQT_SIGNAL(timeout()), this, TQT_SLOT(eventLoopTimeout()));
+}
+
+#include <dcopclient.moc>
+