b8a046caa6f0eb0eea1cee9ed72a75ec4ac2c13d
Guy Maurel <guy.j@maurel.de>
2021-02-17 18:13:29 +0100

    Transform from array to vector

--- src/align_func_proto.cpp
+++ src/align_func_proto.cpp
@@ -40,39 +40,31 @@ void align_func_proto(size_t span)
    //   and
    //   token-1->brace_level == token-2->brace_level
    // we don't check if token-1 and token-2 are in the same block
-   size_t max_level_is       = 0;
-   size_t max_brace_level_is = 0;
 
    log_rule_B("align_var_def_star_style");
    size_t mystar_style = options::align_var_def_star_style();
 
    log_rule_B("align_var_def_amp_style");
-   size_t       myamp_style = options::align_var_def_amp_style();
+   size_t myamp_style = options::align_var_def_amp_style();
 
-   const size_t max_level_count = 16;
-   const size_t max_brace_level = 16;
 
-   AlignStack   many_as[max_level_count + 1][max_brace_level + 1];
+   size_t     num_of_column     = 1;
+   size_t     num_of_row        = 1;
+   AlignStack *stack_init_value = nullptr;
 
+
+   // Issue #2984
+   vector<vector<AlignStack *> > many_as;
    // Issue #2771
-   AlignStack many_as_brace[max_level_count + 1][max_brace_level + 1];
+   vector<vector<AlignStack *> > many_as_brace;
+
+   // init the vector ...
+   many_as.resize(num_of_column, vector<AlignStack *>(num_of_row, stack_init_value));
+   many_as_brace.resize(num_of_column, vector<AlignStack *>(num_of_row, stack_init_value));
 
    log_rule_B("align_single_line_brace_gap");
    size_t mybr_gap = options::align_single_line_brace_gap();
 
-   for (size_t idx = 0; idx <= max_level_count; idx++)
-   {
-      for (size_t idx_brace = 0; idx_brace <= max_brace_level; idx_brace++)
-      {
-         many_as[idx][idx_brace].Start(myspan, mythresh);
-         many_as[idx][idx_brace].m_gap        = mygap;
-         many_as[idx][idx_brace].m_star_style = static_cast<AlignStack::StarStyle>(mystar_style);
-         many_as[idx][idx_brace].m_amp_style  = static_cast<AlignStack::StarStyle>(myamp_style);
-
-         many_as_brace[idx][idx_brace].Start(myspan, 0);
-         many_as_brace[idx][idx_brace].m_gap = mybr_gap;
-      }
-   }
 
    bool    look_bro = false;
    chunk_t *toadd;
@@ -84,38 +76,70 @@ void align_func_proto(size_t span)
               __func__, __LINE__, pc->orig_line, pc->orig_col, pc->elided_text(copy),
               get_token_name(pc->type), pc->level, pc->brace_level);
 
-      if (pc->level > max_level_count)                             // Issue #2960
+      // make the vector larger if necessary
+      if (  pc->level >= num_of_column                             // Issue #2960
+         || pc->brace_level >= num_of_row)
       {
-         fprintf(stderr, "%s(%d): pc->level is %zu. This is too big, at line %zu, column %zu. Make a report, please.\n",
-                 __func__, __LINE__, pc->level, pc->orig_line, pc->orig_col);
-         log_flush(true);
-         exit(EX_SOFTWARE);
-      }
+         num_of_column = pc->level + 1;
+         num_of_row    = pc->brace_level + 1;
 
-      if (pc->brace_level > max_brace_level)
-      {
-         fprintf(stderr, "%s(%d): pc->brace_level is %zu. This is too big, at line %zu, column %zu. Make a report, please.\n",
-                 __func__, __LINE__, pc->level, pc->orig_line, pc->orig_col);
-         log_flush(true);
-         exit(EX_SOFTWARE);
+         many_as.resize(num_of_column);
+         many_as_brace.resize(num_of_column);
+
+         for (size_t i = 0; i < num_of_column; ++i)
+         {
+            many_as[i].resize(num_of_row);
+            many_as_brace[i].resize(num_of_row);
+         }
       }
 
       if (  chunk_is_newline(pc)
          && !pc->flags.test(PCF_IN_FCN_CALL))                 // Issue #2831
       {
          look_bro = false;
-         many_as[pc->level][pc->brace_level].Debug();
-         many_as_brace[pc->level][pc->brace_level].Debug();
+         AlignStack *stack_at_l_bl = many_as.at(pc->level).at(pc->brace_level);
 
-         for (size_t idx = 0; idx <= max_level_count; idx++)
+         if (stack_at_l_bl == nullptr)
          {
-            for (size_t idx_brace = 0; idx_brace <= max_brace_level; idx_brace++)
+            // get a Stack
+            stack_at_l_bl = new AlignStack();
+            // start it
+            stack_at_l_bl->Start(myspan, mythresh);
+            stack_at_l_bl->m_gap        = mygap;
+            stack_at_l_bl->m_star_style = static_cast<AlignStack::StarStyle>(mystar_style);
+            stack_at_l_bl->m_amp_style  = static_cast<AlignStack::StarStyle>(myamp_style);
+            // store
+            many_as.at(pc->level).at(pc->brace_level) = stack_at_l_bl;
+         }
+         stack_at_l_bl->Debug();
+
+         for (size_t idx = 0; idx < num_of_column; idx++)
+         {
+            for (size_t idx_brace = 0; idx_brace < num_of_row; idx_brace++)
             {
-               many_as[idx][idx_brace].NewLines(pc->nl_count);
+               AlignStack *stack_at_l_bl = many_as.at(idx).at(idx_brace);
+
+               if (stack_at_l_bl != nullptr)
+               {
+                  stack_at_l_bl->NewLines(pc->nl_count);
+               }
             }
          }
 
-         many_as_brace[pc->level][pc->brace_level].NewLines(pc->nl_count);
+         AlignStack *stack_at_l_bl_brace = many_as_brace.at(pc->level).at(pc->brace_level);
+
+         if (stack_at_l_bl_brace == nullptr)
+         {
+            // get a Stack
+            stack_at_l_bl_brace = new AlignStack();
+            // start it
+            stack_at_l_bl_brace->Start(myspan, mythresh);
+            stack_at_l_bl_brace->m_gap = mybr_gap;
+            // store
+            many_as_brace.at(pc->level).at(pc->brace_level) = stack_at_l_bl_brace;
+         }
+         stack_at_l_bl_brace->Debug();
+         stack_at_l_bl_brace->NewLines(pc->nl_count);
       }
       else if (  chunk_is_token(pc, CT_FUNC_PROTO)
               || (  chunk_is_token(pc, CT_FUNC_DEF)
@@ -133,32 +157,26 @@ void align_func_proto(size_t span)
          {
             toadd = pc;
          }
-
-         if (pc->level > max_level_count)
-         {
-            fprintf(stderr, "%s(%d): Not enought memory for Stack\n",
-                    __func__, __LINE__);
-            fprintf(stderr, "%s(%d): the current maximum for level is %zu\n",
-                    __func__, __LINE__, max_level_count);
-            log_flush(true);
-         }
-
-         if (pc->level > max_brace_level)
-         {
-            fprintf(stderr, "%s(%d): Not enought memory for Stack\n",
-                    __func__, __LINE__);
-            fprintf(stderr, "%s(%d): the current maximum for brace_level is %zu\n",
-                    __func__, __LINE__, max_brace_level);
-            log_flush(true);
-            exit(EX_SOFTWARE);
-         }
          chunk_t *tmp = step_back_over_member(toadd);
          LOG_FMT(LAS, "%s(%d): tmp->text() is '%s', orig_line is %zu, orig_col is %zu, level is %zu, brace_level is %zu\n",
                  __func__, __LINE__, tmp->text(), tmp->orig_line, tmp->orig_col,
                  tmp->level, tmp->brace_level);
-         max_level_is       = max(max_level_is, pc->level);
-         max_brace_level_is = max(max_brace_level_is, pc->level);
-         many_as[pc->level][pc->brace_level].Add(tmp);
+         // test the Stack
+         AlignStack *stack_at_l_bl = many_as.at(pc->level).at(pc->brace_level);
+
+         if (stack_at_l_bl == nullptr)
+         {
+            // get a Stack
+            stack_at_l_bl = new AlignStack();
+            // start it
+            stack_at_l_bl->Start(myspan, mythresh);
+            stack_at_l_bl->m_gap        = mygap;
+            stack_at_l_bl->m_star_style = static_cast<AlignStack::StarStyle>(mystar_style);
+            stack_at_l_bl->m_amp_style  = static_cast<AlignStack::StarStyle>(myamp_style);
+            // store
+            many_as.at(pc->level).at(pc->brace_level) = stack_at_l_bl;
+         }
+         stack_at_l_bl->Add(tmp);
          log_rule_B("align_single_line_brace");
          look_bro = (chunk_is_token(pc, CT_FUNC_DEF))
                     && options::align_single_line_brace();
@@ -167,19 +185,35 @@ void align_func_proto(size_t span)
               && chunk_is_token(pc, CT_BRACE_OPEN)
               && pc->flags.test(PCF_ONE_LINER))
       {
-         many_as_brace[pc->level][pc->brace_level].Add(pc);
+         AlignStack *stack_at_l_bl_brace = many_as_brace.at(pc->level).at(pc->brace_level);
+         stack_at_l_bl_brace->Add(pc);
          look_bro = false;
       }
    }
 
    LOG_FMT(LAS, "%s(%d):  as\n", __func__, __LINE__);
 
-   for (size_t idx = 0; idx <= max_level_count; idx++)
+   // purge
+   for (size_t idx = 0; idx < num_of_column; idx++)
    {
-      for (size_t idx_brace = 0; idx_brace <= max_brace_level; idx_brace++)
+      for (size_t idx_brace = 0; idx_brace < num_of_row; idx_brace++)
       {
-         many_as[idx][idx_brace].End();
-         many_as_brace[idx][idx_brace].End();
+         AlignStack *stack_at_l_bl = many_as.at(idx).at(idx_brace);
+
+         if (stack_at_l_bl != nullptr)
+         {
+            stack_at_l_bl->End();
+            delete stack_at_l_bl;
+            stack_at_l_bl = nullptr;
+         }
+         AlignStack *stack_at_l_bl_brace = many_as_brace.at(idx).at(idx_brace);
+
+         if (stack_at_l_bl_brace != nullptr)
+         {
+            stack_at_l_bl_brace->End();
+            delete stack_at_l_bl_brace;
+            stack_at_l_bl_brace = nullptr;
+         }
       }
    }
 } // align_func_proto
