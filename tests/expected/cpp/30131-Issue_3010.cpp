0173b91cb696242d145992700890950299f42560
Guy Maurel <guy.j@maurel.de>
2021-03-05 16:47:02 +0100

    control indentation for SomeLongNamespaceName

---
+++ tests/expected/cpp/30131-Issue_3010.cpp
@@ -0,0 +1,16 @@
+namespace SomeLongNamespaceName {
+class Foo { };
+}
+
+class Bar : SomeLongNamespaceName::Foo {
+public:
+Bar()
+        : SomeLongNamespaceName::Foo(),
+        myNumber(3),      // <-- this line
+        myOtherNumber(5)
+{
+}
+private:
+int myNumber;
+int myOtherNumber;
+};
