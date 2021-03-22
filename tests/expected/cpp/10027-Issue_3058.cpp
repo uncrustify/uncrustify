bf4e64a687859475fd27f8a68719ec8c29f852b3
Guy Maurel <guy.j@maurel.de>
2021-03-22 17:54:44 +0100

    update test data

--- tests/expected/cpp/10027-Issue_3058.cpp
+++ tests/expected/cpp/10027-Issue_3058.cpp
@@ -1,21 +1,86 @@
-void main()
+KJS::Value KJS::KateJSViewProtoFunc::call(KJS::ExecState *exec, KJS::Object &thisObj, const KJS::List &args)
 {
-  switch( m_modOnHdReason )
+  switch (id)
   {
-    case CenteredAutoFit:
+    case KateJSView::SetCursorPositionReal:
     {
-      if( ww <= w && wh <= h ) {
-        d.setRect((w - ww) / 2, (h - wh) / 2, ww, wh);     // like Centered
-        break;
-      }
-      break;   // Just a comment
+      return KJS::Boolean( view->setCursorPositionReal( args[0].toUInt32(exec), args[1].toUInt32(exec) ) );
     }
 
-    case CenteredMaxpect:
+    // SelectionInterface goes in the view, in anticipation of the future
+    case KateJSView::Selection:
     {
-      d.setRect((w - ww) / 2, (h - wh) / 2, ww, wh);
-      break;
+      return KJS::String( view->selection() );
+    }
+  }
+
+  return KJS::Undefined();
+}
+
+void KateXmlIndent::getLineInfo (uint line, uint &prevIndent, int &numTags,
+                                 uint &attrCol, bool &unclosedTag)
+{
+  for(pos = 0; pos < len; ++pos) {
+    int ch = text.at(pos).unicode();
+    switch(ch) {
+      case '<':
+      {
+        ++numTags;
+        break;
+      }
+
+      // don't indent because of DOCTYPE, comment, CDATA, etc.
+      case '!':
+      {
+        if(lastCh == '<') --numTags;
+        break;
+      }
+
+      // don't indent because of xml decl or PI
+      case '?':
+      {
+        if(lastCh == '<') --numTags;
+        break;
+      }
     }
   }
 }
 
+static YYSIZE_T yytnamerr (char *yyres, const char *yystr)
+{
+  if (*yystr == '"')
+  {
+    for (;;)
+      switch (*++yyp)
+      {
+        case '\\':
+        {
+          if (*++yyp != '\\')
+            yyres[yyn] = *yyp;
+        }
+        /* Fall through.  */
+        default:
+        {
+          if (yyres)
+            yyres[yyn] = *yyp;
+          yyn++;
+          break;
+        }
+      }
+  }
+  return yystpcpy (yyres, yystr) - yyres;
+}
+
+Value RegExpProtoFuncImp::call(ExecState *exec, Object &thisObj, const List &args)
+{
+  if (!thisObj.inherits(&RegExpImp::info)) {
+    if (thisObj.inherits(&RegExpPrototypeImp::info)) {
+      switch (id) {
+        case ToString:
+        { return String("//");              // FireFox returns /(?:)/
+        }
+      }
+    }
+    return err;
+  }
+}
