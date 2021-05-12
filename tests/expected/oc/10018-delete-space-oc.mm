38f44f6163d52d48195efc804c3b68bfd0aa4a49
Guy Maurel <guy.j@maurel.de>
2021-05-12 10:33:16 +0200

    some more test data for the options sp_

--- tests/expected/oc/10018-delete-space-oc.mm
+++ tests/expected/oc/10018-delete-space-oc.mm
@@ -24,45 +24,45 @@ class HierarchyState;
 	IBOutlet id m_SelectAllItem;
 }
 
-- (IBAction)SaveAssets:(id)sender;
-- (IBAction)CloseScene:(id)sender;
-- (IBAction)NewProject:(id)sender;
-- (IBAction)OpenProject:(id)sender;
+- (IBAction)SaveAssets: (id)sender;
+- (IBAction)CloseScene: (id)sender;
+- (IBAction)NewProject: (id)sender;
+- (IBAction)OpenProject: (id)sender;
 
-- (IBAction)SaveAsSceneToDisk:(id)sender;
+- (IBAction)SaveAsSceneToDisk: (id)sender;
 
-- (IBAction)EnterSerialNumber:(id)sender;
-- (IBAction)ReturnLicense:(id)sender;
-- (IBAction)CompileScene:(id)sender;
-- (IBAction)CompileSceneAutomatic:(id)sender;
+- (IBAction)EnterSerialNumber: (id)sender;
+- (IBAction)ReturnLicense: (id)sender;
+- (IBAction)CompileScene: (id)sender;
+- (IBAction)CompileSceneAutomatic: (id)sender;
 
-- (IBAction)saveDocument:(id)sender;
+- (IBAction)saveDocument: (id)sender;
 
-- (IBAction)LoadSceneFromDisk:(id)sender;
+- (IBAction)LoadSceneFromDisk: (id)sender;
 
 - (void)RemoveDisplayTimer;
-- (void)RegisterUpdateTimer:(int)frequency;
+- (void)RegisterUpdateTimer: (int)frequency;
 
-- (void)refreshModifiedFile:(NSAppleEventDescriptor*)event withReplyEvent:(NSAppleEventDescriptor*)replyEvent;
-- (void)closeFile:(NSAppleEventDescriptor*)event withReplyEvent:(NSAppleEventDescriptor*)replyEvent;
+- (void)refreshModifiedFile: (NSAppleEventDescriptor*)event withReplyEvent: (NSAppleEventDescriptor*)replyEvent;
+- (void)closeFile: (NSAppleEventDescriptor*)event withReplyEvent: (NSAppleEventDescriptor*)replyEvent;
 
-- (IBAction)ShowAboutDialog:(id)sender;
-- (IBAction)ShowPreferences:(id)sender;
-- (IBAction)ShowPackageManager:(id)sender;
+- (IBAction)ShowAboutDialog: (id)sender;
+- (IBAction)ShowPreferences: (id)sender;
+- (IBAction)ShowPackageManager: (id)sender;
 
-- (IBAction) delete:(id)sender;
-- (IBAction) copy:(id)action;
-- (IBAction)paste:(id)action;
-- (IBAction)duplicate:(id)action;
-- (IBAction)cut:(id)action;
-- (IBAction)selectAll:(id)action;
-- (IBAction)find:(id)action;
-- (IBAction)frameSelected:(id)action;
-- (IBAction)frameSelectedWithLock:(id)action;
+- (IBAction) delete: (id)sender;
+- (IBAction) copy: (id)action;
+- (IBAction)paste: (id)action;
+- (IBAction)duplicate: (id)action;
+- (IBAction)cut: (id)action;
+- (IBAction)selectAll: (id)action;
+- (IBAction)find: (id)action;
+- (IBAction)frameSelected: (id)action;
+- (IBAction)frameSelectedWithLock: (id)action;
 
 // Assetstore protocol handler and registration
 - (void)registerAssetStoreURLProtocol;
-- (void)getUrl:(NSAppleEventDescriptor*)event withReplyEvent:(NSAppleEventDescriptor*)replyEvent;
+- (void)getUrl: (NSAppleEventDescriptor*)event withReplyEvent: (NSAppleEventDescriptor*)replyEvent;
 
 @end
 
---
+++ tests/input/cpp/sp_brace_catch.cpp
@@ -0,0 +1,8 @@
+int foo()
+{
+try { foo(bar); } catch (int *e) { return 0; }
+
+if (false) try { throw int(); } catch(...){}
+
+return 1;
+}	
