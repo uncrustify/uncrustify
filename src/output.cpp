78e0a1853c3102fab781e0366e257ab8b18183cd
Dmitry Marakasov <amdmi3@amdmi3.ru>
2021-05-14 20:46:30 +0300

    Always include required headers for map and stack

--- src/output.cpp
+++ src/output.cpp
@@ -17,12 +17,9 @@
 #include "unc_ctype.h"
 #include "unicode.h"
 
+#include <map>
 #include <regex>
 #include <set>
-
-#ifdef WIN32
-#include <map>                    // to get std::map
-#endif // WIN32
 
 
 constexpr static auto LCURRENT = LOUTPUT;
--- src/tokenize.cpp
+++ src/tokenize.cpp
@@ -16,10 +16,7 @@
 #include "unc_ctype.h"
 
 #include <regex>
-
-#ifdef WIN32
-#include <stack>            // to get std::stack
-#endif // WIN32
+#include <stack>
 
 
 #define LE_COUNT(x)    cpd.le_counts[static_cast<size_t>(LE_ ## x)]
