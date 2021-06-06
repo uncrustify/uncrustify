8631a8524d3a3563802c497d402e726584cffa6a
CharlieYJH <charlieyinjunhao@hotmail.com>
2021-06-06 13:54:22 -0700

    Update source formatting and remove tags from .gitignore

--- src/indent.cpp
+++ src/indent.cpp
@@ -1590,7 +1590,8 @@ void indent_text(void)
                }
                chunk_t *target = chunk_get_next_ncnnlnp(match);
 
-               while (tail == nullptr && target != nullptr)
+               while (  tail == nullptr
+                     && target != nullptr)
                {
                   if (chunk_is_semicolon(target) && target->level == match->level)
                   {
@@ -1627,10 +1628,15 @@ void indent_text(void)
             // 1. The matching brace is on the same line as the ending semicolon
             // 2a. If it's an assignment, check that both sides of the assignment operator are on the same line
             // 2b. If it's inside some closure, check that all the frames are on the same line, and it is in the top level closure
-            if (  options::align_assign_span() == 0 && !options::indent_align_assign() && are_chunks_in_same_line(chunk_skip_to_match(frm.top().pc), tail)
-               && (  (  !enclosure && are_chunks_in_same_line(chunk_get_prev_ncnnlnp(frm.prev().pc), frm.prev().pc)
+            if (  options::align_assign_span() == 0
+               && !options::indent_align_assign()
+               && are_chunks_in_same_line(chunk_skip_to_match(frm.top().pc), tail)
+               && (  (  !enclosure
+                     && are_chunks_in_same_line(chunk_get_prev_ncnnlnp(frm.prev().pc), frm.prev().pc)
                      && are_chunks_in_same_line(frm.prev().pc, chunk_get_next_ncnnlnp(frm.prev().pc)))
-                  || (enclosure && linematch && toplevel)))
+                  || (  enclosure
+                     && linematch
+                     && toplevel)))
             {
                frm.top().brace_indent -= indent_size;
             }
