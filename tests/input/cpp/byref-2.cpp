9eb630a141e407f561f6d4b32a6ada9051db088d
Guy Maurel <guy.j@maurel.de>
2021-06-09 16:10:04 +0200

    update test data

--- tests/input/cpp/byref-2.cpp
+++ tests/input/cpp/byref-2.cpp
@@ -1,6 +1,12 @@
-int&aa(int&x,int&b);   // Sp Before Byref Func, Sp After Byref Func, Sp Before Byref, Sp After Byref
-int aa(int&x,int&)     // Sp Before Byref, Sp Before Unnamed Byref, Sp After Byref
+int   &    aa(int   &   ,int    &   );
+// Sp Before Byref Func
+//   Sp After Byref Func
+//            Sp Before Unnamed Byref
+//                    Sp Before Unnamed Byref
+int        bb(int    &   x,int    &)
+//          Sp Before Byref
+//            Sp After Byref
+//                    Sp Before Unnamed Byref
 {
-	b = aa(x,b);
-	c = aa(&y,&d);     // Sp Addr
+        return 5;
 }
