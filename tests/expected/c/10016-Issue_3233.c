6ebd10396bc7f82432e922643120eeb37c2749d8
Guy Maurel <guy.j@maurel.de>
2021-07-13 18:38:35 +0200

    the parent_type of the string must be consider
    fixes #3233

---
+++ tests/expected/c/10016-Issue_3233.c
@@ -0,0 +1,8 @@
+#include "config.h"
+
+#include "nautilus-previewer.h"
+
+#define PREVIEWER2_DBUS_IFACE "org.gnome.NautilusPreviewer2"
+#define PREVIEWER_DBUS_PATH "/org/gnome/NautilusPreviewer"
+
+static GDBusProxy *previewer_v2_proxy = NULL;
