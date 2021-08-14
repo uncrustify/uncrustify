8ce27cb03d1242ad6cecb82cf79d0f04d815f948
Alex Henrie <alexh@vpitech.com>
2021-08-14 10:37:28 -0600

    Don't duplicate leading '//' when sp_cmt_cpp_start=ignore
    Fixes #3272

---
+++ tests/expected/c/10023-Issue_3272.h
@@ -0,0 +1,11 @@
+//#define INLINE_BUSY_WAIT(ticks)            \
+//{                                          \
+//    volatile size_t busy_wait_counter = 0; \
+//    while (busy_wait_counter++ < ticks);   \
+//}
+
+// #define INLINE_BUSY_WAIT(ticks)            \
+// {                                          \
+//     volatile size_t busy_wait_counter = 0; \
+//     while (busy_wait_counter++ < ticks);   \
+// }
