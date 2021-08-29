b4b1c2ced0aba4f002089c20396906112af96301
Guy Maurel <guy.j@maurel.de>
2021-08-29 18:16:19 +0200

    make from a using statement a new type entry

--- tests/expected/cpp/30310-braced_init_list.cpp
+++ tests/expected/cpp/30310-braced_init_list.cpp
@@ -27,10 +27,10 @@ int c = {1};
 int d = int{1};
 some_type e {1};
 some_type f = {1};
-some_type g = some_type {1};
-std::vector<some_type> h {some_type {4}, 5};
-::std::vector<some_type> i = ::std::vector<some_type>{4, some_type {5}};
-some_type j = ::std::vector<some_type>{4, some_type {5}}[1];
+some_type g = some_type{1};
+std::vector<some_type> h {some_type{4}, 5};
+::std::vector<some_type> i = ::std::vector<some_type>{4, some_type{5}};
+some_type j = ::std::vector<some_type>{4, some_type{5}}[1];
 some_type k[2] {1, 2};
 some_type l[2] = {1, 2};
 
@@ -49,7 +49,7 @@ int getA() const override {
 
 some_type inc(some_type a)
 {
-        return some_type {++a};
+        return some_type{++a};
 }
 
 some_type sum(some_type a, some_type b = some_type{1})
@@ -82,67 +82,67 @@ void braced_init_list_some_type()
         {
                 some_type a {};
                 some_type b = {};
-                some_type c = some_type {};
-                some_type d = some_type {some_type {}};
+                some_type c = some_type{};
+                some_type d = some_type{some_type{}};
                 some_type{};
-                some_type{some_type {}};
+                some_type{some_type{}};
         }
         {
                 some_type a {1};
                 some_type b = {1};
-                some_type c = some_type {1};
-                some_type d = some_type {some_type {1}};
+                some_type c = some_type{1};
+                some_type d = some_type{some_type{1}};
                 some_type{1};
-                some_type{some_type {1}};
+                some_type{some_type{1}};
         }
         {
                 ::some_type a {1};
                 ::some_type b = {1};
-                ::some_type c = ::some_type {1};
-                ::some_type d = ::some_type {::some_type {1}};
-                ::some_type {1};
-                ::some_type {::some_type {1}};
+                ::some_type c = ::some_type{1};
+                ::some_type d = ::some_type{::some_type{1}};
+                ::some_type{1};
+                ::some_type{::some_type{1}};
         }
         {
                 Ns::some_type a {1};
                 Ns::some_type b = {1};
-                Ns::some_type c = Ns::some_type {1};
-                Ns::some_type d = Ns::some_type {Ns::some_type {1}};
-                Ns::some_type {1};
-                Ns::some_type {Ns::some_type {1}};
+                Ns::some_type c = Ns::some_type{1};
+                Ns::some_type d = Ns::some_type{Ns::some_type{1}};
+                Ns::some_type{1};
+                Ns::some_type{Ns::some_type{1}};
         }
         {
                 ::Ns::some_type a {1};
                 ::Ns::some_type b = {1};
-                ::Ns::some_type c = ::Ns::some_type {1};
-                ::Ns::some_type d = ::Ns::some_type {::Ns::some_type {1}};
-                ::Ns::some_type {1};
-                ::Ns::some_type {::Ns::some_type {1}};
+                ::Ns::some_type c = ::Ns::some_type{1};
+                ::Ns::some_type d = ::Ns::some_type{::Ns::some_type{1}};
+                ::Ns::some_type{1};
+                ::Ns::some_type{::Ns::some_type{1}};
         }
 }
 
 void braced_init_list_some_type_auto()
 {
         {
-                auto b = some_type {};
-                auto c = some_type {some_type {}};
+                auto b = some_type{};
+                auto c = some_type{some_type{}};
         }
         {
                 auto a = {1};
-                auto b = some_type {1};
-                auto c = some_type {some_type {1}};
+                auto b = some_type{1};
+                auto c = some_type{some_type{1}};
         }
         {
-                auto b = ::some_type {1};
-                auto c = ::some_type {::some_type {1}};
+                auto b = ::some_type{1};
+                auto c = ::some_type{::some_type{1}};
         }
         {
-                auto b = Ns::some_type {1};
-                auto c = Ns::some_type {Ns::some_type {1}};
+                auto b = Ns::some_type{1};
+                auto c = Ns::some_type{Ns::some_type{1}};
         }
         {
-                auto b = ::Ns::some_type {1};
-                auto c = ::Ns::some_type {::Ns::some_type {1}};
+                auto b = ::Ns::some_type{1};
+                auto c = ::Ns::some_type{::Ns::some_type{1}};
         }
 }
 
@@ -151,37 +151,37 @@ void braced_init_list_function_call()
         {
                 some_type a {sum(some_type{}, some_type{})};
                 some_type b = sum(some_type{}, some_type{});
-                some_type c = some_type {sum(some_type{}, some_type{})};
+                some_type c = some_type{sum(some_type{}, some_type{})};
                 some_type{sum(some_type{}, some_type{})};
-                some_type{some_type {sum(some_type{}, some_type{})}};
+                some_type{some_type{sum(some_type{}, some_type{})}};
         }
         {
                 some_type a {sum(some_type{1}, some_type{1})};
                 some_type b = sum(some_type{1}, some_type{1});
-                some_type c = some_type {sum(some_type{1}, some_type{1})};
+                some_type c = some_type{sum(some_type{1}, some_type{1})};
                 some_type{sum(some_type{a}, some_type{b})};
-                some_type{some_type {sum(some_type{a}, some_type{b})}};
+                some_type{some_type{sum(some_type{a}, some_type{b})}};
         }
         {
                 ::some_type a {sum(::some_type{1}, ::some_type{1})};
                 ::some_type b = sum(::some_type{1}, ::some_type{1});
-                ::some_type c = ::some_type {sum(::some_type{1}, ::some_type{1})};
-                ::some_type {sum(::some_type{a}, ::some_type{b})};
-                ::some_type {::some_type {sum(::some_type{a}, ::some_type{b})}};
+                ::some_type c = ::some_type{sum(::some_type{1}, ::some_type{1})};
+                ::some_type{sum(::some_type{a}, ::some_type{b})};
+                ::some_type{::some_type{sum(::some_type{a}, ::some_type{b})}};
         }
         {
                 Ns::some_type a {sum(Ns::some_type{1}, Ns::some_type{1})};
                 Ns::some_type b = sum(Ns::some_type{1}, Ns::some_type{1});
-                Ns::some_type c = Ns::some_type {sum(Ns::some_type{1}, Ns::some_type{1})};
-                Ns::some_type {sum(Ns::some_type{a}, Ns::some_type{b})};
-                Ns::some_type {Ns::some_type {sum(Ns::some_type{a}, Ns::some_type{b})}};
+                Ns::some_type c = Ns::some_type{sum(Ns::some_type{1}, Ns::some_type{1})};
+                Ns::some_type{sum(Ns::some_type{a}, Ns::some_type{b})};
+                Ns::some_type{Ns::some_type{sum(Ns::some_type{a}, Ns::some_type{b})}};
         }
         {
                 ::Ns::some_type a {sum(::Ns::some_type{1}, ::Ns::some_type{1})};
                 ::Ns::some_type b = sum(::Ns::some_type{1}, ::Ns::some_type{1});
-                ::Ns::some_type c = ::Ns::some_type {sum(::Ns::some_type{1}, ::Ns::some_type{1})};
-                ::Ns::some_type {sum(::Ns::some_type{a}, ::Ns::some_type{b})};
-                ::Ns::some_type {::Ns::some_type {sum(::Ns::some_type{a}, ::Ns::some_type{b})}};
+                ::Ns::some_type c = ::Ns::some_type{sum(::Ns::some_type{1}, ::Ns::some_type{1})};
+                ::Ns::some_type{sum(::Ns::some_type{a}, ::Ns::some_type{b})};
+                ::Ns::some_type{::Ns::some_type{sum(::Ns::some_type{a}, ::Ns::some_type{b})}};
         }
 }
 
@@ -195,7 +195,7 @@ void braced_init_list_function_call_newline()
                 };
                 some_type b = sum(
                         some_type{}, some_type{});
-                some_type c = some_type {
+                some_type c = some_type{
                         sum(
                                 some_type{}, some_type{})};
                 some_type
@@ -205,8 +205,8 @@ void braced_init_list_function_call_newline()
                          )
                 };
                 some_type
-                {some_type {sum
-                                    (some_type{}, some_type{})}};
+                {some_type{sum
+                                   (some_type{}, some_type{})}};
         }
 }
 
@@ -220,7 +220,7 @@ void braced_init_list_array()
         {
                 some_type a[] {1, 2};
                 some_type b[] = {1, 2};
-                some_type c[] = {some_type {1}, some_type {2}};
+                some_type c[] = {some_type{1}, some_type{2}};
         }
 }
 
@@ -232,18 +232,18 @@ void braced_init_list_template()
                 std::vector<some_type> c = {{}, {}};
                 std::vector<some_type> d = std::vector<some_type>{};
                 std::vector<some_type> e = std::vector<some_type>{{}, {}};
-                std::vector<some_type> f = std::vector<some_type>{some_type {}, some_type {}};
+                std::vector<some_type> f = std::vector<some_type>{some_type{}, some_type{}};
                 std::vector<some_type>{};
                 std::vector<some_type>{{}, {}};
-                std::vector<some_type>{some_type {}, some_type {}};
+                std::vector<some_type>{some_type{}, some_type{}};
         }
         {
                 std::vector<some_type> a {1, 2};
                 std::vector<some_type> b = {1, 2};
                 std::vector<some_type> c = std::vector<some_type>{1, 2};
-                std::vector<some_type> d = std::vector<some_type>{some_type {1}, some_type {2}};
+                std::vector<some_type> d = std::vector<some_type>{some_type{1}, some_type{2}};
                 std::vector<some_type>{1, 2};
-                std::vector<some_type>{some_type {1}, some_type {2}};
+                std::vector<some_type>{some_type{1}, some_type{2}};
         }
 }
 
---
+++ tests/input/cpp/Issue_3291.cpp
@@ -0,0 +1,10 @@
+using Bar = int ;
+
+void
+foo2(Bar);
+
+void
+foo1(Bar b);
+
+void
+foo3(int);
