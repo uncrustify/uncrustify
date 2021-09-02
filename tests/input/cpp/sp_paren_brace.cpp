d184a697eedd59d6a9e2f28a142f5ff347cdb809
Guy Maurel <guy.j@maurel.de>
2021-09-02 18:45:48 +0200

    some more test data for sp_paren_brace

---
+++ tests/input/cpp/sp_paren_brace.cpp
@@ -0,0 +1,18 @@
+void *stopper_for_apply = (int[])               {0};
+//                               ^ here
+
+template<typename T, typename U>
+auto add(T t, U u) -> decltype(t + u)                {
+//                                   ^ here
+	return t + u;
+}
+
+void f()noexcept()               {
+//                ^ here
+}
+
+#define FOO5(x) for(;;) (!(x))                { *(volatile int*)0 = 1; }
+//                            ^ here
+
+(struct foo)      {...}
+//          ^ here
