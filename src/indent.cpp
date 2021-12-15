5052695a7a9289123539bd397966e97678f4dede
Richard Maxwell <jodi.the.tigger@gmail.com>
2021-12-15 14:48:07 +1300

    Fix lambda indentation in cpp function argument list regression
    Fixes #3428

--- src/indent.cpp
+++ src/indent.cpp
@@ -1577,38 +1577,6 @@ void indent_text(void)
          {
             LOG_FMT(LINDENT2, "%s(%d): indent_macro_brace\n", __func__, __LINE__);
          }
-         else if (  language_is_set(LANG_CPP)
-                 && options::indent_cpp_lambda_only_once()
-                 && (get_chunk_parent_type(pc) == CT_CPP_LAMBDA))
-         {
-            log_rule_B("indent_cpp_lambda_only_once");
-
-            size_t namespace_indent_to_ignore = 0;                   // Issue #1813
-            log_rule_B("indent_namespace");
-
-            if (!options::indent_namespace())
-            {
-               for (auto i = frm.rbegin(); i != frm.rend(); ++i)
-               {
-                  if (i->ns_cnt)
-                  {
-                     namespace_indent_to_ignore = i->ns_cnt;
-                     break;
-                  }
-               }
-            }
-            // Issue # 1296
-            frm.top().brace_indent = 1 + ((pc->brace_level - namespace_indent_to_ignore) * indent_size);
-            indent_column_set(frm.top().brace_indent);
-            frm.top().indent = indent_column + indent_size;
-            log_indent();
-            frm.top().indent_tab = frm.top().indent;
-            frm.top().indent_tmp = frm.top().indent;
-            log_indent_tmp();
-
-            frm.prev().indent_tmp = frm.top().indent_tmp;
-            log_indent_tmp();
-         }
          else if (  options::indent_cpp_lambda_body()
                  && get_chunk_parent_type(pc) == CT_CPP_LAMBDA)
          {
@@ -1671,6 +1639,45 @@ void indent_text(void)
                }
             }
 
+            // Issues: #1813, #3409, #3428
+            // if enclosure is set, the namespace is already accounted for
+            // so don't apply the correction twice.
+            // And for some reason, it's also accounted for when
+            // indent_namespace_single_indent is set
+            if (!enclosure && !options::indent_namespace_single_indent())
+            {
+               size_t namespace_indent_to_ignore = 0;
+               log_rule_B("indent_namespace");
+
+               for (auto i = frm.rbegin(); i != frm.rend(); ++i)
+               {
+                  if (i->ns_cnt)
+                  {
+                     const auto foo = i->ns_cnt;
+                     namespace_indent_to_ignore = indent_size * foo;
+                     break;
+                  }
+               }
+
+               if (namespace_indent_to_ignore && options::indent_namespace())
+               {
+                  // I honestly don't know what's going on, so this is an
+                  // emperical fix. For some reason lambda's don't have
+                  // their indent calculated properly when indent_namespace
+                  // is true. But only if they are not in enclosures.
+                  namespace_indent_to_ignore = indent_size;
+               }
+
+               if (namespace_indent_to_ignore <= frm.top().brace_indent)
+               {
+                  frm.top().brace_indent -= namespace_indent_to_ignore;
+               }
+               else
+               {
+                  frm.top().brace_indent = 1;
+               }
+            }
+
             // A few things to check:
             // 1. The matching brace is on the same line as the ending semicolon
             // 2a. If it's an assignment, check that both sides of the assignment operator are on the same line
@@ -1699,6 +1706,38 @@ void indent_text(void)
             frm.top().indent = indent_column + indent_size;
             log_indent();
 
+            frm.top().indent_tab = frm.top().indent;
+            frm.top().indent_tmp = frm.top().indent;
+            log_indent_tmp();
+
+            frm.prev().indent_tmp = frm.top().indent_tmp;
+            log_indent_tmp();
+         }
+         else if (  language_is_set(LANG_CPP)
+                 && options::indent_cpp_lambda_only_once()
+                 && (get_chunk_parent_type(pc) == CT_CPP_LAMBDA))
+         {
+            log_rule_B("indent_cpp_lambda_only_once");
+
+            size_t namespace_indent_to_ignore = 0;                   // Issue #1813
+            log_rule_B("indent_namespace");
+
+            if (!options::indent_namespace())
+            {
+               for (auto i = frm.rbegin(); i != frm.rend(); ++i)
+               {
+                  if (i->ns_cnt)
+                  {
+                     namespace_indent_to_ignore = i->ns_cnt;
+                     break;
+                  }
+               }
+            }
+            // Issue # 1296
+            frm.top().brace_indent = 1 + ((pc->brace_level - namespace_indent_to_ignore) * indent_size);
+            indent_column_set(frm.top().brace_indent);
+            frm.top().indent = indent_column + indent_size;
+            log_indent();
             frm.top().indent_tab = frm.top().indent;
             frm.top().indent_tmp = frm.top().indent;
             log_indent_tmp();
