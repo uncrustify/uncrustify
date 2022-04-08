uncrustify/uncrustify

commit c05ccdbd7e213c10f7b7f68763ee8dfa3c22fb3c

Replace chunk_is_comment_or_newline_or_ignored() and chunk_get_prev_nc_nnl_ni() with Chunk::IsCommentNewlineOrIgnored() and Chunk::GetPrevNcNnlNi()
Signed-off-by: Michele Calgaro <michele.calgaro@yahoo.it>

src/EnumStructUnionParser.cpp
@@ -518,7 +518,7 @@ static std::tuple<Chunk *, Chunk *, Chunk *> match_variable(Chunk *pc, std::size
    if (  identifier != nullptr
       && start != nullptr
       && (  end != nullptr
-         || chunk_is_token(chunk_get_prev_nc_nnl_ni(identifier), CT_WORD)))
+         || chunk_is_token(identifier->GetPrevNcNnlNi(), CT_WORD)))
    {
       return(std::make_tuple(start, identifier, end));
    }
@@ -651,22 +651,27 @@ static std::pair<Chunk *, Chunk *> match_variable_start(Chunk *pc, std::size_t l
 {
    LOG_FUNC_ENTRY();
 
-   Chunk *identifier = nullptr;
+   Chunk *identifier = Chunk::NullChunkPtr;
 
-   while (pc != nullptr)
+   if (pc == nullptr)
+   {
+      pc = Chunk::NullChunkPtr;
+   }
+
+   while (pc->IsNotNullChunk())
    {
       /**
        * skip any right-hand side assignments
        */
       Chunk *before_rhs_exp_start = skip_expression_rev(pc);
-      Chunk *prev                 = nullptr;
+      Chunk *prev                 = Chunk::NullChunkPtr;
       Chunk *next                 = pc;
 
       while (  chunk_is_after(next, before_rhs_exp_start)
             && pc != prev)
       {
          next = prev;
-         prev = chunk_get_prev_nc_nnl_ni(next);
+         prev = next->GetPrevNcNnlNi();
 
          if (chunk_is_token(next, CT_ASSIGN))
          {
@@ -677,10 +682,10 @@ static std::pair<Chunk *, Chunk *> match_variable_start(Chunk *pc, std::size_t l
        * skip current and preceding chunks if at a higher brace level
        */
 
-      while (  pc != nullptr
+      while (  pc->IsNotNullChunk()
             && pc->level > level)
       {
-         pc = chunk_get_prev_nc_nnl_ni(pc);
+         pc = pc->GetPrevNcNnlNi();
       }
 
       /**
@@ -699,7 +704,7 @@ static std::pair<Chunk *, Chunk *> match_variable_start(Chunk *pc, std::size_t l
        * matching a variable declaration/definition
        */
 
-      prev = chunk_get_prev_nc_nnl_ni(pc);
+      prev = pc->GetPrevNcNnlNi();
 
       if (!adj_tokens_match_var_def_pattern(prev, pc))
       {
@@ -719,7 +724,7 @@ static std::pair<Chunk *, Chunk *> match_variable_start(Chunk *pc, std::size_t l
          }
       }
 
-      if (  identifier == nullptr
+      if (  identifier->IsNullChunk()
          && chunk_is_token(pc, CT_WORD))
       {
          /**
@@ -800,13 +805,18 @@ static Chunk *skip_scope_resolution_and_nested_name_specifiers_rev(Chunk *pc)
 {
    LOG_FUNC_ENTRY();
 
-   if (  (  pc != nullptr
+   if (pc == nullptr)
+   {
+      pc = Chunk::NullChunkPtr;
+   }
+
+   if (  (  pc->IsNotNullChunk()
          && pc->flags.test(PCF_IN_TEMPLATE))
       || chunk_is_token(pc, CT_DC_MEMBER)
       || chunk_is_token(pc, CT_TYPE)
       || chunk_is_token(pc, CT_WORD))
    {
-      while (pc != nullptr)
+      while (pc->IsNotNullChunk())
       {
          /**
           * skip to any preceding match for angle brackets
@@ -815,7 +825,7 @@ static Chunk *skip_scope_resolution_and_nested_name_specifiers_rev(Chunk *pc)
          {
             pc = chunk_skip_to_match_rev(pc, E_Scope::PREPROC);
          }
-         auto *prev = chunk_get_prev_nc_nnl_ni(pc);
+         Chunk *prev = pc->GetPrevNcNnlNi();
 
          /**
           * call a separate function to validate adjacent tokens as potentially
@@ -1699,7 +1709,7 @@ void EnumStructUnionParser::mark_extracorporeal_lvalues()
     *       following isn't necessary
     */
    Chunk *next = m_start;
-   Chunk *prev = nullptr;
+   Chunk *prev = Chunk::NullChunkPtr;
 
    /**
     * if the class is a template, go the extra step and correct the
@@ -1710,9 +1720,9 @@ void EnumStructUnionParser::mark_extracorporeal_lvalues()
    {
       while (true)
       {
-         prev = chunk_get_prev_nc_nnl_ni(next);
+         prev = next->GetPrevNcNnlNi();
 
-         if (  prev == nullptr
+         if (  prev->IsNullChunk()
             || (  !prev->flags.test(PCF_IN_TEMPLATE)
                && chunk_is_not_token(prev, CT_TEMPLATE)))
          {
@@ -1721,8 +1731,8 @@ void EnumStructUnionParser::mark_extracorporeal_lvalues()
          next = prev;
       }
    }
-   auto *body_end   = get_body_end();
-   auto *body_start = get_body_start();
+   Chunk *body_end   = get_body_end();
+   Chunk *body_start = get_body_start();
 
    while (next != m_end)
    {
@@ -1809,7 +1819,7 @@ void EnumStructUnionParser::mark_pointer_types(Chunk *pc)
       do
       {
          // TODO: should there be a CT_BYREF_TYPE?
-         pc = chunk_get_prev_nc_nnl_ni(pc);
+         pc = pc->GetPrevNcNnlNi();
 
          if (chunk_is_ptr_operator(pc))
          {
@@ -2008,7 +2018,7 @@ void EnumStructUnionParser::parse(Chunk *pc)
    }
    else if (chunk_is_enum(prev))
    {
-      auto *prev_prev = chunk_get_prev_nc_nnl_ni(prev);
+      auto *prev_prev = prev->GetPrevNcNnlNi();
 
       if (  chunk_is_enum(prev_prev)
          && chunk_is_enum(prev))
@@ -2166,7 +2176,7 @@ Chunk *EnumStructUnionParser::parse_angles(Chunk *angle_open)
              * bracket should be preceded by a CT_WORD token and we should have
              * found a closing angle bracket
              */
-            auto *prev = chunk_get_prev_nc_nnl_ni(angle_open);
+            auto *prev = angle_open->GetPrevNcNnlNi();
 
             if (chunk_is_not_token(prev, CT_WORD))
             {
@@ -2233,7 +2243,7 @@ Chunk *EnumStructUnionParser::parse_braces(Chunk *brace_open)
 
       auto *enum_base_start   = get_enum_base_start();
       auto *inheritance_start = get_inheritance_start();
-      auto *prev              = chunk_get_prev_nc_nnl_ni(pc);
+      auto *prev              = pc->GetPrevNcNnlNi();
 
       /**
        * check to see if the open brace was preceded by a closing paren;
@@ -2265,7 +2275,7 @@ Chunk *EnumStructUnionParser::parse_braces(Chunk *brace_open)
              * a function definition
              */
             auto *type       = m_start->GetNextNcNnl();
-            auto *identifier = chunk_get_prev_nc_nnl_ni(paren_open, E_Scope::PREPROC);
+            auto *identifier = paren_open->GetPrevNcNnlNi(E_Scope::PREPROC);
             is_potential_function_definition = (  (  chunk_is_token(identifier, CT_FUNCTION)
                                                   || chunk_is_token(identifier, CT_FUNC_DEF)
                                                   || chunk_is_token(identifier, CT_WORD))
@@ -2796,7 +2806,7 @@ bool EnumStructUnionParser::try_pre_identify_type()
       else if (  next->IsNotNullChunk()
               && chunk_is_token(next, CT_WORD)
               && chunk_is_token(next_next, CT_WORD)
-              && chunk_get_prev_nc_nnl_ni(m_end) == next_next)
+              && m_end->GetPrevNcNnlNi() == next_next)
       {
          /**
           * check to see if we've got a macro reference preceding the last word chunk;
@@ -2854,19 +2864,19 @@ bool EnumStructUnionParser::try_pre_identify_type()
        * the chunk preceding the previously selected chunk should indicate the type
        */
 
-      pc = chunk_get_prev_nc_nnl_ni(pc, E_Scope::PREPROC);
+      pc = pc->GetPrevNcNnlNi(E_Scope::PREPROC);
 
       if (  chunk_is_token(pc, CT_QUALIFIER)
          && std::strncmp(pc->str.c_str(), "final", 5) == 0)
       {
-         pc = chunk_get_prev_nc_nnl_ni(pc, E_Scope::PREPROC);
+         pc = pc->GetPrevNcNnlNi(E_Scope::PREPROC);
       }
 
       if (  language_is_set(LANG_D)
          && chunk_is_paren_close(pc))
       {
          pc = chunk_skip_to_match_rev(pc);
-         pc = chunk_get_prev_nc_nnl_ni(pc);
+         pc = pc->GetPrevNcNnlNi();
       }
 
       if (chunk_is_token(pc, CT_WORD))

src/chunk.cpp
@@ -773,15 +773,15 @@ Chunk *Chunk::GetPrevNcNnlNet(E_Scope scope) const
 }
 
 
-Chunk *Chunk::GetNextNisq(E_Scope scope) const
+Chunk *Chunk::GetPrevNcNnlNi(E_Scope scope) const
 {
-   return(Search(&Chunk::IsSquareBracket, scope, E_Direction::FORWARD, false));
+   return(Search(&Chunk::IsCommentNewlineOrIgnored, scope, E_Direction::BACKWARD, false));
 }
 
 
-Chunk *chunk_get_prev_nc_nnl_ni(Chunk *cur, E_Scope scope)
+Chunk *Chunk::GetNextNisq(E_Scope scope) const
 {
-   return(__internal_chunk_search(cur, chunk_is_comment_or_newline_or_ignored, scope, E_Direction::BACKWARD, false));
+   return(Search(&Chunk::IsSquareBracket, scope, E_Direction::FORWARD, false));
 }
 
 

src/chunk.h
@@ -232,6 +232,14 @@ public:
 
 
    /**
+    * @brief returns the prev non-comment, non-newline, non-ignored chunk
+    * @param scope code region to search in
+    * @return pointer to prev non-comment, non-newline, non-ignored chunk or Chunk::NullChunkPtr if no chunk was found
+    */
+   Chunk *GetPrevNcNnlNi(E_Scope scope = E_Scope::ALL) const;
+
+
+   /**
     * @brief returns the next chunk not in or part of balanced square
     *        brackets. This handles stacked [] instances to accommodate
     *        multi-dimensional array declarations
@@ -334,6 +342,12 @@ public:
    bool IsComment() const;
 
    /**
+    * @brief checks whether the chunk is valid and has an empty text
+    * @return true if the chunk is valid and has an empty text
+    */
+   bool IsEmptyText() const;
+
+   /**
     * @brief checks whether the chunk is a preprocessor
     * @return true if the chunk is a preprocessor, false otherwise
     */
@@ -344,6 +358,12 @@ public:
     * @return true if the chunk is either a comment or a newline, false otherwise
     */
    bool IsCommentOrNewline() const;
+
+   /**
+    * @brief checks whether the chunk is either a comment, a newline or ignored
+    * @return true if the chunk is either a comment, a newline or ignored, false otherwise
+    */
+   bool IsCommentNewlineOrIgnored() const;
 
    /**
     * @brief checks whether the chunk is a comment, a newline or a preprocessor
@@ -358,12 +378,6 @@ public:
    bool IsCommentOrNewlineInPreproc() const;
 
    /**
-    * @brief checks whether the chunk is valid and has an empty text
-    * @return true if the chunk is valid and has an empty text
-    */
-   bool IsEmptyText() const;
-
-   /**
     * @brief checks whether the chunk is a comment, a newline or has an empty text
     * @return true if the chunk is a comment, a newline or has an empty text
     */
@@ -374,6 +388,7 @@ public:
     * @return true if the chunk is a square bracket
     */
    bool IsSquareBracket() const;
+
 
    // --------- Data members
 
@@ -496,15 +511,6 @@ Chunk *chunk_first_on_line(Chunk *pc);
 
 //! check if a given chunk is the last on its line
 bool chunk_is_last_on_line(Chunk *pc);
-
-
-/**
- * Gets the prev non-NEWLINE and non-comment and non-ignored chunk
- *
- * @param cur    chunk to use as start point
- * @param scope  code region to search in
- */
-Chunk *chunk_get_prev_nc_nnl_ni(Chunk *cur, E_Scope scope = E_Scope::ALL);
 
 
 /**
@@ -713,6 +719,13 @@ inline bool Chunk::IsComment() const
 }
 
 
+inline bool Chunk::IsEmptyText() const
+{
+   return(  IsNotNullChunk()
+         && Len() == 0);
+}
+
+
 inline bool Chunk::IsPreproc() const
 {
    return(  IsNotNullChunk()
@@ -743,18 +756,19 @@ inline bool Chunk::IsCommentOrNewlineInPreproc() const
 }
 
 
-inline bool Chunk::IsEmptyText() const
-{
-   return(  IsNotNullChunk()
-         && Len() == 0);
-}
-
-
 inline bool Chunk::IsCommentNewlineOrEmptyText() const
 {
    return(  IsComment()
          || IsNewline()
          || IsEmptyText());
+}
+
+
+inline bool Chunk::IsCommentNewlineOrIgnored() const
+{
+   return(  IsComment()
+         || IsNewline()
+         || Is(CT_IGNORED));
 }
 
 
@@ -907,15 +921,6 @@ static inline bool chunk_is_semicolon(Chunk *pc)
 {
    return(  chunk_is_token(pc, CT_SEMICOLON)
          || chunk_is_token(pc, CT_VSEMICOLON));
-}
-
-
-//! checks if a chunk is valid and either a comment or newline or ignored
-static inline bool chunk_is_comment_or_newline_or_ignored(Chunk *pc)
-{
-   return(  chunk_is_comment(pc)
-         || chunk_is_newline(pc)
-         || chunk_is_token(pc, CT_IGNORED));
 }
 
 

src/combine.cpp
@@ -288,7 +288,7 @@ static void flag_asm(Chunk *pc)
       {
          // if there is a string on both sides, then this is two ASM_COLONs
          if (  chunk_is_token(tmp->GetNextNcNnl(E_Scope::PREPROC), CT_STRING)
-            && chunk_is_token(chunk_get_prev_nc_nnl_ni(tmp, E_Scope::PREPROC), CT_STRING)) // Issue #2279
+            && chunk_is_token(tmp->GetPrevNcNnlNi(E_Scope::PREPROC), CT_STRING)) // Issue #2279
          {
             Chunk nc;
 
@@ -449,7 +449,7 @@ void do_symbol_check(Chunk *prev, Chunk *pc, Chunk *next)
             }
          }
 
-         for (tmp = chunk_get_prev_nc_nnl_ni(pc); tmp != nullptr; tmp = chunk_get_prev_nc_nnl_ni(tmp)) // Issue #2279
+         for (tmp = pc->GetPrevNcNnlNi(); tmp->IsNotNullChunk(); tmp = tmp->GetPrevNcNnlNi()) // Issue #2279
          {
             if (  chunk_is_semicolon(tmp)
                || chunk_is_token(tmp, CT_BRACE_OPEN)
@@ -1400,7 +1400,7 @@ void do_symbol_check(Chunk *prev, Chunk *pc, Chunk *next)
             bool  is_multiplication = false;
             Chunk *tmp              = pc;
 
-            while (tmp != nullptr)
+            while (tmp->IsNotNullChunk())
             {
                if (  chunk_is_token(tmp, CT_SEMICOLON)
                   || get_chunk_parent_type(tmp) == CT_CLASS)
@@ -1414,7 +1414,7 @@ void do_symbol_check(Chunk *prev, Chunk *pc, Chunk *next)
                   is_multiplication = true;
                   break;
                }
-               tmp = chunk_get_prev_nc_nnl_ni(tmp); // Issue #2279
+               tmp = tmp->GetPrevNcNnlNi(); // Issue #2279
             }
 
             if (is_multiplication)
@@ -1491,7 +1491,7 @@ void do_symbol_check(Chunk *prev, Chunk *pc, Chunk *next)
          {
             Chunk *tmp = pc;
 
-            while (tmp != nullptr)
+            while (tmp->IsNotNullChunk())
             {
                if (  chunk_is_token(tmp, CT_SEMICOLON)
                   || chunk_is_token(tmp, CT_BRACE_OPEN)
@@ -1503,7 +1503,7 @@ void do_symbol_check(Chunk *prev, Chunk *pc, Chunk *next)
                {
                   set_chunk_type(pc, CT_PTR_TYPE);
                }
-               tmp = chunk_get_prev_nc_nnl_ni(tmp); // Issue #2279
+               tmp = tmp->GetPrevNcNnlNi(); // Issue #2279
             }
          }
       }
@@ -1559,9 +1559,9 @@ void do_symbol_check(Chunk *prev, Chunk *pc, Chunk *next)
             if (  chunk_is_token(prev, CT_WORD)
                && !chunk_is_token(next, CT_NUMBER)) // Issue #3407
             {
-               Chunk *tmp = chunk_get_prev_nc_nnl_ni(prev); // Issue #2279
+               Chunk *tmp = prev->GetPrevNcNnlNi(); // Issue #2279
 
-               if (tmp != nullptr)
+               if (tmp->IsNotNullChunk())
                {
                   if (  chunk_is_semicolon(tmp)
                      || chunk_is_token(tmp, CT_BRACE_OPEN)
@@ -1835,9 +1835,9 @@ static void check_double_brace_init(Chunk *bo1)
 {
    LOG_FUNC_ENTRY();
    LOG_FMT(LJDBI, "%s(%d): orig_line is %zu, orig_col is %zu", __func__, __LINE__, bo1->orig_line, bo1->orig_col);
-   Chunk *pc = chunk_get_prev_nc_nnl_ni(bo1);   // Issue #2279
+   Chunk *pc = bo1->GetPrevNcNnlNi();   // Issue #2279
 
-   if (pc == nullptr)
+   if (pc->IsNullChunk())
    {
       return;
    }
@@ -1916,7 +1916,7 @@ void fix_symbols(void)
       // a brace immediately preceded by word in C++11 is an initializer list though it may also
       // by a type casting initializer list if the word is really a type; sadly uncrustify knows
       // only built-in types and knows nothing of user-defined types
-      Chunk *prev = chunk_get_prev_nc_nnl_ni(pc);   // Issue #2279
+      Chunk *prev = pc->GetPrevNcNnlNi();   // Issue #2279
 
       if (  is_cpp
          && chunk_is_token(pc, CT_BRACE_OPEN)
@@ -1960,14 +1960,14 @@ void fix_symbols(void)
       }
       LOG_FMT(LFCNR, "%s(%d): pc->orig_line       is %zu, orig_col is %zu, Text() is '%s', type is %s\n",
               __func__, __LINE__, pc->orig_line, pc->orig_col, pc->Text(), get_token_name(pc->type));
-      Chunk *prev = chunk_get_prev_nc_nnl_ni(pc, E_Scope::PREPROC);   // Issue #2279
+      Chunk *prev = pc->GetPrevNcNnlNi(E_Scope::PREPROC);   // Issue #2279
 
       if (chunk_is_token(prev, CT_QUALIFIER))
       {
-         prev = chunk_get_prev_nc_nnl_ni(prev, E_Scope::PREPROC);     // Issue #3513
+         prev = prev->GetPrevNcNnlNi(E_Scope::PREPROC);     // Issue #3513
       }
 
-      if (prev == nullptr)
+      if (prev->IsNullChunk())
       {
          prev = &dummy;
       }
@@ -2445,20 +2445,14 @@ static void handle_cpp_lambda(Chunk *sq_o)
    Chunk *ret = nullptr;
 
    // abort if type of the previous token is not contained in this whitelist
-   Chunk *prev = chunk_get_prev_nc_nnl_ni(sq_o);   // Issue #2279
+   Chunk *prev = sq_o->GetPrevNcNnlNi();   // Issue #2279
 
-   if (prev == nullptr)
+   if (prev->IsNullChunk())
    {
       LOG_FMT(LFCNR, "%s(%d): prev is nullptr\n", __func__, __LINE__);
    }
-   else
-   {
-      //LOG_FMT(LFCNR, "%s(%d): prev is '%s'/%s\n",
-      //        __func__, __LINE__,
-      //        prev->Text(), get_token_name(prev->type));
-   }
 
-   if (  prev == nullptr
+   if (  prev->IsNullChunk()
       || (  chunk_is_not_token(prev, CT_ASSIGN)
          && chunk_is_not_token(prev, CT_COMMA)
          && chunk_is_not_token(prev, CT_PAREN_OPEN)   // allow Js like self invoking lambda syntax: ([](){})();
@@ -2903,10 +2897,10 @@ static void handle_oc_block_literal(Chunk *pc)
    {
       return; // let's be paranoid
    }
-   Chunk *prev = chunk_get_prev_nc_nnl_ni(pc);   // Issue #2279
+   Chunk *prev = pc->GetPrevNcNnlNi();   // Issue #2279
    Chunk *next = pc->GetNextNcNnl();
 
-   if (  prev == nullptr
+   if (  prev->IsNullChunk()
       || next->IsNullChunk())
    {
       return; // let's be paranoid
@@ -3009,11 +3003,11 @@ static void handle_oc_block_literal(Chunk *pc)
          flag_parens(apo, PCF_OC_ATYPE, CT_FPAREN_OPEN, CT_OC_BLOCK_EXPR, true);
          fix_fcn_def_params(apo);
       }
-      lbp = chunk_get_prev_nc_nnl_ni(apo);   // Issue #2279
+      lbp = apo->GetPrevNcNnlNi();   // Issue #2279
    }
    else
    {
-      lbp = chunk_get_prev_nc_nnl_ni(bbo);   // Issue #2279
+      lbp = bbo->GetPrevNcNnlNi();   // Issue #2279
    }
 
    // mark the return type, if any
@@ -3023,7 +3017,7 @@ static void handle_oc_block_literal(Chunk *pc)
       make_type(lbp);
       chunk_flags_set(lbp, PCF_OC_RTYPE);
       set_chunk_parent(lbp, CT_OC_BLOCK_EXPR);
-      lbp = chunk_get_prev_nc_nnl_ni(lbp);   // Issue #2279
+      lbp = lbp->GetPrevNcNnlNi();   // Issue #2279
    }
    // mark the braces
    set_chunk_parent(bbo, CT_OC_BLOCK_EXPR);
@@ -3047,7 +3041,7 @@ static void handle_oc_block_type(Chunk *pc)
       return;
    }
    // make sure we have '( ^'
-   Chunk *tpo = chunk_get_prev_nc_nnl_ni(pc); // type paren open   Issue #2279
+   Chunk *tpo = pc->GetPrevNcNnlNi(); // type paren open   Issue #2279
 
    if (chunk_is_paren_open(tpo))
    {
@@ -3055,10 +3049,10 @@ static void handle_oc_block_type(Chunk *pc)
        * block type: 'RTYPE (^LABEL)(ARGS)'
        * LABEL is optional.
        */
-      Chunk *tpc = chunk_skip_to_match(tpo);      // type close paren (after '^')
-      Chunk *nam = chunk_get_prev_nc_nnl_ni(tpc); // name (if any) or '^'   Issue #2279
-      Chunk *apo = tpc->GetNextNcNnl();           // arg open paren
-      Chunk *apc = chunk_skip_to_match(apo);      // arg close paren
+      Chunk *tpc = chunk_skip_to_match(tpo); // type close paren (after '^')
+      Chunk *nam = tpc->GetPrevNcNnlNi();    // name (if any) or '^'   Issue #2279
+      Chunk *apo = tpc->GetNextNcNnl();      // arg open paren
+      Chunk *apc = chunk_skip_to_match(apo); // arg close paren
 
       /*
        * If this is a block literal instead of a block type, 'nam'
@@ -3110,7 +3104,7 @@ static void handle_oc_block_type(Chunk *pc)
          set_chunk_type(apc, CT_FPAREN_CLOSE);
          set_chunk_parent(apc, CT_FUNC_PROTO);
          fix_fcn_def_params(apo);
-         mark_function_return_type(nam, chunk_get_prev_nc_nnl_ni(tpo), pt);   // Issue #2279
+         mark_function_return_type(nam, tpo->GetPrevNcNnlNi(), pt);   // Issue #2279
       }
    }
 } // handle_oc_block_type
@@ -3369,7 +3363,7 @@ static void handle_oc_message_send(Chunk *os)
          LOG_FMT(LFCN, "%s(%d): (18) SET TO CT_FUNC_CALL: orig_line is %zu, orig_col is %zu, Text() '%s'\n",
                  __func__, __LINE__, tmp->orig_line, tmp->orig_col, tmp->Text());
          set_chunk_type(tmp, CT_FUNC_CALL);
-         tmp = chunk_get_prev_nc_nnl_ni(set_paren_parent(tt, CT_FUNC_CALL));   // Issue #2279
+         tmp = set_paren_parent(tt, CT_FUNC_CALL)->GetPrevNcNnlNi();   // Issue #2279
       }
       else
       {
@@ -3821,7 +3815,7 @@ static void handle_cs_property(Chunk *bro)
    bool  did_prop = false;
    Chunk *pc      = bro;
 
-   while ((pc = chunk_get_prev_nc_nnl_ni(pc)) != nullptr)   // Issue #2279
+   while ((pc = pc->GetPrevNcNnlNi())->IsNotNullChunk()) // Issue #2279
    {
       if (pc->level == bro->level)
       {
@@ -3990,7 +3984,7 @@ static void handle_proto_wrap(Chunk *pc)
    // Mark return type (TODO: move to own function)
    tmp = pc;
 
-   while ((tmp = chunk_get_prev_nc_nnl_ni(tmp)) != nullptr)   // Issue #2279
+   while ((tmp = tmp->GetPrevNcNnlNi())->IsNotNullChunk()) // Issue #2279
    {
       if (  !chunk_is_type(tmp)
          && chunk_is_not_token(tmp, CT_OPERATOR)

commit 8d9f527ee98cc91f607215f9b6f9f6d0db29ef90

Fix regression introduced by commit c05ccdbd. Unnecessary newlines were added after 'friend' statements containing a class scope.
Signed-off-by: Michele Calgaro <michele.calgaro@yahoo.it>

src/EnumStructUnionParser.cpp
@@ -515,8 +515,8 @@ static std::tuple<Chunk *, Chunk *, Chunk *> match_variable(Chunk *pc, std::size
     * chunk indicates a variable declaration/definition
     */
 
-   if (  identifier != nullptr
-      && start != nullptr
+   if (  identifier->IsNotNullChunk()
+      && start->IsNotNullChunk()
       && (  end != nullptr
          || chunk_is_token(identifier->GetPrevNcNnlNi(), CT_WORD)))
    {
@@ -749,7 +749,7 @@ static std::pair<Chunk *, Chunk *> match_variable_start(Chunk *pc, std::size_t l
       }
       pc = prev;
    }
-   return(std::make_pair(nullptr, nullptr));
+   return(std::make_pair(Chunk::NullChunkPtr, Chunk::NullChunkPtr));
 } // match_variable_start
 
 