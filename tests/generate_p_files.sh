36777ae1bdd2ba4ab2b4a033682e895ffb312d4a
Guy Maurel <guy.j@maurel.de>
2022-05-28 10:45:52 +0200

    a tool to generate the p files from the sources

---
+++ tests/generate_p_files.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+#
+# Author:  guy maurel
+# May 2022
+#
+# generate p-files from the sources
+#
+# the test is only usefull for the developpers
+#
+LIST=`ls -1 ../src/*.cpp`
+LIST_H=`ls -1 ../src/*.h`
+P_FILES="../build/P-files"
+#
+mkdir -p ${P_FILES}
+#
+for each in ${LIST} ${LIST_H}
+do
+  bn=`basename ${each}`
+  echo ${bn}
+  uncrustify -q -c ../forUncrustifySources.cfg -f ${each} -o /dev/null -p ${P_FILES}/${bn}-p.txt
+done
