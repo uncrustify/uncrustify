4e4fa2e3c808f78e8cf1a25ce39b2243797c0140
Peter Tao <petertao@fb.com>
2022-07-22 04:53:55 -0700

    Add more tests

---
+++ tests/expected/oc/50908-align_colon_with_ternary_2.m
@@ -0,0 +1,31 @@
+static void func() {
+	[object test:NO ? PARAM1 : PARAM2 test2:YES];
+}
+
+
+static void func() {
+	[object test:NO ? PARAM1 : PARAM2
+	       test2:YES
+	       test3:YES];
+}
+
+static void func() {
+	[object test:NO ? PARAM1 : PARAM2
+	       test2:YES
+	       test3:YES ? PARAM1 : PARAM2
+	       test4:NO];
+}
+
+static void func() {
+	[object test:NO ? PARAM1 : YES ? nil:nil
+	       test2:YES
+	       test3:YES ? PARAM1 : PARAM2
+	       test4:NO];
+}
+
+static void func() {
+	NSString *s = YES ? [object test:nil
+	                           test2:YES
+	                           test3:nil
+	                           test4:NO] : nil;
+}
