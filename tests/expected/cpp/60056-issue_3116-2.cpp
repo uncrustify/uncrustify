723c87ab4e5244cf59a98f9b7c09e069dc57d9df
Daniele Bartolini <dbartolini.aa@gmail.com>
2022-07-22 20:42:51 +0200

    Fix indent_cpp_lambda_body in some circumnstances

--- tests/expected/cpp/60056-issue_3116-2.cpp
+++ tests/expected/cpp/60056-issue_3116-2.cpp
@@ -1,10 +1,10 @@
 obj.AddObject(Object::UniqueName(), 10, [this] {
-    holder.Access([this](const auto &info) {
-        if (IsGood(info)) {
-            Add(info);
-        }
+        holder.Access([this](const auto &info) {
+            if (IsGood(info)) {
+                Add(info);
+            }
+        });
     });
-});
 
 obj.AddObject(
     Object::UniqueName(),
@@ -20,12 +20,12 @@ obj.AddObject(
 
 {
     obj.AddObject(Object::UniqueName(), 10, [this] {
-        holder.Access([this](const auto &info) {
-            if (IsGood(info)) {
-                Add(info);
-            }
+            holder.Access([this](const auto &info) {
+                if (IsGood(info)) {
+                    Add(info);
+                }
+            });
         });
-    });
 
     obj.AddObject(
         Object::UniqueName(),
