8749a2acd1d9dfb57d8be03300fb31cc2e7d5f4e
Frank J. T. Wojcik <fwojcik@uw.edu>
2022-08-08 08:54:19 -0700

    Prevents adding duplicate long class comments
    If mod_add_long_class_closebrace_comment is enabled together with nl_after_class,
    then the added newline will mean that the added class comment will not be detected on
    a re-run of uncrustify, and it will add a second class comment. This will look like:
    
    class FooClass {
    }; // class FooClass
       // class FooClass

---
+++ tests/expected/cpp/34212-func_class.h
@@ -0,0 +1,16 @@
+void MD5::reverse_u32(UINT8 *buf, int n_u32);
+
+MD5::MD5();
+
+class AlignStack
+{
+public:
+bool m_skip_first;
+
+AlignStack();
+
+
+~AlignStack();
+
+void End();
+}; // class AlignStack
