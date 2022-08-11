ad2e8438f77a8ec00bfe698fa8b5b0049149bbc8
Frank J. T. Wojcik <fwojcik@uw.edu>
2022-08-10 21:12:52 -0700

    Adds unit tests for nl_*var_def_blk*
    These tests cover all permutations of nl_func_var_def_blk, nl_var_def_blk_start, and
    nl_var_def_blk_end, and ensures they are all totally independent and non-overlapping.

---
+++ tests/expected/cpp/31662-var_def_blks.cpp
@@ -0,0 +1,32 @@
+unsigned a;
+char b;
+int bar(void);
+unsigned c;
+char d;
+int foo(void) {
+	char buf[1000] = {0};
+	size_t len;
+	float amount;
+	double remainder;
+
+	bar();
+	uint64_t quux;
+	uint64_t quack;
+	for (int i = 0; i < 1000; i++) {
+		uint64_t checksum;
+		double remainder;
+		buf[i] += len;
+		uint32_t xyzzy;
+		uint32_t quark;
+	}
+	float amount;
+	double remainder;
+	baz();
+	uint32_t llama;
+	uint32_t treehouse;
+}
+char * w;
+uint8_t x;
+int baz(void);
+long y;
+int z;
