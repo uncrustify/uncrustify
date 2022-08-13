871ff1dbbc5b9e423243c29d5f5494506c8db39d
Frank J. T. Wojcik <fwojcik@uw.edu>
2022-08-13 16:06:00 -0700

    Fix align_assign* when multiple definitions are encountered
    If assignment aligning is happening for a group of variable definition lines, and a
    line with multiple definitions is encountered, then align_assign.c will not align the
    most recent assignments before that line.

---
+++ tests/input/c/align_assign_var_defs.c
@@ -0,0 +1,9 @@
+int foo(void) {
+    unsigned a = 123;
+    unsigned abcde = 456;
+    unsigned abcdefghijklmnop = 789;
+    unsigned w, x, y, z;
+    unsigned u = 16730;
+    unsigned vvvvv = 4917;
+    unsigned wwwwwwwwww = 12428;
+}
