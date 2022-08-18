dbb7eefacf575ea53a19a429b225e6d8cc7e733f
Peter Tao <petertao@fb.com>
2022-08-17 23:55:00 -0700

    Fix OC+ lambda not being detected

---
+++ tests/expected/oc/50910-3767.mm
@@ -0,0 +1,7 @@
+const auto test = [=](NSString *param) {};
+const auto test = [&](NSString *param) {};
+const auto test = [](NSString *param) {};
+func([=](NSString *param) {});
+func([&](NSString *param) {});
+func([](NSString *param) {});
+func(param1, [=](NSString *param) {});
