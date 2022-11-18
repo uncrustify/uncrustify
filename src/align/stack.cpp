fe94372c45c52cf514921b2e0e7ebbc87798bb87
Ray Strode <rstrode@redhat.com>
2022-11-18 11:12:00 -0500

    Add a gap before dangling parameter pointers
    Function parameter alignment now documents how dangling pointers occupy
    the ordinary gap before a parameter name.
    
    That name gap cannot require a specific amount of whitespace between the
    parameter type and its pointer operators.
    
    This commit advances support for explicit spacing before dangling
    parameter operators by adding an opt-in pointer gap to AlignStack and
    exposing it as align_func_params_star_gap. The option applies to
    dangling pointer style and defaults to zero. It treats the pointer and
    name gaps as overlapping minima and regenerates the option artifacts.
    
    The next commit will pin down the pointer-only spacing behavior with a
    focused C regression test.

--- src/align/stack.cpp
+++ src/align/stack.cpp
@@ -7,7 +7,8 @@
  * @author  Ben Gardner
  * @license GPL v2+
  */
-#include <limits>   // for numeric_limits<>
+#include <algorithm> // for max
+#include <limits>    // for numeric_limits<>
 
 #include "align/stack.h"
 
@@ -52,6 +53,7 @@ void AlignStack::Start(size_t span, int thresh)
    m_nl_seqnum   = 0;
    m_seqnum      = 0;
    m_gap         = 0;
+   m_star_gap    = 0;
    m_right_align = false;
    m_star_style  = SS_IGNORE;
    m_amp_style   = SS_IGNORE;
@@ -391,6 +393,9 @@ void AlignStack::NewLines(size_t cnt)
 
 void AlignStack::Flush()
 {
+   size_t max_stars = 0;
+   size_t star_gap  = 0;
+
    // produces much more log output. Use it only debugging purpose
    // WITH_STACKID_DEBUG;
 
@@ -442,8 +447,9 @@ void AlignStack::Flush()
       Chunk *pc = m_aligned.Get(idx)->m_pc;
 
       // Set the column adjust and gap
-      size_t col_adj = 0;
-      size_t gap     = 0;
+      size_t col_adj   = 0;
+      size_t gap       = 0;
+      size_t extra_gap = 0;
 
       if (pc != pc->GetAlignData().ref)
       {
@@ -458,7 +464,21 @@ void AlignStack::Flush()
          {
             col_adj = pc->GetAlignData().start->GetColumn() - pc->GetColumn();
             gap     = pc->GetAlignData().start->GetColumn() - pc->GetAlignData().ref->GetColumnEnd();
+
+            if (m_star_gap > 0)
+            {
+               if (col_adj + m_star_gap > gap)
+               {
+                  extra_gap = std::max(extra_gap, col_adj + m_star_gap - gap);
+               }
+               max_stars = std::max(max_stars, col_adj);
+            }
          }
+      }
+
+      if (m_gap > gap)
+      {
+         extra_gap = std::max(extra_gap, m_gap - gap);
       }
 
       if (m_right_align)
@@ -483,13 +503,17 @@ void AlignStack::Flush()
       pc->AlignData().col_adj = col_adj;
 
       // See if this pushes out the max_col
-      const size_t endcol = pc->GetColumn() + col_adj
-                            + (gap < m_gap ? m_gap - gap : 0);
+      const size_t endcol = pc->GetColumn() + col_adj + extra_gap;
 
       if (endcol > m_max_col)
       {
          m_max_col = endcol;
       }
+   }
+
+   if (max_stars + m_star_gap > m_gap)
+   {
+      star_gap = max_stars + m_star_gap - m_gap;
    }
 
    log_rule_B("align_on_tabstop");
@@ -537,7 +561,7 @@ void AlignStack::Flush()
          pc->AlignData().amp_style   = m_amp_style;
          pc->AlignData().star_style  = m_star_style;
       }
-      pc->AlignData().gap  = m_gap;
+      pc->AlignData().gap  = m_gap + star_gap;
       pc->AlignData().next = m_aligned.GetChunk(idx + 1);
 
       // Indent the token, taking col_adj into account
