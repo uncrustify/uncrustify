4d8f0dee5a5251e8b6ddba7e7ad514f5dbd7a651
Ray Strode <rstrode@redhat.com>
2022-11-22 12:29:45 -0500

    Add test for new align_func_params_star_gap option
    This commit adds a small test file to show what the
    align_func_params_star_gap option is supposed to do.

---
+++ tests/expected/c/00410-star-style-dangle-gap.c
@@ -0,0 +1,36 @@
+//           alignment gap ↘ |12345678| ↙ alignment gap
+//                  star gap ↘ |1234| ↙ star gap
+int overlapping_gaps(int              one,
+                     int             *two,
+                     unsigned        *three,
+                     long long      **four)
+//                  star gap ↗ |1234| ↖ star gap
+//           alignment gap ↗ |12345678| ↖ alignment gap
+{
+	return 0;
+}
+
+//               alignment gap ↘ |12345678| ↙ alignment gap
+//                 star gap ↘ |1234| ↙ star gap
+int intersecting_gaps(int                 one,
+                      int                *two,
+                      unsigned         ***three,
+                      long long    *******four)
+//                 star gap ↗ |1234| ↖ star gap
+//               alignment gap ↗ |12345678| ↖ alignment gap
+{
+	return 0;
+}
+
+//                         alignment gap ↘ |12345678| ↙ alignment gap
+//                      star gap ↘ |1234| ↙ star gap
+int disjoint_gaps(int                               one,
+                  int                              *two,
+                  unsigned                    ******three,
+                  long long                    *****four,
+                  unsigned long long    ************five)
+//                      star gap ↗ |1234| ↖ star gap
+//                         alignment gap ↗ |12345678| ↖ alignment gap
+{
+	return 0;
+}
