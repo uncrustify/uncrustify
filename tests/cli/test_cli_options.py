18d8e0e281d59768e51860b43fa34d00339db3bd
Guy Maurel <guy.j@maurel.de>
2023-08-29 17:32:57 +0200

    print_stack

--- tests/cli/test_cli_options.py
+++ tests/cli/test_cli_options.py
@@ -126,7 +126,7 @@ def get_file_content(fp):
     out = None
 
     #eprint(abspath(fp))
-    #traceback.print_stack()
+    traceback.print_stack()
     if isfile(fp):
         with open(fp, encoding="utf-8", newline="\n") as f:
             out = f.read()
