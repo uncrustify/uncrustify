72c1bc9fe01973f8457d7570083471e485306618
Michele Calgaro <michele.calgaro@yahoo.it>
2023-10-19 11:59:05 +0900

    Fix issue #4127: conversion on single line trailing javadoc comments
    Signed-off-by: Michele Calgaro <michele.calgaro@yahoo.it>

---
+++ tests/input/c/comment_conversion_javadoc_single.c
@@ -0,0 +1,21 @@
+/* Normal comment */
+
+/** Javadoc comment */
+
+/* Multiline
+ * Normal
+ * comment */
+
+/* Another
+   Multiline
+   Normal
+   comment
+*/
+
+/**
+ * Javadoc
+ * Multiline
+ * Normal
+ * comment
+*/
+
