9a802f20481ada47a75d91a29d872baafe5cd70a
Guy Maurel <guy.j@maurel.de>
2024-06-01 16:55:59 +0200

    provide test data

---
+++ tests/input/c/Issue_3259.h
@@ -0,0 +1,5 @@
+int (*found_proc)(struct i2c_adapter *, int, int);
+
+static int i2c_probe_address(struct i2c_adapter *adapter, int addr, int kind,
+                             int (*found_proc1)(struct i2c_adapter *, int, int),
+                             int (*found_proc2)(struct i2c_adapter *, int, int));
