3d7bc1d90a719148bc59f620f3d98985611fa38b
Guy Maurel <guy.j@maurel.de>
2024-06-05 18:11:11 +0200

    update data

--- tests/expected/c/00420-Issue_2278.c
+++ tests/expected/c/00420-Issue_2278.c
@@ -3,8 +3,9 @@ typedef int tucBOOL;
 struct LIST_zNODE { int a; int b;};
 struct LIST_zzzDATA { int a; int b;};
 
-int foo1( LIST_tzHEAD *pList,
-          tucBOOL (   *pFn )( struct LIST_zNODE   *pNode,
-                              struct LIST_zzzDATA *pListData,
-                              void                *arg1 ),
+int foo1( LIST_tzHEAD                 *pList,
+          tucBOOL                   ( *pFn )(
+		  struct LIST_zNODE   *pNode,
+		  struct LIST_zzzDATA *pListData,
+		  void                *arg1 ),
           void        *arg2 );
