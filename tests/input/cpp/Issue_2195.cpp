689540f688c4b175a332a825de5d2bff9f7cdde3
Guy Maurel <guy.j@maurel.de>
2024-06-05 17:18:51 +0200

    comment too big

--- tests/input/cpp/Issue_2195.cpp
+++ tests/input/cpp/Issue_2195.cpp
@@ -1,5 +1,6 @@
 void Launcher::signal(int code)
 {
+
     //1 HUP      2 INT      3 QUIT     4 ILL      5 TRAP     6 ABRT     7 BUS
     //8 FPE      9 KILL    10 USR1    11 SEGV    12 USR2    13 PIPE    14 ALRM
     //15 TERM    16 STKFLT  17 CHLD    18 CONT    19 STOP    20 TSTP    21 TTIN
