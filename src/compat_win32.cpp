e3b5d97a7fe82962f140930202c915218555c5b9
Guy Maurel <guy.j@maurel.de>
2024-06-26 16:54:03 +0200

    adopt PR #3809

--- src/compat_win32.cpp
+++ src/compat_win32.cpp
@@ -32,14 +32,19 @@ bool unc_getenv(const char *name, std::string &str)
    if (buf != nullptr)
    {
       len = GetEnvironmentVariableA(name, buf, len);
+      buf[len] = 0;
+
+      str = buf;
+      //printf("%s: name=%s len=%zu value=%s\n", __func__, name, len, str.c_str());
+      free(buf);
+
+      return(true);
    }
-   buf[len] = 0;
+   else
+   {
+      return(false);
+   }
 
-   str = buf;
-   //printf("%s: name=%s len=%zu value=%s\n", __func__, name, len, str.c_str());
-   free(buf);
-
-   return(true);
 }
 
 
