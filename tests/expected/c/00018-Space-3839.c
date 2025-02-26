b56141ea4fc094b866a6eb28f76ad904ef437f5b
Guy Maurel <guy.j@maurel.de>
2025-02-26 18:20:42 +0100

    provide some more test data for the option indent_relative_single_line_comments

---
+++ tests/expected/c/00018-Space-3839.c
@@ -0,0 +1,13 @@
+void foo(void)
+{
+        list_for_each(k)
+        {
+                if (a)
+                        if (b)
+                        {
+                                c++;
+                        }
+        }
+        while (1)
+                ; /* hang forever */
+}
---
+++ tests/input/c/Space-3839.c
@@ -0,0 +1,12 @@
+void foo(void)
+{
+   list_for_each(k) {
+      if (a)
+         if (b)
+         {
+            c++;
+         }
+   }
+   while (1)
+      ;  /* hang forever */
+}
