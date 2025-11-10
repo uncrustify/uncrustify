83338b088e337775d421f83ee00d999027e5f3dd
Guy Maurel <guy.j@maurel.de>
2025-11-10 18:17:35 +0100

    use static constexpr size_t TOO_BIG_VALUE

--- src/uncrustify_limits.h
+++ src/uncrustify_limits.h
@@ -9,6 +9,21 @@ namespace limits
 static constexpr int MAX_OPTION_NAME_LEN = 32;
 static constexpr int AL_SIZE             = 8000;
 static constexpr int MAX_KEYWORDS        = 300;
+/*
+ * Consider the setting functions, which have a size_t type parameter.
+ *
+ * The functions which set a (new) value for anything about the column of a line
+ * use a value which is little.
+ * Sometimes, along the development of the code, one get a value which is very
+ * big, the most time, the value is wrong, suche as:
+ *
+ * size_t test = 0;
+ * test = test - 13;
+ *
+ * Such a code should never happear.
+ * The test against TOO_BIG_VALUE is an help at DEBUG time.
+ */
+static constexpr size_t TOO_BIG_VALUE = 10000;
 
 // uncrustify doesn't support more than one variable definition per line/ type,
 // the maximum level of pointer indirection is 3 (i.e., ***p).
