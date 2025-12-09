ad0745f3190826c13b5ecbbad28f25a46556a582
Eric Chamberland <ericc@giref.ulaval.ca>
2025-12-08 22:48:09 -0500

    test(align): Add C++ tests for align_var_def_span_num_{empty,pp,cmt}_lines
    Tests 34734-34747: Verify line skip behavior for local variable alignment in C++ methods.
    - empty_lines tests (34734-34736): skip 1/2/3 empty lines
    - cmt_lines tests (34737-34739): skip 1/2/3 comment lines
    - pp_lines tests (34740-34742): skip 1/2/3 preprocessor lines
    - mixed tests (34743-34747): combined skip configurations

---
+++ tests/expected/cpp/34741-align_var_def_span_num_pp_lines.cpp
@@ -0,0 +1,23 @@
+// Test: align_var_def_span_num_pp_lines in C++ methods
+// With span=1 and varying num_pp_lines settings.
+
+class TestClass
+{
+public:
+void test_method()
+{
+	int               x;
+	unsigned long int y;
+#ifdef DEBUG
+	double            z;
+#endif
+#ifdef EXTRA
+	float             w;
+#endif
+#ifdef MORE
+#ifdef NESTED
+	long s;
+#endif
+#endif
+}
+};
