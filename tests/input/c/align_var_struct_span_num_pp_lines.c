8d06cb57e4827e60a9ffe32f5d68e79474988daa
Eric Chamberland <ericc@giref.ulaval.ca>
2025-12-08 22:47:40 -0500

    test(align): Add C tests for align_var_struct_span_num_{empty,pp,cmt}_lines
    Tests 10130-10143: Verify line skip behavior for struct member alignment in C.
    - empty_lines tests (10130-10132): skip 1/2/3 empty lines
    - cmt_lines tests (10133-10135): skip 1/2/3 comment lines
    - pp_lines tests (10136-10138): skip 1/2/3 preprocessor lines
    - mixed tests (10139-10143): combined skip configurations

---
+++ tests/input/c/align_var_struct_span_num_pp_lines.c
@@ -0,0 +1,19 @@
+// Test: align_var_struct_span_num_pp_lines
+// With span=1 and varying num_pp_lines settings.
+
+struct TestStruct
+{
+    int x;
+    unsigned long int y;
+#ifdef DEBUG
+    double z;
+#endif
+#ifdef EXTRA
+    float w;
+#endif
+#ifdef MORE
+#ifdef NESTED
+    long s;
+#endif
+#endif
+};
