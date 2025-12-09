a67631f66a243b85d5fce458ae11e8717ef2b8f3
Eric Chamberland <ericc@giref.ulaval.ca>
2025-12-08 23:14:46 -0500

    test(align): Add C++ tests for align_var_class_span_num_{empty,pp,cmt}_lines
    Tests 34720-34733: Verify line skip behavior for class member alignment in C++.
      - empty_lines tests (34720-34722): skip 1/2/3 empty lines
      - cmt_lines tests (34723-34725): skip 1/2/3 comment lines
      - pp_lines tests (34726-34728): skip 1/2/3 preprocessor lines
      - mixed tests (34729-34733): combined skip configurations

---
+++ tests/expected/cpp/34722-align_var_class_span_num_empty_lines.cpp
@@ -0,0 +1,18 @@
+// Test: align_var_class_span_num_empty_lines
+// With span=1 and varying num_empty_lines settings.
+
+class TestClass
+{
+public:
+int               x;
+unsigned long int y;
+
+double            z;
+
+
+float             w;
+
+
+
+short             s;
+};
---
+++ tests/expected/cpp/34732-align_var_class_span_num_mixed.cpp
@@ -0,0 +1,28 @@
+// Test: align_var_class_span with mixed empty/comment/PP lines
+// Tests interleaved PP, comments, and empty lines
+
+class TestClass
+{
+public:
+int               x;
+unsigned long int y;
+#ifdef A
+//!< 1 PP + 1 cmt between y and z
+double            z;
+
+//!< 1 empty + 1 cmt between z and w
+float             w;
+#endif
+#ifdef B
+//!< 2 PP + 1 cmt between w and s
+long              s;
+//!< 1 cmt between s and t
+//!< 2 cmt
+//!< 3 cmt
+long int          t;
+
+
+//!< 2 empty + 1 cmt between t and u
+char u;
+#endif
+};
