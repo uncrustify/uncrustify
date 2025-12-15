c8d649c23694c9cdab5a4fb6a7fe8e44ce0d2771
Eric Chamberland <ericc@giref.ulaval.ca>
2025-12-15 15:32:06 -0500

    test(align): Add C++ tests for align_func_proto_span_num_{empty,pp,cmt}_lines
      Tests 34756-34772: Verify line skip behavior for functions and methods alignment in C++.
        - empty_lines tests (34756-34760): skip 1/2/3 empty lines, also test span=2,3
        - cmt_lines tests (34761-34763): skip 1/2/3 comment lines
        - pp_lines tests (34764-34766): skip 1/2/3 preprocessor lines
        - mixed tests (34767-34772): combined skip configurations and span=0

---
+++ tests/input/cpp/align_func_proto_span_num_pp_lines.cpp
@@ -0,0 +1,18 @@
+// Test: align_func_proto_span_num_pp_lines - Preprocessor lines between function prototypes
+
+class APIClass {
+public:
+    LongReturnType api_function_one();
+#ifdef FEATURE_A
+    int api_function_two();
+#endif
+#ifdef FEATURE_B
+    short api_function_three(int param);
+#endif
+#if defined(FEATURE_C)
+#define SOME_MACRO
+    unsigned int api_function_four();
+#endif
+
+    long int final_function_five();
+};
