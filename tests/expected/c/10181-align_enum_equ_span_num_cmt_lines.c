32bb3cdceb23a9a44d32f7407a9f3d15aba0ef43
Eric Chamberland <ericc@giref.ulaval.ca>
2025-12-18 20:14:31 -0500

    test(align): Add complete C test suite for align_assign/enum_equ_span_num_*_lines
    Add systematic C tests following the same pattern as C++:
    
    Basic tests (tests 10173-10186):
    - align_enum_equ_span_num_empty_lines: 1 empty line
    - align_assign_span_num_empty_lines: 1 empty line
    - align_assign_span_num_cmt_lines: 1, 2, 3 comment lines (10175-10177)
    - align_assign_span_num_pp_lines: 1, 2, 3 PP lines (10178-10180)
    - align_enum_equ_span_num_cmt_lines: 1, 2, 3 comment lines (10181-10183)
    - align_enum_equ_span_num_pp_lines: 1, 2, 3 PP lines (10184-10186)
    
    All 12 new C tests pass.

---
+++ tests/expected/c/10181-align_enum_equ_span_num_cmt_lines.c
@@ -0,0 +1,17 @@
+/* Test: align_enum_equ_span_num_cmt_lines - Comment lines between enum values */
+
+enum TestEnum
+{
+    VERY_LONG_VALUE_NAME = 100,
+    /* Single comment */
+    SHORT_VAL            = 200,
+    /* Comment 1 */
+    /* Comment 2 */
+    MEDIUM_VALUE = 300,
+    /* Comment 1 */
+    /* Comment 2 */
+    /* Comment 3 */
+    ANOTHER_LONG_NAME = 400,
+
+    FINAL_VAL = 500
+};
---
+++ tests/input/c/align_enum_equ_span_num_empty_lines.c
@@ -0,0 +1,17 @@
+/* Test: align_enum_equ_span_num_empty_lines - Empty lines between enum values */
+
+enum TestEnum
+{
+    VERY_LONG_VALUE_NAME = 100,
+
+    SHORT_VAL = 200,
+
+
+    MEDIUM_VALUE = 300,
+
+
+
+    ANOTHER_LONG_NAME = 400,
+
+    FINAL_VAL = 500
+};
